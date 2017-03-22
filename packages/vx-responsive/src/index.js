c8762d1664e1abed3749e6fddb94f731b4d54157
Harry Shoff <harry@airbnb.com>
2017-03-21 18:14:11 -0700

    v0.0.41

---
+++ packages/vx-responsive/src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export default function ResponsiveSVG({
+  children,
+  preserveAspectRatio = 'xMinYMin meet',
+  viewBox = "0 0 300 300",
+}) {
+  return (
+    <div style={{
+      display: 'inline-block',
+      position: 'relative',
+      width: '100%',
+      paddingBottom: '100%',
+      verticalAling: 'top',
+      overflow: 'hidden',
+    }}>
+      <svg
+        preserveAspectRatio={preserveAspectRatio}
+        viewBox={viewBox}
+        style={{
+          display: 'inline-block',
+          position: 'absolute',
+          top: 0,
+          left: 0,
+        }}
+      >
+        {children}
+      </svg>
+    </div>
+  );
+}
