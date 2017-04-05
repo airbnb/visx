bf1023a90bba459a67bbfc4d8216b222321d6817
Harry Shoff <harry@airbnb.com>
2017-04-05 10:50:48 -0700

    v0.0.84

---
+++ packages/vx-scale/src/scales/power.js
@@ -0,0 +1,21 @@
+import { scalePow } from 'd3-scale';
+
+export default ({
+  range,
+  rangeRound,
+  domain,
+  base,
+  nice = false,
+  clamp = false,
+}) => {
+  const scale = scalePow();
+
+  if (range) scale.range(range);
+  if (rangeRound) scale.rangeRound(rangeRound);
+  if (domain) scale.domain(domain);
+  if (nice) scale.nice();
+  if (clamp) scale.clamp(true);
+  if (base) scale.base(base);
+
+  return scale;
+}
