533060b6ddf31929e46dde18c090305d749b10a2
Harry Shoff <harry@airbnb.com>
2017-04-19 16:58:23 -0700

    v0.0.96

---
+++ packages/vx-brush/src/index.js
@@ -0,0 +1,11 @@
+import withBrush from './enhancers/withBrush';
+import BoxBrush from './components/BoxBrush';
+import constrainToRegion from './utils/constrainToRegion';
+import getCoordsFromEvent from './utils/getCoordsFromEvent';
+
+export default {
+  withBrush,
+  BoxBrush,
+  constrainToRegion,
+  getCoordsFromEvent,
+}
