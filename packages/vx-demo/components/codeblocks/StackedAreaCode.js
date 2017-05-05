d7d6e63d4880050a2e2b75b84eaae914ef25901f
Harrison Shoff <hi@hshoff.com>
2017-05-05 08:31:14 -0700

    [gallery] update stack area codeblock

--- packages/vx-demo/components/codeblocks/StackedAreaCode.js
+++ packages/vx-demo/components/codeblocks/StackedAreaCode.js
@@ -18,14 +18,23 @@ import { stack as d3stack } from 'd3-shape';
 import { timeParse } from 'd3-time-format';
 import round from '../util/round';
 import colorScale from '../util/sillyColorScale';
+import withState from 'recompose/withState';
+import compose from 'recompose/compose';
 
-export default ({
+const enhance = compose(
+  withState('selected', 'updateSelected', [])
+);
+
+export default enhance(({
   margin,
   width,
   height,
+  selected,
+  updateSelected,
 }) => {
   const data = Mock.browserUsage;
   const keys = Object.keys(data[0]).filter(k => k !== 'date');
+  const browserNames = [...keys].reverse();
 
   const yMax = height - margin.top - margin.bottom;
   const xMax = width - margin.left - margin.right;
@@ -49,13 +58,14 @@ export default ({
         top={margin.top}
         left={margin.left}
         scale={yScale}
-        tickFormat={(v) => '{round(v * 100)}%'}
+        tickFormat={(v) => '%{round(v * 100)}%'}
         label={'browser market share (%)'}
         stroke={'#1b1a1e'}
         tickTextFill={'#000'}
       />
       <Group top={margin.top} left={margin.left}>
         <Shape.AreaStack
+          reverse
           top={margin.top}
           left={margin.left}
           keys={keys}
@@ -63,11 +73,19 @@ export default ({
           x={(d) => xScale(x(d.data))}
           y0={(d) => yScale(d[0] / 100)}
           y1={(d) => yScale(d[1] / 100)}
-          fill={(d,i) => colorScale(i)}
-          fillOpacity={0.8}
           stroke={(d,i) => colorScale(i)}
           strokeWidth={1}
-          reverse
+          fillOpacity={(d,i) => selected.includes(browserNames[i]) ? 0.8 : 0.2}
+          fill={(d,i) => colorScale(i)}
+          onMouseEnter={(d, i) => event => {
+            updateSelected((prevState) => ([browserNames[i]]))
+          }}
+          onMouseLeave={(d,i) => event => {
+            updateSelected(prevState => {
+              if (prevState.includes(browserNames[i])) return [];
+              return prevState;
+            })
+          }}
         />
         {stack(data).reverse().map((series,i) => {
           const lastPoint = series[series.length - 1];
@@ -75,7 +93,7 @@ export default ({
           const lastPointY1 = lastPoint[1] / 100;
 
           return (
-            <g key={'labels-{series.key}'}>
+            <g key={'labels-%{series.key}'}>
               {lastPointY1 - lastPointY0 > 0.01 &&
                 <Text.TextOutline
                   fontSize={10}
@@ -84,8 +102,8 @@ export default ({
                   dy={'.5em'}
                   textAnchor={'end'}
                   fill="black"
-                  outlineStroke={"rgba(255,255,255,0.5)"}
-                  outlineStrokeWidth={2}
+                  outlineStroke={"white"}
+                  outlineStrokeWidth={3}
                   fontFamily={"Roboto Mono"}
                 >
                   {series.key}
@@ -108,7 +126,7 @@ export default ({
       />
     </svg>
   );
-}`}
+})`}
     </Codeblock>
   );
 }
