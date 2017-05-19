1262d38908b73b8b67ee6a2bdbdf23e771c7208c
Harry Shoff <harry@airbnb.com>
2017-05-19 15:46:17 -0700

    [gradient] add tests

---
+++ packages/vx-gradient/test/Gradients.test.js
@@ -0,0 +1,72 @@
+import {
+  GradientDarkgreenGreen,
+  GradientLightgreenGreen,
+  GradientOrangeRed,
+  GradientPinkBlue,
+  GradientPinkRed,
+  GradientPurpleOrange,
+  GradientPurpleRed,
+  GradientPurpleTeal,
+  GradientSteelPurple,
+  GradientTealBlue,
+} from '../src';
+
+describe('<GradientDarkgreenGreen />', () => {
+  test('it should be defined', () => {
+    expect(GradientDarkgreenGreen).toBeDefined()
+  })
+})
+
+describe('<GradientLightgreenGreen />', () => {
+  test('it should be defined', () => {
+    expect(GradientLightgreenGreen).toBeDefined()
+  })
+})
+
+describe('<GradientOrangeRed />', () => {
+  test('it should be defined', () => {
+    expect(GradientOrangeRed).toBeDefined()
+  })
+})
+
+describe('<GradientPinkBlue />', () => {
+  test('it should be defined', () => {
+    expect(GradientPinkBlue).toBeDefined()
+  })
+})
+
+describe('<GradientPinkRed />', () => {
+  test('it should be defined', () => {
+    expect(GradientPinkRed).toBeDefined()
+  })
+})
+
+describe('<GradientPurpleOrange />', () => {
+  test('it should be defined', () => {
+    expect(GradientPurpleOrange).toBeDefined()
+  })
+})
+
+describe('<GradientPurpleRed />', () => {
+  test('it should be defined', () => {
+    expect(GradientPurpleRed).toBeDefined()
+  })
+})
+
+describe('<GradientPurpleTeal />', () => {
+  test('it should be defined', () => {
+    expect(GradientPurpleTeal).toBeDefined()
+  })
+})
+
+describe('<GradientSteelPurple />', () => {
+  test('it should be defined', () => {
+    expect(GradientSteelPurple).toBeDefined()
+  })
+})
+
+describe('<GradientTealBlue />', () => {
+  test('it should be defined', () => {
+    expect(GradientTealBlue).toBeDefined()
+  })
+})
