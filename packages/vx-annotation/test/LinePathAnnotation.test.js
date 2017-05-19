0fce211871cfd2548ffecb5f1194e3b770fa1c2e
Harrison Shoff <hi@hshoff.com>
2017-05-18 22:55:12 -0700

    [annotation] add tests

---
+++ packages/vx-annotation/test/LinePathAnnotation.test.js
@@ -0,0 +1,7 @@
+import { LinePathAnnotation } from '../src';
+
+describe('<LinePathAnnotation />', () => {
+  test('it should be defined', () => {
+    expect(LinePathAnnotation).toBeDefined()
+  })
+})
