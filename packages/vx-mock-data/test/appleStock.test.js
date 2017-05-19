d4d16e19ba760d8e5b126889587cf9406b16b2bd
Harrison Shoff <hi@hshoff.com>
2017-05-18 22:48:49 -0700

    [mock-data] add tests

---
+++ packages/vx-mock-data/test/appleStock.test.js
@@ -0,0 +1,19 @@
+import { appleStock } from '../src';
+
+describe('mocks/appleStock', () => {
+  test('it should be defined', () => {
+    expect(appleStock).toBeDefined()
+  })
+
+  test('it should be an array', () => {
+    expect(appleStock.length).toBeDefined()
+  })
+
+  test('it should return [{ date, close }]', () => {
+    const data = appleStock
+    expect(data[0].date).toBeDefined()
+    expect(data[0].close).toBeDefined()
+    expect(typeof data[0].date).toEqual('string')
+    expect(typeof data[0].close).toEqual('number')
+  })
+})
