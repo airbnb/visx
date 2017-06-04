4a4441817d200624b94a799e105f27e15459540a
Flaqueeau <econrad@zagmail.gonzaga.edu>
2017-06-04 14:28:52 -0700

    [scales] fix export

--- packages/vx-scale/src/index.js
+++ packages/vx-scale/src/index.js
@@ -2,6 +2,7 @@ export { default as scaleBand } from './scales/band';
 export { default as scalePoint } from './scales/point';
 export { default as scaleLinear } from './scales/linear';
 export { default as scaleTime } from './scales/time';
+export { default as scaleUtc } from './scales/utc';
 export { default as scaleLog } from './scales/log';
 export { default as scalePower } from './scales/power';
 export { default as scaleOrdinal } from './scales/ordinal';
