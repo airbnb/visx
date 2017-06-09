27c0a70bcd1585bbf112f109cb3b50c424e45647
Chris Williams <chris.williams@airbnb.com>
2017-06-09 16:49:55 -0700

    [ClipPath] add ClipPath package

---
+++ packages/vx-clip-path/src/index.js
@@ -0,0 +1,3 @@
+export { default as ClipPath } from './clip-paths/ClipPath';
+export { default as CircleClipPath } from './clip-paths/CircleClipPath';
+export { default as RectClipPath } from './clip-paths/RectClipPath';
