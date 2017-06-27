362fd204bdf4691a4759a592661e70bb8a96720d
Chris Williams <chris.williams@airbnb.com>
2017-06-27 16:19:00 -0700

    [vx-voronoi] add @vx/voronoi, example tile and mouseove proof-of-concept page, and tests

---
+++ packages/vx-voronoi/src/components/VoronoiPolygon.js
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import cx from 'classnames';
+
+import additionalProps from '../util/additionalProps';
+
+const propTypes = {
+  polygon: PropTypes.arrayOf(PropTypes.array),
+};
+
+export default function VoronoiPolygon({
+  polygon,
+  className,
+  ...restProps,
+}) {
+  if (!polygon) return null;
+  const data = polygon.data;
+  return (
+    <path
+      className={cx('vx-voronoi-polygon', className)}
+      d={`M${polygon.join('L')}Z`}
+      {...additionalProps(restProps, data)}
+    />
+  );
+}
+
+VoronoiPolygon.propTypes = propTypes;
