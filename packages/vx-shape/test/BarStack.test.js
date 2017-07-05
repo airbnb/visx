4706d261bb8c630ad508de7d275594f01bd2238f
Harry Shoff <harry@airbnb.com>
2017-07-05 10:01:00 -0700

    [shape] fix barstack.test.js

--- packages/vx-shape/test/BarStack.test.js
+++ packages/vx-shape/test/BarStack.test.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { BarStack } from '../src';
 import { shallow } from 'enzyme';
 
+const xScale = jest.fn();
+xScale.bandwidth = jest.fn();
+xScale.step = jest.fn();
+xScale.paddingInner = jest.fn();
+xScale.paddingOuter = jest.fn();
+
 describe('<BarStack />', () => {
   test('it should be defined', () => {
-    expect(BarStack).toBeDefined()
-  })
+    expect(BarStack).toBeDefined();
+  });
 
   test('it should have className .vx-bar-stack', () => {
     const wrapper = shallow(
@@ -14,47 +20,47 @@ describe('<BarStack />', () => {
         top={2}
         left={3}
         x={d => d}
-        xScale={d => d}
+        xScale={xScale}
         yScale={d => d}
         zScale={d => d}
         keys={[]}
-      />
-    )
-    expect(wrapper.prop('className')).toEqual('vx-bar-stack')
-  })
+      />,
+    );
+    expect(wrapper.prop('className')).toEqual('vx-bar-stack');
+  });
 
   test('it should set className prop', () => {
     const wrapper = shallow(
       <BarStack
-        className='test'
+        className="test"
         data={[]}
         top={2}
         left={3}
         x={d => d}
-        xScale={d => d}
+        xScale={xScale}
         yScale={d => d}
         zScale={d => d}
         keys={[]}
-      />
-    )
-    expect(wrapper.prop('className')).toEqual('vx-bar-stack test')
-  })
+      />,
+    );
+    expect(wrapper.prop('className')).toEqual('vx-bar-stack test');
+  });
 
   test('it should set top & left props', () => {
     const wrapper = shallow(
       <BarStack
-        className='test'
+        className="test"
         data={[]}
         top={2}
         left={3}
         x={d => d}
-        xScale={d => d}
+        xScale={xScale}
         yScale={d => d}
         zScale={d => d}
         keys={[]}
-      />
-    )
-    expect(wrapper.prop('top')).toEqual(2)
-    expect(wrapper.prop('left')).toEqual(3)
-  })
-})
+      />,
+    );
+    expect(wrapper.prop('top')).toEqual(2);
+    expect(wrapper.prop('left')).toEqual(3);
+  });
+});
