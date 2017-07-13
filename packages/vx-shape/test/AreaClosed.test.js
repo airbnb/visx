9f311db4ddad771651f68723ff8b07192f066b8e
Flaqueeau <econrad@zagmail.gonzaga.edu>
2017-07-12 19:51:11 -0700

    [shape] Add tests for Arc, AreaClosed, & Line

--- packages/vx-shape/test/AreaClosed.test.js
+++ packages/vx-shape/test/AreaClosed.test.js
@@ -1,7 +1,42 @@
-import { AreaClosed } from '../src';
+import React from "react";
+import { shallow } from "enzyme";
+import { extent, max } from "d3-array";
+import { AreaClosed } from "../src";
+import { appleStock } from "../../vx-mock-data";
+import { scaleTime, scaleLinear } from "../../vx-scale";
 
-describe('<AreaClosed />', () => {
-  test('it should be defined', () => {
-    expect(AreaClosed).toBeDefined()
-  })
-})
+const xStock = d => new Date(d.date);
+const yStock = d => d.close;
+
+const fakeXScale = scaleTime({
+  range: [0, 100],
+  domain: extent(appleStock, xStock)
+});
+
+const fakeYScale = scaleLinear({
+  range: [100, 0],
+  domain: [0, max(appleStock, yStock)],
+  nice: true
+});
+
+const AreaClosedWrapper = ({ ...restProps }) =>
+  shallow(
+    <AreaClosed
+      data={appleStock}
+      xScale={fakeXScale}
+      yScale={fakeYScale}
+      x={xStock}
+      y={yStock}
+      {...restProps}
+    />
+  );
+
+describe("<AreaClosed />", () => {
+  test("it should be defined", () => {
+    expect(AreaClosed).toBeDefined();
+  });
+
+  test("it should have the .vx-area-closed class", () => {
+    expect(AreaClosedWrapper().find('path').prop("className")).toBe("vx-area-closed");
+  });
+});
--- packages/vx-shape/test/Line.test.js
+++ packages/vx-shape/test/Line.test.js
@@ -1,7 +1,19 @@
-import { Line } from '../src';
+import React from "react";
+import { shallow } from "enzyme";
+import { Line } from "../src";
 
-describe('<Line />', () => {
-  test('it should be defined', () => {
-    expect(Line).toBeDefined()
-  })
-})
+const LineWrapper = ({ ...restProps }) => shallow(<Line {...restProps} />);
+
+describe("<Line />", () => {
+  test("it should be defined", () => {
+    expect(Line).toBeDefined();
+  });
+
+  test("it should contain a <line/>", () => {
+    expect(LineWrapper().find("line").length).toBe(1);
+  });
+
+  test("it should have the .vx-line class", () => {
+    expect(LineWrapper().prop("className")).toBe("vx-line");
+  });
+});
