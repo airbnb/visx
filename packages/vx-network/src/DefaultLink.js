6a8859c31ce0d87bba6c97b7f267f638e2fb1651
Harry Shoff <harry@airbnb.com>
2017-08-14 12:05:47 -0700

    [network] add classnames, prettier style polish

--- packages/vx-network/src/DefaultLink.js
+++ packages/vx-network/src/DefaultLink.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
-export default function DefaultLink({link}) {
-  return <line x1={link.source.x} y1={link.source.y}
-               x2={link.target.x} y2={link.target.y}
-               strokeWidth={2} stroke="#999" strokeOpacity={0.6}
-  />;
+export default function DefaultLink({ link }) {
+  return (
+    <line
+      x1={link.source.x}
+      y1={link.source.y}
+      x2={link.target.x}
+      y2={link.target.y}
+      strokeWidth={2}
+      stroke="#999"
+      strokeOpacity={0.6}
+    />
+  );
 }
