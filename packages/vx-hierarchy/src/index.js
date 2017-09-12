2f20252557a616b3a2dbcceda39c8390682ace60
Harry Shoff <harry@airbnb.com>
2017-09-12 12:27:41 -0700

    [hierarchy] update path in index.js

--- packages/vx-hierarchy/src/index.js
+++ packages/vx-hierarchy/src/index.js
@@ -1,4 +1,4 @@
 export { default as Tree } from "./hierarchies/Tree";
 export { default as Cluster } from "./hierarchies/Cluster";
-export { default as HierarchyDefaultLink } from "./DefaultLink";
-export { default as HierarchyDefaultNode } from "./DefaultNode";
+export { default as HierarchyDefaultLink } from "./HierarchyDefaultLink";
+export { default as HierarchyDefaultNode } from "./HierarchyDefaultNode";
