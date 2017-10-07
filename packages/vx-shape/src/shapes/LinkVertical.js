931dbee38864980564652502763df132847e71f4
Harry Shoff <harry@airbnb.com>
2017-10-06 22:56:59 -0700

    [shape] add innerRef to <LinkVertical />

--- packages/vx-shape/src/shapes/LinkVertical.js
+++ packages/vx-shape/src/shapes/LinkVertical.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import cx from 'classnames';
+import PropTypes from 'prop-types';
 import { linkVertical } from 'd3-shape';
 import additionalProps from '../util/additionalProps';
 
+LinkVertical.propTypes = {
+  innerRef: PropTypes.func,
+};
+
 export default function LinkVertical({
   className,
+  innerRef,
   data,
   x = d => d.x,
   y = d => d.y,
   ...restProps
 }) {
-  const link = linkVertical()
+  const link = linkVertical();
   link.x(x);
   link.y(y);
   return (
     <path
+      ref={innerRef}
       className={cx('vx-link-vertical', className)}
       d={link(data)}
       {...additionalProps(restProps, data)}
--- packages/vx-shape/test/LinkVertical.test.js
+++ packages/vx-shape/test/LinkVertical.test.js
@@ -1,7 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { hierarchy } from 'd3-hierarchy';
 import { LinkVertical } from '../src';
+
+const mockHierarchy = hierarchy({
+  name: 'Eve',
+  children: [
+    { name: 'Cain' },
+    {
+      name: 'Seth',
+      children: [{ name: 'Enos' }, { name: 'Noam' }],
+    },
+  ],
+});
+const link = mockHierarchy.links()[0];
 
 describe('<LinkVertical />', () => {
   test('it should be defined', () => {
-    expect(LinkVertical).toBeDefined()
-  })
-})
+    expect(LinkVertical).toBeDefined();
+  });
+
+  test('it should expose its ref via an innerRef prop', done => {
+    const node = document.createElement('div');
+    const refCallback = n => {
+      expect(n.tagName).toEqual('PATH');
+      done();
+    };
+    ReactDOM.render(
+      <LinkVertical innerRef={refCallback} data={link} />,
+      node,
+    );
+  });
+});
