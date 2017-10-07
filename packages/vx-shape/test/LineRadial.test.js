8d9385c1536096427c6a988c55924dca3c7c759b
Harry Shoff <harry@airbnb.com>
2017-10-06 22:02:24 -0700

    [shape] add innerRef prop to <LineRadial />

--- packages/vx-shape/test/LineRadial.test.js
+++ packages/vx-shape/test/LineRadial.test.js
@@ -1,7 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { LineRadial } from '../src';
+
+const lineRadialProps = {
+  data: [{ x: 0, y: 0 }, { x: 1, y: 1 }],
+};
 
 describe('<LineRadial />', () => {
   test('it should be defined', () => {
-    expect(LineRadial).toBeDefined()
-  })
-})
+    expect(LineRadial).toBeDefined();
+  });
+
+  test('it should expose its ref via an innerRef prop', done => {
+    const node = document.createElement('div');
+    const refCallback = n => {
+      expect(n.tagName).toEqual('PATH');
+      done();
+    };
+    ReactDOM.render(
+      <LineRadial innerRef={refCallback} {...lineRadialProps} />,
+      node,
+    );
+  });
+});
