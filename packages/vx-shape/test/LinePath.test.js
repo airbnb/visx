e37fefbe0fcf512ca3de7fff7ae5d3fb02d32632
Harry Shoff <harry@airbnb.com>
2017-10-06 21:55:50 -0700

    [shape] add innerRef prop to <LinePath />

--- packages/vx-shape/test/LinePath.test.js
+++ packages/vx-shape/test/LinePath.test.js
@@ -1,7 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import { LinePath } from '../src';
+
+const linePathProps = {
+  data: [{ x: 0, y: 0 }, { x: 1, y: 1 }],
+  x: d => d.x,
+  y: d => d.y,
+  xScale: d => d,
+  yScale: d => d,
+};
 
 describe('<LinePath />', () => {
   test('it should be defined', () => {
-    expect(LinePath).toBeDefined()
-  })
-})
+    expect(LinePath).toBeDefined();
+  });
+
+  test('it should expose its ref via an innerRef prop', done => {
+    const node = document.createElement('div');
+    const refCallback = n => {
+      expect(n.tagName).toEqual('PATH');
+      done();
+    };
+    ReactDOM.render(
+      <LinePath innerRef={refCallback} {...linePathProps} />,
+      node,
+    );
+  });
+});
