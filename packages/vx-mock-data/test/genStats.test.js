21ca91d2411b34c5e6fffe46f2350ea131330fbb
Conglei Shi <conglei.shi@airbnb.com>
2017-11-12 23:37:45 -0800

    added tests

---
+++ packages/vx-mock-data/test/genStats.test.js
@@ -0,0 +1,15 @@
+import { genStats } from '../src';
+
+describe('generators/genStats', () => {
+  test('it should be defined', () => {
+    expect(genStats).toBeDefined();
+  });
+
+  test('it should have boxPlot and binData', () => {
+    const data = genStats(2);
+    expect(data.length).toBeDefined();
+    expect(data.length).toEqual(2);
+    expect(data[0].boxPlot).toBeDefined();
+    expect(data[0].binData).toBeDefined();
+  });
+});
---
+++ packages/vx-stats/test/ViolinPlot.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { scaleLinear } from '../../vx-scale';
+import { ViolinPlot, computeStats } from '../src';
+
+const data = [1, 2, 3, 4, 5, 6, 6, 7, 8, 9, 1];
+const { binData } = computeStats(data);
+
+const valueScale = scaleLinear({
+  rangeRound: [10, 0],
+  domain: [0, 10]
+});
+
+describe('<VoilinPlot />', () => {
+  test('it should be defined', () => {
+    expect(ViolinPlot).toBeDefined();
+  });
+
+  test('it should have className .vx-violin', () => {
+    const wrapper = shallow(
+      <ViolinPlot
+        binData={binData}
+        left={3}
+        width={100}
+        valueScale={valueScale}
+      />,
+    );
+    expect(wrapper.prop('className')).toEqual('vx-violin');
+  });
+
+  test('it should render one path element', () => {
+    const wrapper = shallow(
+      <ViolinPlot
+        binData={binData}
+        left={3}
+        width={100}
+        valueScale={valueScale}
+      />,
+    );
+    expect(wrapper.find('path').length).toEqual(1);
+  });
+
+});
