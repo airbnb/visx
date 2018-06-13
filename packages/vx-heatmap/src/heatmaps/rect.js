e56b906c23a3a4fe440b3ed75a21d8bb3c4d6a4f
hshoff <hi@hshoff.com>
2018-06-12 21:28:35 -0400

    [shape] add index + yBin to heatmap circle/rect

--- packages/vx-heatmap/src/heatmaps/rect.js
+++ packages/vx-heatmap/src/heatmaps/rect.js
@@ -1,7 +1,25 @@
-import React from 'react';
-import cx from 'classnames';
 import { Group } from '@vx/group';
+import cx from 'classnames';
+import PropTypes from 'prop-types';
+import React from 'react';
 import additionalProps from '../util/additionalProps';
+
+HeatmapRect.propTypes = {
+  data: PropTypes.array,
+  binWidth: PropTypes.number,
+  binHeight: PropTypes.number,
+  x: PropTypes.number,
+  gap: PropTypes.number,
+  step: PropTypes.number,
+  xScale: PropTypes.func,
+  yScale: PropTypes.func,
+  colorScale: PropTypes.func,
+  opacityScale: PropTypes.func,
+  yBin: PropTypes.func,
+  bin: PropTypes.func,
+  bins: PropTypes.func,
+  count: PropTypes.func
+};
 
 export default function HeatmapRect({
   className,
@@ -15,19 +33,21 @@ export default function HeatmapRect({
   yScale,
   colorScale,
   opacityScale = d => 1,
-  bin = d => d.bin,
-  bins = d => d.bins,
+  yBin,
+  bin = (d, i) => d.bin,
+  bins = (d, i) => d.bins,
   count = d => d.count,
   ...restProps
 }) {
+  yBin = yBin || bin;
   const width = binWidth - gap;
   const height = binHeight - gap;
   return (
     <Group>
       {data.map((d, i) => {
         return (
-          <Group key={`heatmap-${i}`} className="vx-heatmap-column" left={xScale(bin(d))}>
-            {bins(d).map((b, j) => {
+          <Group key={`heatmap-${i}`} className="vx-heatmap-column" left={xScale(bin(d, i))}>
+            {bins(d, i).map((b, j) => {
               return (
                 <rect
                   key={`heatmap-tile-rect-${j}`}
@@ -36,7 +56,7 @@ export default function HeatmapRect({
                   width={width}
                   height={height}
                   x={x}
-                  y={yScale(bin(b) + step) + gap}
+                  y={yScale(yBin(b, j) + step) + gap}
                   fillOpacity={opacityScale(count(b))}
                   {...additionalProps(restProps, {
                     bin: b,
