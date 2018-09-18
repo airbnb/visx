013f2abd5fee794ea0c246e9d1de08559817af68
Harry Shoff <harry@airbnb.com>
2018-09-18 15:35:26 -0700

    [demo] add key to polygons tile

--- packages/vx-demo/pages/polygons.js
+++ packages/vx-demo/pages/polygons.js
@@ -7,39 +7,49 @@ export default () => (
     {`import React from 'react';
 import { Polygon } from '@vx/shape';
 import { Group } from '@vx/group';
-import { GradientPinkBlue } from '@vx/gradient';
+import { GradientPinkRed } from '@vx/gradient';
+import { scaleBand } from '@vx/scale';
 
-const additionalProps = [
+const polygons = [
   {
+    sides: 3,
     fill: 'rgb(174, 238, 248)',
     rotate: 90
   },
   {
+    sides: 4,
     fill: 'rgb(229, 253, 61)',
     rotate: 45
   },
   {
+    sides: 6,
     fill: 'rgb(229, 130, 255)',
     rotate: 0
   },
   {
-    fill: 'url(#pink)',
+    sides: 8,
+    fill: 'url(#polygon-pink)',
     rotate: 0
   }
 ];
 
 export default ({ width, height }) => {
+  const yScale = scaleBand({
+    rangeRound: [0, height],
+    domain: polygons.map((p, i) => i),
+    padding: 0.5
+  });
   return (
     <svg width={width} height={height}>
-      <GradientPinkBlue id="gradients" />
-      {[3, 4, 6, 8].map((n, i) => (
-        <Group top={i * 70 + 50} left={width / 2}>
-          <Polygon sides={n} size={25} {...additionalProps[i]} />
+      <rect x={0} y={0} width={width} height={height} fill="#7f82e3" rx={14} />
+      <GradientPinkRed id="polygon-pink" />
+      {polygons.map((polygon, i) => (
+        <Group key={\`polygon-\${i}\`} top={yScale(i)} left={width / 2}>
+          <Polygon sides={polygon.sides} size={25} fill={polygon.fill} rotate={polygon.rotate} />
         </Group>
       ))}
     </svg>
   );
-};
-`}
+};`}
   </Show>
 );
