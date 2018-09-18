06777b48b7d2ea84a2e9d4246b9d9efbc3edff05
Harry Shoff <harry@airbnb.com>
2018-09-18 15:45:56 -0700

    [text] position measurement svg offscreen

--- packages/vx-text/src/util/getStringWidth.js
+++ packages/vx-text/src/util/getStringWidth.js
@@ -11,6 +11,8 @@ function getStringWidth(str, style) {
       svg.style.width = 0;
       svg.style.height = 0;
       svg.style.position = 'absolute';
+      svg.style.top = '-99px';
+      svg.style.left = '-99px';
       textEl = document.createElementNS('http://www.w3.org/2000/svg', 'text');
       textEl.setAttribute('id', MEASUREMENT_ELEMENT_ID);
       svg.appendChild(textEl);
