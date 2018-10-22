77c560cd18556a9f65911b0cd13b810f8cae7f51
hshoff <hi@hshoff.com>
2018-10-21 20:58:57 -0400

    [heatmap][breaking] rm additionalProps, add children as fn

--- packages/vx-heatmap/src/heatmaps/rect.js
+++ packages/vx-heatmap/src/heatmaps/rect.js
@@ -1,11 +1,12 @@
-import { Group } from '@vx/group';
+import React from 'react';
 import cx from 'classnames';
 import PropTypes from 'prop-types';
-import React from 'react';
-import additionalProps from '../util/additionalProps';
+import { Group } from '@vx/group';
 
 HeatmapRect.propTypes = {
   data: PropTypes.array,
+  top: PropTypes.number,
+  left: PropTypes.number,
   binWidth: PropTypes.number,
   binHeight: PropTypes.number,
   x: PropTypes.number,
@@ -16,11 +17,14 @@ HeatmapRect.propTypes = {
   opacityScale: PropTypes.func,
   bins: PropTypes.func,
   count: PropTypes.func,
-  className: PropTypes.string
+  className: PropTypes.string,
+  children: PropTypes.func
 };
 
 export default function HeatmapRect({
   className,
+  top,
+  left,
   data,
   binWidth,
   binHeight,
@@ -28,42 +32,55 @@ export default function HeatmapRect({
   gap = 1,
   xScale,
   yScale,
-  colorScale,
+  colorScale = d => undefined,
   opacityScale = d => 1,
   bins = d => d.bins,
   count = d => d.count,
+  children,
   ...restProps
 }) {
   const width = binWidth - gap;
   const height = binHeight - gap;
+  const heatmap = data.map((datum, column) => {
+    const x0 = xScale(column);
+    return bins(datum).map((bin, row) => {
+      const countValue = count(bin);
+      return {
+        bin,
+        row,
+        column,
+        datum,
+        width,
+        height,
+        gap,
+        count: countValue,
+        x,
+        x0,
+        y: yScale(row) + gap,
+        color: colorScale(countValue),
+        opacity: opacityScale(countValue)
+      };
+    });
+  });
+  if (children) return children(heatmap);
   return (
-    <Group>
-      {data.map((d, i) => {
-        return (
-          <Group key={`heatmap-${i}`} className="vx-heatmap-column" left={xScale(i)}>
-            {bins(d).map((b, j) => {
-              return (
-                <rect
-                  key={`heatmap-tile-rect-${j}`}
-                  className={cx('vx-heatmap-rect', className)}
-                  fill={colorScale(count(b))}
-                  width={width}
-                  height={height}
-                  x={x}
-                  y={yScale(j) + gap}
-                  fillOpacity={opacityScale(count(b))}
-                  {...additionalProps(restProps, {
-                    bin: b,
-                    index: j,
-                    datum: d,
-                    datumIndex: i,
-                    data
-                  })}
-                />
-              );
-            })}
-          </Group>
-        );
+    <Group className="vx-heatmap-rects" top={top} left={left}>
+      {heatmap.map((_bins, column) => {
+        return _bins.map(bin => {
+          return (
+            <rect
+              key={`heatmap-tile-rect-${bin.row}-${bin.column}`}
+              className={cx('vx-heatmap-rect', className)}
+              width={bin.width}
+              height={bin.height}
+              x={bin.x + bin.x0}
+              y={bin.y}
+              fill={bin.color}
+              fillOpacity={bin.opacity}
+              {...restProps}
+            />
+          );
+        });
       })}
     </Group>
   );
