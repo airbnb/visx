b81e261275ca64aef54206c58b8d6392e55ba5ed
Harrison Shoff <hi@hshoff.com>
2019-01-03 12:06:38 -0500

    [grid][docs] add docs

--- packages/vx-grid/src/index.js
+++ packages/vx-grid/src/index.js
@@ -1,3 +1,3 @@
-export { default as GridRows } from './grids/Rows';
-export { default as GridColumns } from './grids/Columns';
+export { default as GridRows } from './grids/GridRows';
+export { default as GridColumns } from './grids/GridColumns';
 export { default as Grid } from './grids/Grid';
