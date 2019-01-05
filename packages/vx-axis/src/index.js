de810f5cf7d10c815c1aa8bd1c980a7e1ed03172
Chris Williams <chris_williams@airbnb.com>
2019-01-04 17:20:34 -0800

    [axis] add AxisRadial

--- packages/vx-axis/src/index.js
+++ packages/vx-axis/src/index.js
@@ -3,4 +3,5 @@ export { default as AxisLeft } from './axis/AxisLeft';
 export { default as AxisRight } from './axis/AxisRight';
 export { default as AxisTop } from './axis/AxisTop';
 export { default as AxisBottom } from './axis/AxisBottom';
+export { default as AxisRadial } from './axis/AxisRadial';
 export { default as Orientation } from './constants/orientation';
---
+++ packages/vx-axis/src/utils/polarToCartesian.js
@@ -0,0 +1,6 @@
+export default function polarToCartesian({ radius, angle }) {
+  return {
+      x: radius * Math.cos(angle),
+      y: radius * Math.sin(angle),
+  };
+}
