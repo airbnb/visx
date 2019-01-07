3ecf00afb3527b14d3475ff5d0648d0ce1150e02
Harry Shoff <harry@airbnb.com>
2019-01-07 12:12:17 -0500

    [mock data] add genPhyllotaxis

---
+++ packages/vx-mock-data/test/genPhyllotaxis.test.js
@@ -0,0 +1,28 @@
+import { genPhyllotaxis } from '../src';
+
+describe('generators/genPhyllotaxis', () => {
+  test('it should be defined', () => {
+    expect(genPhyllotaxis).toBeDefined();
+  });
+
+  test('it should return a function', () => {
+    const pointFn = genPhyllotaxis({
+      radius: 10,
+      width: 200,
+      height: 200
+    });
+    expect(typeof pointFn).toEqual('function');
+  });
+
+  test('it should return a point [x, y] when calling the returned function', () => {
+    const pointFn = genPhyllotaxis({
+      radius: 10,
+      width: 200,
+      height: 200
+    });
+    const point = pointFn(3);
+    const expected = { x: 110, y: 113 };
+    expect(Math.floor(point.x)).toEqual(expected.x);
+    expect(Math.floor(point.y)).toEqual(expected.y);
+  });
+});
