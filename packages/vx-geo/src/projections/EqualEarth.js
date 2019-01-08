d7918717deb6b0c140f751524f8983293811c108
Loren Riesenfeld <lorenries@users.noreply.github.com>
2019-01-08 18:03:44 -0500

    set projection to equalEarth for docs

--- packages/vx-geo/src/projections/EqualEarth.js
+++ packages/vx-geo/src/projections/EqualEarth.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Projection from './Projection';
 
 /**
- * All props pass through to `<Projection projection="naturalEarth" {...props} />`
+ * All props pass through to `<Projection projection="equalEarth" {...props} />`
  */
 export default function EqualEarth(props) {
   return <Projection projection="equalEarth" {...props} />;
