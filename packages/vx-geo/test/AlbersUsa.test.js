272cb483911f481a156d03836f496263dc32ad59
Loren Riesenfeld <lorenries@users.noreply.github.com>
2019-01-19 13:19:53 -0500

    [geo] add Albers USA projection

---
+++ packages/vx-geo/test/AlbersUsa.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { AlbersUsa } from '../src';
+
+describe('<AlbersUsa />', () => {
+  test('it should be defined', () => {
+    expect(AlbersUsa).toBeDefined();
+  });
+});
