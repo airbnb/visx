043df1be5ae2f38ffa119ef5c6c9b2458ca0d14e
Harry Shoff <harry@airbnb.com>
2019-03-14 11:33:00 -0400

    [geo] fix typo in test

--- packages/vx-geo/test/CustomProjection.test.js
+++ packages/vx-geo/test/CustomProjection.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { CustomProjection } from '../src';
 
-describe('<CustonProjection />', () => {
+describe('<CustomProjection />', () => {
   test('it should be defined', () => {
     expect(CustomProjection).toBeDefined();
   });
