c888ebaf4c8a03d70fb52589ca070aae3aa81baf
Harry Shoff <harry@airbnb.com>
2019-03-19 10:50:15 -0400

    [internal] attempt to fix travisci

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   collectCoverage: true,
+  coverageReporters: ['lcov', 'text'],
   coverageDirectory: '<rootDir>/coverage',
-  collectCoverageFrom: ['<rootDir>/packages/**/src/*.{js}', '!**/node_modules/**', '!**/dist/**'],
+  collectCoverageFrom: ['<rootDir>/packages/**/src/*.{js}', '!**/node_modules/**'],
   projects: ['<rootDir>/packages/**/*.{js}'],
   testPathIgnorePatterns: ['/node_modules/', '/dist/', '/build/', '/docs/'],
-  transform: {
-    '^.+\\.js?$': '<rootDir>/babel-jest.setup.js'
-  },
+  transform: { '^.+\\.js?$': '<rootDir>/babel-jest.setup.js' },
+  transformIgnorePatterns: ['/node_modules/', '/dist/', '/build/', '/docs/'],
   setupFilesAfterEnv: ['raf/polyfill', 'jest-enzyme'],
   testEnvironment: 'enzyme'
 };
