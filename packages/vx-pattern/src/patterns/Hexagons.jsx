f2f13b6b05db7236f78503306641132d3b07fa12
Chris Williams <chris_williams@airbnb.com>
2019-08-23 17:48:01 -0700

    prettier(vx-pattern)

--- packages/vx-pattern/src/patterns/Hexagons.jsx
+++ packages/vx-pattern/src/patterns/Hexagons.jsx
@@ -36,7 +36,8 @@ export default function PatternHexagons({
       className={cx('vx-pattern-hexagon', className)}
       path={`M ${height},0 l ${height},0 l ${height / 2},${(height * sqrtSize) / 2} l ${-height /
         2},${(height * sqrtSize) / 2} l ${-height},0 l ${-height / 2},${(-height * sqrtSize) /
-        2} Z M 0,${(height * sqrtSize) / 2} l ${height / 2},0 M ${3 * height},${(height * sqrtSize) /
+        2} Z M 0,${(height * sqrtSize) / 2} l ${height / 2},0 M ${3 * height},${(height *
+        sqrtSize) /
         2} l ${-height / 2},0`}
       id={id}
       width={size}
