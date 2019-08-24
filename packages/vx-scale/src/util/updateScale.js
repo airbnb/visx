1e212009c84e2eb34aa8722cd2e09d50add32f46
Chris Williams <chris_williams@airbnb.com>
2019-08-23 17:38:38 -0700

    fix(vx-text): allow any child

--- packages/vx-scale/src/util/updateScale.js
+++ packages/vx-scale/src/util/updateScale.js
@@ -1,6 +1,6 @@
 const has = Object.prototype.hasOwnProperty;
 
-export default function updateScale(scale, { ...args }) {
+export default function updateScale(scale, { ...args } = {}) {
   const nextScale = scale.copy();
   Object.keys(args).forEach(key => {
     if (has.call(nextScale, key)) nextScale[key](args[key]);
