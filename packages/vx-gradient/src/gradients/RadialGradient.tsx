f17c544f79c64e43b432b717a99b501889d35eb7
Chris Williams <chris_williams@airbnb.com>
2019-09-17 15:42:25 -0700

    typescript(vx-gradient/RadialGradient): re-use props from LinearGradient where possible

--- packages/vx-gradient/src/gradients/RadialGradient.tsx
+++ packages/vx-gradient/src/gradients/RadialGradient.tsx
@@ -1,30 +1,20 @@
 import React from 'react';
+import { LinearGradientProps } from './LinearGradient';
 
-type RadialGradientElementProps = React.SVGProps<SVGRadialGradientElement>;
-type StopElementProps = React.SVGProps<SVGStopElement>;
-
-export type RadialGradientProps = {
-  /** Unique id for the gradient. Should be unique across all page elements. */
-  id: string;
-  /** Start color of gradient (inside). */
-  from?: StopElementProps['stopColor'];
-  /** End color of gradient (outside). */
-  to?: StopElementProps['stopColor'];
-  /** Number or percent defining the where the 'from' starting color is placed along the gradient. */
-  fromOffset?: StopElementProps['offset'];
-  /** Opacity of the 'from' starting color. */
-  fromOpacity?: StopElementProps['stopOpacity'];
-  /** Number or percent defining the where the 'to' ending color is placed along the gradient. */
-  toOffset?: StopElementProps['offset'];
-  /** Opacity of the 'to' ending color. */
-  toOpacity?: StopElementProps['stopOpacity'];
-  /** Rotation to apply to gradient. */
-  rotate?: string | number;
-  /** Transform to apply to linearGradient, overrides rotate. */
-  transform?: RadialGradientElementProps['gradientTransform'];
-  /** Override of radialGradient children. */
-  children?: React.ReactNode;
-} & RadialGradientElementProps; // passed as rest props to radialGradient
+export type RadialGradientProps = Pick<
+  LinearGradientProps,
+  | 'id'
+  | 'from'
+  | 'to'
+  | 'fromOffset'
+  | 'fromOpacity'
+  | 'toOffset'
+  | 'toOpacity'
+  | 'rotate'
+  | 'transform'
+  | 'children'
+> &
+  React.SVGProps<SVGRadialGradientElement>; // passed as rest props to radialGradient
 
 export default function RadialGradient({
   children,
