081e50e444a1f5f1cb89e6e3b106ddeaf5988bfd
Chris Williams <chris_williams@airbnb.com>
2019-09-17 18:21:25 -0700

    typescript(vx-drag): use generic in raise util

--- packages/vx-drag/src/util/raise.ts
+++ packages/vx-drag/src/util/raise.ts
@@ -1,5 +1,5 @@
 /** Given at an array of items, moves the item at the specified index to the end of the array. */
-export default function raise(items: unknown[], raiseIndex: number) {
+export default function raise<T>(items: T[], raiseIndex: number) {
   const array = [...items];
   const lastIndex = array.length - 1;
   const [raiseItem] = array.splice(raiseIndex, 1);
