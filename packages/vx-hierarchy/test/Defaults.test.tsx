2a2ca35f3c70a7bccddb088a35ce2f176a49c6fd
Chris Williams <chris_williams@airbnb.com>
2019-09-27 15:16:17 -0700

    test(vx-hierarchy): add more tests for new exports

--- packages/vx-hierarchy/test/Defaults.test.tsx
+++ packages/vx-hierarchy/test/Defaults.test.tsx
@@ -1,19 +1,54 @@
-import { HierarchyDefaultLink, HierarchyDefaultNode, HierarchyDefaultRectNode } from '../src';
+import {
+  HierarchyDefaultLink,
+  HierarchyDefaultNode,
+  HierarchyDefaultRectNode,
+  hierarchy,
+  treemapBinary,
+  treemapDice,
+  treemapResquarify,
+  treemapSlice,
+  treemapSliceDice,
+  treemapSquarify,
+} from '../src';
+import { isTSAnyKeyword } from '@babel/types';
 
 describe('<DefaultLink />', () => {
-  test('it should be defined', () => {
+  it('should be defined', () => {
     expect(HierarchyDefaultLink).toBeDefined();
   });
 });
 
 describe('<DefaultNode />', () => {
-  test('it should be defined', () => {
+  it('should be defined', () => {
     expect(HierarchyDefaultNode).toBeDefined();
   });
 });
 
 describe('<DefaultRectNode />', () => {
-  test('it should be defined', () => {
+  it('should be defined', () => {
     expect(HierarchyDefaultRectNode).toBeDefined();
+  });
+});
+
+describe('d3 exports', () => {
+  it('should export hierarchy', () => {
+    expect(hierarchy).toBeDefined();
+  });
+
+  it('should export treemap tiling functions', () => {
+    const tilers = [
+      treemapBinary,
+      treemapDice,
+      treemapResquarify,
+      treemapSlice,
+      treemapSliceDice,
+      treemapSquarify,
+    ];
+
+    expect.assertions(tilers.length);
+
+    tilers.forEach(tiler => {
+      expect(tiler).toBeDefined();
+    });
   });
 });
