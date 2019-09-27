54935a30f21004c035d0e0c18754e534bf73481a
Chris Williams <chris_williams@airbnb.com>
2019-09-27 15:52:47 -0700

    new(vx-demo): update treemap tile + pages with tile method select

--- packages/vx-demo/src/components/tiles/treemap.js
+++ packages/vx-demo/src/components/tiles/treemap.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import { Group } from '@vx/group';
-import { Treemap } from '@vx/hierarchy';
-import { hierarchy, stratify, treemapSquarify } from 'd3-hierarchy';
+import {
+  Treemap,
+  hierarchy,
+  stratify,
+  treemapSquarify,
+  treemapBinary,
+  treemapDice,
+  treemapResquarify,
+  treemapSlice,
+  treemapSliceDice,
+} from '@vx/hierarchy';
 import { shakespeare } from '@vx/mock-data';
 
 import { scaleLinear } from '@vx/scale';
@@ -20,59 +29,100 @@ const data = stratify()
   .parentId(d => d.parent)(shakespeare)
   .sum(d => d.size || 0);
 
-export default ({
-  width,
-  height,
-  // events = false,
-  margin = {
-    top: 0,
-    left: 30,
-    right: 40,
-    bottom: 80,
-  },
-}) => {
-  if (width < 10) return null;
+const tileMethods = {
+  treemapSquarify,
+  treemapBinary,
+  treemapDice,
+  treemapResquarify,
+  treemapSlice,
+  treemapSliceDice,
+};
 
-  const yMax = height - margin.top - margin.bottom;
-  const root = hierarchy(data).sort((a, b) => b.value - a.value);
+export default class extends React.Component {
+  state = {
+    tileMethod: 'treemapSquarify',
+  };
 
-  return (
-    <svg width={width} height={height}>
-      <rect width={width} height={height} rx={14} fill={bg} />
-      <Treemap top={margin.top} root={root} size={[width, yMax]} tile={treemapSquarify} round>
-        {treemap => {
-          const nodes = treemap.descendants().reverse();
-          return (
-            <Group>
-              {nodes.map((node, i) => {
-                const nodeWidth = node.x1 - node.x0;
-                const nodeHeight = node.y1 - node.y0;
+  render() {
+    const {
+      width,
+      height,
+      // events = false,
+      margin = {
+        top: 0,
+        left: 30,
+        right: 40,
+        bottom: 80,
+      },
+    } = this.props;
+
+    const { tileMethod } = this.state;
+
+    if (width < 10) return null;
+
+    const yMax = height - margin.top - margin.bottom;
+    const root = hierarchy(data).sort((a, b) => b.value - a.value);
+
+    return (
+      <div>
+        <label>tile method</label>{' '}
+        <select
+          onClick={e => e.stopPropagation()}
+          onChange={e => this.setState({ tileMethod: e.target.value })}
+          value={tileMethod}
+        >
+          {Object.keys(tileMethods).map(tile => (
+            <option key={tile} value={tile}>
+              {tile}
+            </option>
+          ))}
+        </select>
+        <div>
+          <svg width={width} height={height}>
+            <rect width={width} height={height} rx={14} fill={bg} />
+            <Treemap
+              top={margin.top}
+              root={root}
+              size={[width, yMax]}
+              tile={tileMethods[tileMethod]}
+              round
+            >
+              {treemap => {
+                const nodes = treemap.descendants().reverse();
                 return (
-                  <Group key={`node-${i}`} top={node.y0} left={node.x0}>
-                    {node.depth === 1 && (
-                      <rect
-                        width={nodeWidth}
-                        height={nodeHeight}
-                        stroke={bg}
-                        strokeWidth={4}
-                        fill="transparent"
-                      />
-                    )}
-                    {node.depth > 2 && (
-                      <rect
-                        width={nodeWidth}
-                        height={nodeHeight}
-                        stroke={bg}
-                        fill={colorScale(node.value)}
-                      />
-                    )}
+                  <Group>
+                    {nodes.map((node, i) => {
+                      const nodeWidth = node.x1 - node.x0;
+                      const nodeHeight = node.y1 - node.y0;
+                      return (
+                        <Group key={`node-${i}`} top={node.y0} left={node.x0}>
+                          {node.depth === 1 && (
+                            <rect
+                              width={nodeWidth}
+                              height={nodeHeight}
+                              stroke={bg}
+                              strokeWidth={4}
+                              fill="transparent"
+                            />
+                          )}
+                          {node.depth > 2 && (
+                            <rect
+                              width={nodeWidth}
+                              height={nodeHeight}
+                              stroke={bg}
+                              fill={colorScale(node.value)}
+                            />
+                          )}
+                        </Group>
+                      );
+                    })}
                   </Group>
                 );
-              })}
-            </Group>
-          );
-        }}
-      </Treemap>
-    </svg>
-  );
-};
+              }}
+            </Treemap>
+          </svg>
+        </div>
+      </div>
+    );
+  }
+}
