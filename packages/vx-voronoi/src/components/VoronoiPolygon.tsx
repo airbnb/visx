06f338e3ce4f10c332f89957ff697f8062c69914
Chris Williams <chris_williams@airbnb.com>
2019-10-02 13:07:13 -0700

    typescript(vx-voronoi): number[][] => [number, number][]

--- packages/vx-voronoi/src/components/VoronoiPolygon.tsx
+++ packages/vx-voronoi/src/components/VoronoiPolygon.tsx
@@ -3,11 +3,11 @@ import cx from 'classnames';
 
 export type VoronoiPolygonProps = {
   /** Override render function which is provided polygon and generated path. */
-  children?: ({ path, polygon }: { path: string; polygon: number[][] }) => React.ReactNode;
+  children?: ({ path, polygon }: { path: string; polygon: [number, number][] }) => React.ReactNode;
   /** className to apply to path element. */
   className?: string;
   /** Array of coordinate arrays for the polygon (e.g., [[x,y], [x1,y1], ...]), used to generate polygon path. */
-  polygon?: number[][];
+  polygon?: [number, number][];
 };
 
 export default function VoronoiPolygon({
