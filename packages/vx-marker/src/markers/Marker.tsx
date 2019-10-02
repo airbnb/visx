ae2df3f99b1f70253cf3239983a8b8d14cf1b3ad
Chris Williams <chris_williams@airbnb.com>
2019-10-01 17:50:07 -0700

    typescript(vx-marker): re-write packages in TypeScript

--- packages/vx-marker/src/markers/Marker.tsx
+++ packages/vx-marker/src/markers/Marker.tsx
@@ -1,28 +1,51 @@
 import React from 'react';
 import cx from 'classnames';
-import PropTypes from 'prop-types';
 import { Line } from '@vx/shape';
 import { Group } from '@vx/group';
 
-Marker.propTypes = {
-  top: PropTypes.number,
-  left: PropTypes.number,
-  from: PropTypes.object,
-  to: PropTypes.object,
-  stroke: PropTypes.string,
-  strokeWidth: PropTypes.number,
-  strokeDasharray: PropTypes.string,
-  transform: PropTypes.string,
-  label: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
-  labelAnchor: PropTypes.string,
-  labelDx: PropTypes.number,
-  labelDy: PropTypes.number,
-  labelFill: PropTypes.string,
-  labelStroke: PropTypes.string,
-  labelStrokeWidth: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  labelFontSize: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  labelPaintOrder: PropTypes.string,
-  className: PropTypes.string,
+export type MarkerProps = {
+  /** Top offset applied to container g element. */
+  top?: number;
+  /** Left offset applied to container g element. */
+  left?: number;
+  /** x,y coordinates of the marker line start point. If a label is passed its origin is set to this position. */
+  from?: {
+    x?: number;
+    y?: number;
+  };
+  /** x,y coordinates of the marker line end point. */
+  to?: {
+    x?: number;
+    y?: number;
+  };
+  /** stroke color of the marker line. */
+  stroke?: string;
+  /** strokeWidth of the marker line. */
+  strokeWidth?: number;
+  /** strokeDasharray attribute of the marker line. */
+  strokeDasharray?: string;
+  /** transform attribute applied to the marker line. */
+  transform?: string;
+  /** Label with origin at the specified `from` cordinates. */
+  label?: React.ReactNode;
+  /** textAnchor attribute applied to label text element. */
+  labelAnchor?: string;
+  /** dx attribute applied to label text element. */
+  labelDx?: string | number;
+  /** dy attribute applied to label text element. */
+  labelDy?: string | number;
+  /** fill color applied to label text element. */
+  labelFill?: string;
+  /** stroke color applied to label text element. */
+  labelStroke?: string;
+  /** strokeWidth attribute applied to label text element. */
+  labelStrokeWidth?: string | number;
+  /** fontSize attribute applied to label text element. */
+  labelFontSize?: string | number;
+  /** paintOrder attribute applied to label text element. */
+  labelPaintOrder?: string;
+  /** className to apply to line element. */
+  className?: string;
 };
 
 export default function Marker({
@@ -44,7 +67,7 @@ export default function Marker({
   labelStrokeWidth = 3,
   labelPaintOrder = 'stroke',
   className,
-}) {
+}: MarkerProps) {
   return (
     <Group top={top} left={left}>
       <Line
@@ -58,8 +81,8 @@ export default function Marker({
       />
       {label && (
         <text
-          x={from.x}
-          y={from.y}
+          x={from ? from.x : 0}
+          y={from ? from.y : 0}
           dx={labelDx}
           dy={labelDy}
           fontSize={labelFontSize}
