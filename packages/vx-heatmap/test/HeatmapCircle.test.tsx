50729299a730d98227cebfe0c8af47a00feb1dcd
Chris Williams <chris_williams@airbnb.com>
2019-10-04 14:07:28 -0700

    typescript(vx-heatmap): annotate types for mock data

--- packages/vx-heatmap/test/HeatmapCircle.test.tsx
+++ packages/vx-heatmap/test/HeatmapCircle.test.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import { HeatmapCircle } from '../src';
+// @ts-ignore
 import { genBins } from '../../vx-mock-data/src';
 
-const data = genBins(1, 1);
+const data: {
+  bin: number;
+  bins: { bin: number; count: number }[];
+}[] = genBins(1, 1);
+
 const xScale = () => 50;
 const yScale = () => 50;
 const HeatmapWrapper = (props = {}) =>
