f51e7bd9ed60d132c2314792e4e409a1627f70a5
Chris Williams <chris_williams@airbnb.com>
2019-10-09 13:19:04 -0700

    typescript(vx-geo): remove @ts-ignores

--- packages/vx-geo/src/projections/EqualEarth.tsx
+++ packages/vx-geo/src/projections/EqualEarth.tsx
@@ -4,7 +4,6 @@ import Projection, { ProjectionProps, GeoPermissibleObjects } from './Projection
 /**
  * All props pass through to `<Projection projection="equalEarth" {...props} />`
  */
-// @ts-ignore ts-migrate(2304) FIXME: Cannot find name '$TSFixMe'.
 export default function EqualEarth<Datum extends GeoPermissibleObjects>(
   props: ProjectionProps<Datum>,
 ) {
--- packages/vx-geo/src/projections/NaturalEarth.tsx
+++ packages/vx-geo/src/projections/NaturalEarth.tsx
@@ -4,7 +4,6 @@ import Projection, { ProjectionProps, GeoPermissibleObjects } from './Projection
 /**
  * All props pass through to `<Projection projection="naturalEarth" {...props} />`
  */
-// @ts-ignore ts-migrate(2304) FIXME: Cannot find name '$TSFixMe'.
 export default function NaturalEarth<Datum extends GeoPermissibleObjects>(
   props: ProjectionProps<Datum>,
 ) {
