88d1247132fdf5e1edeb73d8155448221ddc7f8e
geekplux <geekplux@gmail.com>
2019-10-22 17:23:15 +0800

    chore(vx-demo): add brush demo

---
+++ packages/vx-demo/src/components/tiles/brush.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function BrushChart() {
+  return <div>brush</div>;
+}
+
+export default BrushChart;
---
+++ packages/vx-demo/src/pages/brush.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Show from '../components/show';
+import BrushChart from '../components/tiles/brush';
+
+export default () => {
+  return (
+    <Show component={BrushChart} title="BrushChart">
+      {`import React from 'react';`}
+    </Show>
+  );
+};
