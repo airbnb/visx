4c0c2fac01fe3e19ce355a4d667abd761a42e6f5
Jens Ochsenmeier <github@jens-ox.de>
2020-01-02 17:24:12 +0100

    fix(vx-demo): update chord diagram example to make more sense

--- packages/vx-demo/src/components/tiles/chord.js
+++ packages/vx-demo/src/components/tiles/chord.js
@@ -3,16 +3,7 @@ import { Arc } from '@vx/shape';
 import { Group } from '@vx/group';
 import { Chord, Ribbon } from '@vx/chord';
 import { scaleOrdinal } from '@vx/scale';
-import { LinearGradient } from '@vx/gradient';
 
-const pink = '#ff2fab';
-const orange = '#ffc62e';
-const purple = '#dc04ff';
-const purple2 = '#7324ff';
-const red = '#d04376';
-const green = '#52f091';
-const blue = '#04a6ff';
-const lime = '#00ddc6';
 const bg = '#e4e3d8';
 
 const matrix = [
@@ -28,7 +19,7 @@ function descending(a, b) {
 
 const color = scaleOrdinal({
   domain: [1, 2, 3, 4],
-  range: ['url(#gpinkorange)', 'url(#gpurplered)', 'url(#gpurplegreen)', 'url(#gbluelime)'],
+  range: ['#1b9e77', '#d95f02', '#7570b3', '#e7298a', '#66a61e', '#e6ab02', '#a6761d', '#666666'],
 });
 
 export default ({ width, height, centerSize = 20, events = false }) => {
@@ -40,10 +31,6 @@ export default ({ width, height, centerSize = 20, events = false }) => {
   return (
     <div className="Chords">
       <svg width={width} height={height}>
-        <LinearGradient id="gpinkorange" from={pink} to={orange} vertical={false} />
-        <LinearGradient id="gpurplered" from={purple} to={red} vertical={false} />
-        <LinearGradient id="gpurplegreen" from={purple2} to={green} vertical={false} />
-        <LinearGradient id="gbluelime" from={blue} to={lime} vertical={false} />
         <rect width={width} height={height} fill={bg} rx={14} />
         <Group top={height / 2} left={width / 2}>
           <Chord matrix={matrix} padAngle={0.05} sortSubgroups={descending}>
@@ -71,7 +58,7 @@ export default ({ width, height, centerSize = 20, events = false }) => {
                         key={`ribbon-${i}`}
                         chord={chord}
                         radius={innerRadius}
-                        fill={color(i)}
+                        fill={color(chord.target.index)}
                         fillOpacity={0.75}
                         onClick={() => {
                           alert(`${JSON.stringify(chord)}`);
--- packages/vx-demo/src/pages/chord.js
+++ packages/vx-demo/src/pages/chord.js
@@ -19,23 +19,14 @@ import { Arc } from '@vx/shape';
 import { Group } from '@vx/group';
 import { Chord, Ribbon } from '@vx/chord';
 import { scaleOrdinal } from '@vx/scale';
-import { LinearGradient } from '@vx/gradient';
 
-const pink = '#ff2fab';
-const orange = '#ffc62e';
-const purple = '#dc04ff';
-const purple2 = '#7324ff';
-const red = '#d04376';
-const green = '#52f091';
-const blue = '#04a6ff';
-const lime = '#00ddc6';
 const bg = '#e4e3d8';
 
 const matrix = [
   [11975, 5871, 8916, 2868],
   [1951, 10048, 2060, 6171],
   [8010, 16145, 8090, 8045],
-  [1013, 990, 940, 6907]
+  [1013, 990, 940, 6907],
 ];
 
 function descending(a, b) {
@@ -44,20 +35,18 @@ function descending(a, b) {
 
 const color = scaleOrdinal({
   domain: [1, 2, 3, 4],
-  range: ['url(#gpinkorange)', 'url(#gpurplered)', 'url(#gpurplegreen)', 'url(#gbluelime)']
+  range: ['#1b9e77', '#d95f02', '#7570b3', '#e7298a', '#66a61e', '#e6ab02', '#a6761d', '#666666'],
 });
 
-export default ({ width, height, centerSize = 20 }) => {
+export default ({ width, height, centerSize = 20, events = false }) => {
+  if (width < 10) return null;
+
   const outerRadius = Math.min(width, height) * 0.5 - (centerSize + 10);
   const innerRadius = outerRadius - centerSize;
 
   return (
     <div className="Chords">
       <svg width={width} height={height}>
-        <LinearGradient id="gpinkorange" from={pink} to={orange} vertical={false} />
-        <LinearGradient id="gpurplered" from={purple} to={red} vertical={false} />
-        <LinearGradient id="gpurplegreen" from={purple2} to={green} vertical={false} />
-        <LinearGradient id="gbluelime" from={blue} to={lime} vertical={false} />
         <rect width={width} height={height} fill={bg} rx={14} />
         <Group top={height / 2} left={width / 2}>
           <Chord matrix={matrix} padAngle={0.05} sortSubgroups={descending}>
@@ -72,7 +61,8 @@ export default ({ width, height, centerSize = 20 }) => {
                         innerRadius={innerRadius}
                         outerRadius={outerRadius}
                         fill={color(i)}
-                        onClick={event => {
+                        onClick={() => {
+                          if (!events) return;
                           alert(\`\${JSON.stringify(group)}\`);
                         }}
                       />
@@ -84,10 +74,10 @@ export default ({ width, height, centerSize = 20 }) => {
                         key={\`ribbon-\${i}\`}
                         chord={chord}
                         radius={innerRadius}
-                        fill={color(i)}
+                        fill={color(chord.target.index)}
                         fillOpacity={0.75}
-                        onClick={event => {
-                          alert(\`\${JSON.stringify(chord)}\`);
+                        onClick={() => {
+                          alert(\'\${JSON.stringify(chord)}\`);
                         }}
                       />
                     );
@@ -99,8 +89,6 @@ export default ({ width, height, centerSize = 20 }) => {
         </Group>
       </svg>
     </div>
-  );
-};
 `}
     </Show>
   );
