4d948618782efdc75a81017dfb6c5b035a7caa62
Chris Williams <chris_williams@airbnb.com>
2020-01-07 10:46:03 -0800

    typescript(vx-scale): allow any ordinal Input that's coercible to a string

--- packages/vx-scale/src/scales/ordinal.ts
+++ packages/vx-scale/src/scales/ordinal.ts
@@ -1,16 +1,20 @@
 import { scaleOrdinal } from 'd3-scale';
 
-export type OrdinalConfig<Output> = {
+export type OrdinalConfig<Input, Output> = {
   /** Sets the input values of the scale, which are strings for an ordinal scale. */
-  domain?: string[];
+  domain?: Input[];
   /** Sets the output values of the scale. */
   range?: Output[];
   /** Sets the output value of the scale for unknown input values. */
   unknown?: Output | { name: 'implicit' };
 };
 
-export default function ordinalScale<Output>({ range, domain, unknown }: OrdinalConfig<Output>) {
-  const scale = scaleOrdinal<Output>();
+export default function ordinalScale<Input extends { toString(): string }, Output>({
+  range,
+  domain,
+  unknown,
+}: OrdinalConfig<Input, Output>) {
+  const scale = scaleOrdinal<Input, Output>();
 
   if (range) scale.range(range);
   if (domain) scale.domain(domain);
