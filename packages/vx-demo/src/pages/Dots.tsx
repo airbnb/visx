1e5130d5d9839fa84555f395bc2f332be5fcb94d
Chris Williams <chris_williams@airbnb.com>
2020-01-09 11:44:06 -0800

    typescript(vx-demo): Update Dots example page code

--- packages/vx-demo/src/pages/Dots.tsx
+++ packages/vx-demo/src/pages/Dots.tsx
@@ -10,99 +10,110 @@ import { Group } from '@vx/group';
 import { Circle } from '@vx/shape';
 import { GradientPinkRed } from '@vx/gradient';
 import { scaleLinear } from '@vx/scale';
-import { genRandomNormalPoints } from '@vx/mock-data';
+import genRandomNormalPoints, {
+  PointsRange,
+} from '@vx/mock-data/lib/generators/genRandomNormalPoints';
 import { withTooltip, Tooltip } from '@vx/tooltip';
 
-const points = genRandomNormalPoints(600).filter((d, i) => {
-  return i < 600;
-});
+import { WithTooltipProvidedProps } from '@vx/tooltip/lib/enhancers/withTooltip';
+import { ShowProvidedProps } from '../../types';
 
-const x = d => d[0];
-const y = d => d[1];
-const z = d => d[2];
+const points: PointsRange[] = genRandomNormalPoints(600).filter((d, i) => i < 600);
 
-let tooltipTimeout;
+const x = (d: PointsRange) => d[0];
+const y = (d: PointsRange) => d[1];
 
-export default withTooltip(props => {
-  const { width, height } = props;
+let tooltipTimeout: number;
 
-  const xMax = width;
-  const yMax = height - 80;
+export default withTooltip<ShowProvidedProps, PointsRange>(
+  ({
+    width,
+    height,
+    hideTooltip,
+    showTooltip,
+    tooltipOpen,
+    tooltipData,
+    tooltipLeft,
+    tooltipTop,
+  }: ShowProvidedProps & WithTooltipProvidedProps<PointsRange>) => {
+    const xMax = width;
+    const yMax = height - 80;
+    if (width < 10) return null;
 
-  const xScale = scaleLinear({
-    domain: [1.3, 2.2],
-    range: [0, xMax],
-    clamp: true
-  });
-  const yScale = scaleLinear({
-    domain: [0.75, 1.6],
-    range: [yMax, 0],
-    clamp: true
-  });
+    const xScale = scaleLinear<number>({
+      domain: [1.3, 2.2],
+      range: [0, xMax],
+      clamp: true,
+    });
+    const yScale = scaleLinear<number>({
+      domain: [0.75, 1.6],
+      range: [yMax, 0],
+      clamp: true,
+    });
 
-  return (
-    <div>
-      <svg width={width} height={height}>
-        <GradientPinkRed id="pink" />
-        <rect width={width} height={height} rx={14} fill={"url(#pink)"} />
-        <Group
-          onTouchStart={event => {
-            if (tooltipTimeout) clearTimeout(tooltipTimeout);
-            props.hideTooltip();
-          }}
-        >
-          {points.map((point, i) => {
-            const cx = xScale(x(point));
-            const cy = yScale(y(point));
-            const r = i % 3 === 0 ? 2 : 2.765;
-            return (
-              <Circle
-                key={\`point-\${point.x}-\${i}\`}
-                className="dot"
-                cx={cx}
-                cy={cy}
-                r={r}
-                fill="#f6c431"
-                onMouseEnter={event => {
-                  if (tooltipTimeout) clearTimeout(tooltipTimeout);
-                  props.showTooltip({
-                    tooltipLeft: cx,
-                    tooltipTop: cy + 20,
-                    tooltipData: point
-                  });
-                }}
-                onMouseLeave={event => {
-                  tooltipTimeout = setTimeout(() => {
-                    props.hideTooltip();
-                  }, 300);
-                }}
-                onTouchStart={event => {
-                  if (tooltipTimeout) clearTimeout(tooltipTimeout);
-                  props.showTooltip({
-                    tooltipLeft: cx,
-                    tooltipTop: cy - 30,
-                    tooltipData: point
-                  });
-                }}
-              />
-            );
-          })}
-        </Group>
-      </svg>
-      {props.tooltipOpen && (
-        <Tooltip left={props.tooltipLeft} top={props.tooltipTop}>
-          <div>
-            <strong>x:</strong> {x(props.tooltipData)}
-          </div>
-          <div>
-            <strong>y:</strong> {y(props.tooltipData)}
-          </div>
-        </Tooltip>
-      )}
-    </div>
-  );
-});
-`}
+    return (
+      <div>
+        <svg width={width} height={height}>
+          <GradientPinkRed id="pink" />
+          <rect width={width} height={height} rx={14} fill="url(#pink)" />
+          <Group
+            onTouchStart={() => {
+              if (tooltipTimeout) clearTimeout(tooltipTimeout);
+              hideTooltip();
+            }}
+          >
+            {points.map((point, i) => {
+              const cx = xScale(x(point));
+              const cy = yScale(y(point));
+              const r = i % 3 === 0 ? 2 : 3;
+              return (
+                <Circle
+                  key={\`point-\${point[0]}-\${i}\`}
+                  className="dot"
+                  cx={cx}
+                  cy={cy}
+                  r={r}
+                  fill="#f6c431"
+                  onMouseEnter={() => {
+                    if (tooltipTimeout) clearTimeout(tooltipTimeout);
+                    showTooltip({
+                      tooltipLeft: cx,
+                      tooltipTop: cy + 20,
+                      tooltipData: point,
+                    });
+                  }}
+                  onMouseLeave={() => {
+                    tooltipTimeout = window.setTimeout(() => {
+                      hideTooltip();
+                    }, 300);
+                  }}
+                  onTouchStart={() => {
+                    if (tooltipTimeout) clearTimeout(tooltipTimeout);
+                    showTooltip({
+                      tooltipLeft: cx,
+                      tooltipTop: cy - 30,
+                      tooltipData: point,
+                    });
+                  }}
+                />
+              );
+            })}
+          </Group>
+        </svg>
+        {tooltipOpen && tooltipData && (
+          <Tooltip left={tooltipLeft} top={tooltipTop}>
+            <div>
+              <strong>x:</strong> {x(tooltipData)}
+            </div>
+            <div>
+              <strong>y:</strong> {y(tooltipData)}
+            </div>
+          </Tooltip>
+        )}
+      </div>
+    );
+  },
+);`}
     </Show>
   );
 };
