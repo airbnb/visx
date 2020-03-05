eb56f92e2c35318e69472a81b95e09b34a7ba884
Chris Williams <chris.williams@airbnb.com>
2020-03-04 17:09:59 -0800

    fix(demo): remove codemirror-webpack-loader from next config

--- packages/vx-demo/src/next.config.js
+++ packages/vx-demo/src/next.config.js
@@ -1,22 +1,14 @@
 /* eslint-disable no-undef */
-const path = require('path');
-
 module.exports = {
   typescript: {
     ignoreDevErrors: true,
     ignoreBuildErrors: true,
   },
   webpack: config => {
-    config.module.rules.push(
-      {
-        test: /\.scss$/,
-        loader: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /code/,
-        loader: path.resolve(__dirname, 'codemirror-webpack-loader.js'),
-      },
-    );
+    config.module.rules.push({
+      test: /\.scss$/,
+      loader: ['style-loader', 'css-loader'],
+    });
     return config;
   },
 };
