5782bdd1d2982bae580604ba5e56a5a20141ceee
Chris Williams <chris.williams@airbnb.com>
2020-03-09 11:39:46 -0700

    fix(demo/Gradients): refactor to simplify + fix bugs

--- packages/vx-demo/src/docs-v2/examples/vx-gradient/Example.tsx
+++ packages/vx-demo/src/docs-v2/examples/vx-gradient/Example.tsx
@@ -13,114 +13,57 @@ import {
   RadialGradient,
 } from '@vx/gradient';
 
-const margin = {
+const defaultMargin = {
   top: 0,
   left: 0,
   right: 0,
-  bottom: 80,
+  bottom: 0,
 };
+
+const numColumns = 5;
+
+const gradientIds = [
+  [...new Array(numColumns)].map((_, idx) => `vx-gradient-demo-row-0-column-${idx}`),
+  [...new Array(numColumns)].map((_, idx) => `vx-gradient-demo-row-1-column-${idx}`),
+];
 
 type Props = {
   width: number;
   height: number;
+  margin?: typeof defaultMargin;
 };
 
-export default function Example({ width, height }: Props) {
-  const barWidth = Math.max(width / 4, 0);
-  const barHeight = Math.max((height - margin.bottom) / 2, 0);
+export default function Example({ width, height, margin = defaultMargin }: Props) {
+  const columnWidth = Math.max(width / gradientIds[0].length, 0);
+  const rowHeight = Math.max((height - margin.bottom) / gradientIds.length, 0);
 
   return (
     <svg width={width} height={height}>
-      <GradientDarkgreenGreen id="DarkgreenGreen" />
-      <GradientLightgreenGreen id="LightgreenGreen" />
-      <GradientOrangeRed id="OrangeRed" />
-      <GradientPinkBlue id="PinkBlue" />
-      <GradientPinkRed id="PinkRed" vertical={false} />
-      <GradientPurpleOrange id="PurpleOrange" vertical={false} />
-      <GradientPurpleRed id="PurpleRed" vertical={false} />
-      <RadialGradient from="#55bdd5" to="#4f3681" id="Radial" r="80%" />
-      <GradientSteelPurple id="SteelPurple" vertical={false} />
-      <GradientTealBlue id="TealBlue" vertical={false} />
-      <Bar
-        x={0}
-        y={0}
-        width={barWidth}
-        height={barHeight}
-        fill="url(#LightgreenGreen)"
-        stroke="#ffffff"
-        strokeWidth={8}
-        rx={14}
-      />
-      <Bar
-        x={barWidth}
-        y={0}
-        width={barWidth}
-        height={barHeight}
-        fill="url(#OrangeRed)"
-        rx={14}
-        stroke="#ffffff"
-        strokeWidth={8}
-      />
-      <Bar
-        x={barWidth * 2}
-        y={0}
-        width={barWidth}
-        height={barHeight}
-        fill="url(#PinkBlue)"
-        rx={14}
-        stroke="#ffffff"
-        strokeWidth={8}
-      />
-      <Bar
-        x={barWidth * 3}
-        y={0}
-        width={barWidth}
-        height={barHeight}
-        fill="url(#DarkgreenGreen)"
-        rx={14}
-        stroke="#ffffff"
-        strokeWidth={8}
-      />
-      <Bar
-        x={0}
-        y={barHeight}
-        width={barWidth}
-        height={barHeight}
-        fill="url(#PinkRed)"
-        rx={14}
-        stroke="#ffffff"
-        strokeWidth={8}
-      />
-      <Bar
-        x={barWidth}
-        y={barHeight}
-        width={barWidth}
-        height={barHeight}
-        fill="url(#TealBlue)"
-        rx={14}
-        stroke="#ffffff"
-        strokeWidth={8}
-      />
-      <Bar
-        x={barWidth * 2}
-        y={barHeight}
-        width={barWidth}
-        height={barHeight}
-        fill="url(#PurpleOrange)"
-        rx={14}
-        stroke="#ffffff"
-        strokeWidth={8}
-      />
-      <Bar
-        x={barWidth * 3}
-        y={barHeight}
-        width={barWidth}
-        height={barHeight}
-        fill="url(#Radial)"
-        rx={14}
-        stroke="#ffffff"
-        strokeWidth={8}
-      />
+      <GradientDarkgreenGreen id={gradientIds[0][0]} />
+      <GradientLightgreenGreen id={gradientIds[0][1]} />
+      <GradientOrangeRed id={gradientIds[0][2]} />
+      <GradientPinkBlue id={gradientIds[0][3]} />
+      <GradientPinkRed id={gradientIds[0][4]} vertical={false} />
+      <GradientPurpleOrange id={gradientIds[1][0]} vertical={false} />
+      <GradientPurpleRed id={gradientIds[1][1]} vertical={false} />
+      <RadialGradient id={gradientIds[1][2]} from="#55bdd5" to="#4f3681" r="80%" />
+      <GradientSteelPurple id={gradientIds[1][3]} vertical={false} />
+      <GradientTealBlue id={gradientIds[1][4]} vertical={false} />
+      {gradientIds.map((rowIds, rowIndex) =>
+        rowIds.map((id, columnIndex) => (
+          <Bar
+            key={id}
+            fill={`url(#${id})`}
+            x={columnIndex * columnWidth}
+            y={rowIndex * rowHeight}
+            width={columnWidth}
+            height={rowHeight}
+            stroke="#ffffff"
+            strokeWidth={8}
+            rx={14}
+          />
+        )),
+      )}
     </svg>
   );
 }
