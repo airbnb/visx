f99cfee37be7e6a0f89af4cbdb3f3d1874e2fa17
Chris Williams <chris.williams@airbnb.com>
2020-03-11 15:15:23 -0700

    type(demo/pie): fix implicit types

--- packages/vx-demo/src/docs-v2/examples/vx-shape-pie/Example.tsx
+++ packages/vx-demo/src/docs-v2/examples/vx-shape-pie/Example.tsx
@@ -176,41 +176,51 @@ function AnimatedPie<Datum>({
     enter: enterUpdateTransition,
     update: enterUpdateTransition,
     leave: animate ? fromLeaveTransition : enterUpdateTransition,
-  }).map(({ item: arc, props, key }) => {
-    const [centroidX, centroidY] = path.centroid(arc);
-    const hasSpaceForLabel = arc.endAngle - arc.startAngle >= 0.1;
+  }).map(
+    ({
+      item: arc,
+      props,
+      key,
+    }: {
+      item: PieArcDatum<Datum>;
+      props: AnimatedStyles;
+      key: string;
+    }) => {
+      const [centroidX, centroidY] = path.centroid(arc);
+      const hasSpaceForLabel = arc.endAngle - arc.startAngle >= 0.1;
 
-    return (
-      <g key={key}>
-        <animated.path
-          // compute interpolated path d attribute from intermediate angle values
-          d={interpolate([props.startAngle, props.endAngle], (startAngle, endAngle) =>
-            path({
-              ...arc,
-              startAngle,
-              endAngle,
-            }),
+      return (
+        <g key={key}>
+          <animated.path
+            // compute interpolated path d attribute from intermediate angle values
+            d={interpolate([props.startAngle, props.endAngle], (startAngle, endAngle) =>
+              path({
+                ...arc,
+                startAngle,
+                endAngle,
+              }),
+            )}
+            fill={getColor(arc)}
+            onClick={() => onClickDatum(arc)}
+            onTouchStart={() => onClickDatum(arc)}
+          />
+          {hasSpaceForLabel && (
+            <animated.g style={{ opacity: props.opacity }}>
+              <text
+                fill="white"
+                x={centroidX}
+                y={centroidY}
+                dy=".33em"
+                fontSize={9}
+                textAnchor="middle"
+                pointerEvents="none"
+              >
+                {getKey(arc)}
+              </text>
+            </animated.g>
           )}
-          fill={getColor(arc)}
-          onClick={() => onClickDatum(arc)}
-          onTouchStart={() => onClickDatum(arc)}
-        />
-        {hasSpaceForLabel && (
-          <animated.g style={{ opacity: props.opacity }}>
-            <text
-              fill="white"
-              x={centroidX}
-              y={centroidY}
-              dy=".33em"
-              fontSize={9}
-              textAnchor="middle"
-              pointerEvents="none"
-            >
-              {getKey(arc)}
-            </text>
-          </animated.g>
-        )}
-      </g>
-    );
-  });
+        </g>
+      );
+    },
+  );
 }
