652b44fd8b6918d914a8f2569744075774613437
Chris Williams <chris.williams@airbnb.com>
2020-05-06 15:05:18 -0700

    new(vx-demo): convert Statsplot to codesandbox

--- packages/vx-demo/src/components/Gallery.tsx
+++ packages/vx-demo/src/components/Gallery.tsx
@@ -28,7 +28,7 @@ import Trees from './tiles/Trees';
 import Dendrograms from './tiles/Dendrograms';
 import Voronoi from './tiles/Voronoi';
 import Legends from './tiles/Legends';
-import StatsPlot from './tiles/Statsplot';
+import StatsPlot from '../docs-v2/examples/vx-stats/Example';
 import GeoCustom from '../docs-v2/examples/vx-geo-custom/Example';
 import GeoMercator from '../docs-v2/examples/vx-geo-mercator/Example';
 import Network from './tiles/Network';
--- packages/vx-demo/src/pages/Statsplot.tsx
+++ packages/vx-demo/src/pages/Statsplot.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Show from '../components/Show';
-import StatsPlot from '../components/tiles/Statsplot';
-import StatsPlotSource from '!!raw-loader!../components/tiles/Statsplot';
+import StatsPlot from '../docs-v2/examples/vx-stats/Example';
+import StatsPlotSource from '!!raw-loader!../docs-v2/examples/vx-stats/Example';
 
 export default () => (
   <Show
     events
-    margin={{ top: 80, right: 0, bottom: 0, left: 0 }}
     component={StatsPlot}
-    title="BoxPlot With ViolinPlot"
+    title="BoxPlot + ViolinPlot"
+    codeSandboxDirectoryName="vx-stats"
   >
     {StatsPlotSource}
   </Show>
