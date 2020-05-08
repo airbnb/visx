2dc3c2bc896456de139ebca43cc2e529702d5be5
Chris Williams <chris.williams@airbnb.com>
2020-05-08 15:13:17 -0700

    types(vx-demo): add VxPackage type

--- packages/vx-demo/src/types/index.ts
+++ packages/vx-demo/src/types/index.ts
@@ -16,3 +16,35 @@ export type ShowProvidedProps = {
   margin?: MarginShape;
   events?: boolean;
 };
+
+export type VxPackage =
+  | 'annotation'
+  | 'axis'
+  | 'curve'
+  | 'glyph'
+  | 'grid'
+  | 'legend'
+  | 'marker'
+  | 'scale'
+  | 'shape'
+  | 'threshold'
+  | 'tooltip'
+  | 'chord'
+  | 'geo'
+  | 'heatmap'
+  | 'hierarchy'
+  | 'network'
+  | 'stats'
+  | 'brush'
+  | 'drag'
+  | 'voronoi'
+  | 'zoom'
+  | 'clip-path'
+  | 'group'
+  | 'gradient'
+  | 'pattern'
+  | 'text'
+  | 'bounds'
+  | 'mock-data'
+  | 'responsive'
+  | 'point';
