12098a16fb4ffbb4f6dadff153538bc6bb1aa6ab
Chris Williams <chris.williams@airbnb.com>
2020-05-08 15:22:47 -0700

    docs(vx-demo): add react-docgen-typescript-loader to next webpack loader

--- packages/vx-demo/src/next.config.js
+++ packages/vx-demo/src/next.config.js
@@ -7,4 +7,22 @@ module.exports = withCss({
     ignoreDevErrors: true,
     ignoreBuildErrors: true,
   },
+  webpack: config => {
+    // append docgen loader to the babel loader (executed bottom->top)
+    const babelConfig = config.module.rules[0];
+    const initialBabelUse = babelConfig.use;
+    babelConfig.use = [
+      ...initialBabelUse,
+      {
+        loader: 'react-docgen-typescript-loader',
+      },
+    ];
+
+    // apply parsing to imports from other packages src/ folders
+    // docgen doesn't work on transpiled packages, and ts parsing isn't
+    // applied outside of vx-demo without this
+    babelConfig.include.push(/vx-.*\/src/);
+
+    return config;
+  },
 });
