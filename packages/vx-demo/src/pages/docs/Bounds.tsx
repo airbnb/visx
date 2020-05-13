e423ef0dff0ce60e6a4698aee3490c916fe8ec56
Chris Williams <chris.williams@airbnb.com>
2020-05-13 13:15:43 -0700

    docs(vx-bounds): update to new DocPage

--- packages/vx-demo/src/pages/docs/Bounds.tsx
+++ packages/vx-demo/src/pages/docs/Bounds.tsx
@@ -1,16 +1,6 @@
 import React from 'react';
-import Markdown from 'react-markdown/with-html';
 
-import Page from '../../components/Page';
-import Footer from '../../components/Footer';
 import BoundsReadme from '!!raw-loader!../../../../vx-bounds/Readme.md';
 import DocPage from '../../components/DocPage';
 
-export default () => (
-  <Page title="@vx/bounds documentation">
-    <DocPage currentPackage="bounds">
-      <Markdown escapeHtml={false} source={BoundsReadme} />
-    </DocPage>
-    <Footer />
-  </Page>
-);
+export default () => <DocPage readme={BoundsReadme} vxPackage="bounds" />;
