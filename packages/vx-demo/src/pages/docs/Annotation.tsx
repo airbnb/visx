dfdde9a69c694ec16aafd36807bcc0f3392c90eb
Chris Williams <chris.williams@airbnb.com>
2020-05-13 13:13:05 -0700

    docs(vx-annotation): update Annotation with new DocPage

--- packages/vx-demo/src/pages/docs/Annotation.tsx
+++ packages/vx-demo/src/pages/docs/Annotation.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import Markdown from 'react-markdown/with-html';
 
-import Page from '../../components/Page';
-import Footer from '../../components/Footer';
-import ApiTable, { DocGenInfo } from '../../components/ApiTable';
 import AnnotationReadme from '!!raw-loader!../../../../vx-annotation/Readme.md';
 import LinePathAnnotation from '../../../../vx-annotation/src/annotations/LinePathAnnotation';
 import DocPage from '../../components/DocPage';
+import { DocGenInfo } from '../../types';
 
 const components = [
   // @ts-ignore
@@ -14,25 +11,5 @@ const components = [
 ] as DocGenInfo[];
 
 export default () => (
-  <Page title="@vx/annotation documentation">
-    <DocPage currentPackage="annotation">
-      <Markdown escapeHtml={false} source={AnnotationReadme} />
-      <h2>Components</h2>
-      <ul>
-        {components.map(component => (
-          <li key={component.displayName}>
-            <a href={`#${component.displayName}`}>
-              <code>&lt;{component.displayName} /&gt;</code>
-            </a>
-          </li>
-        ))}
-      </ul>
-
-      <h2>APIs</h2>
-      {components.map(component => (
-        <ApiTable key={component.displayName} docgenInfo={component} />
-      ))}
-    </DocPage>
-    <Footer />
-  </Page>
+  <DocPage components={components} readme={AnnotationReadme} vxPackage="annotation" />
 );
