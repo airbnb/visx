c0a3880572f3cbd049f286e85ca17ec85ddf55af
Chris Williams <chris.williams@airbnb.com>
2020-05-13 12:31:42 -0700

    docs(vx-chord): add prop type annotations

--- packages/vx-chord/src/Ribbon.tsx
+++ packages/vx-chord/src/Ribbon.tsx
@@ -5,13 +5,21 @@ import { ribbon as d3ribbon, Chord, ChordSubgroup } from 'd3-chord';
 type NumAccessor = (d: ChordSubgroup) => number;
 
 export type RibbonProps = {
+  /** Chord for which to render a ribbon. */
   chord: Chord;
+  /** Sets the source accessor (defaults to chord.source). */
   source?: (d: Chord) => ChordSubgroup;
+  /** Sets the target accessor (defaults to chord.source). */
   target?: (d: Chord) => ChordSubgroup;
+  /** Sets the radius or radius accessor for the ribbon generator. */
   radius?: number | NumAccessor;
+  /** Sets the start angle or start angle accessor for the ribbon generator. */
   startAngle?: number | NumAccessor;
+  /** Sets the end angle or end angle accessor for the ribbon generator. */
   endAngle?: number | NumAccessor;
+  /** Override the default rendering of a chord <path />. */
   children?: ({ path }: { path: string | null }) => string | undefined;
+  /** Classname to apply to the rendered <path />. */
   className?: string;
 };
 
