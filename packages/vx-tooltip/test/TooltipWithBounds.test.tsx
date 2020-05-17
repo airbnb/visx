8382202defdae9b9cd39fb18215862e664efae40
dennisja <dennisjjagwe@gmail.com>
2020-05-17 17:51:23 +0200

    add types to the callbacks to fix the build

--- packages/vx-tooltip/test/TooltipWithBounds.test.tsx
+++ packages/vx-tooltip/test/TooltipWithBounds.test.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { mount } from 'enzyme';
 import { TooltipWithBounds, defaultStyles, Tooltip } from '../src';
 
+type CSSPropertiesKey = keyof React.CSSProperties;
+type CSSPropertiesValue = React.CSSProperties[CSSPropertiesKey];
+
 describe('<TooltipWithBounds />', () => {
   test('it should be defined', () => {
     expect(TooltipWithBounds).toBeDefined();
@@ -10,15 +13,17 @@ describe('<TooltipWithBounds />', () => {
   it('should render the Tooltip with default styles by default', () => {
     const wrapper = mount(<TooltipWithBounds>Hello</TooltipWithBounds>);
     const styles = wrapper.find(Tooltip).props().style!;
-    Object.entries(defaultStyles).forEach(([key, value]) => {
-      expect(styles[key]).toBe(value);
-    });
+    Object.entries(defaultStyles).forEach(
+      ([key, value]: [CSSPropertiesKey, CSSPropertiesValue]) => {
+        expect(styles[key]).toBe(value);
+      },
+    );
   });
 
   it('should render the tooltip without default styles if unstyled is set to true', () => {
     const wrapper = mount(<TooltipWithBounds unstyled>Hello</TooltipWithBounds>);
     const styles = wrapper.find(Tooltip).props().style!;
-    Object.keys(defaultStyles).forEach(key => {
+    Object.keys(defaultStyles).forEach((key: CSSPropertiesKey) => {
       expect(styles[key]).toBeUndefined();
     });
   });
