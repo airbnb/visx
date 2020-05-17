5a7360539def9cd2f12768a786269d03c75c99ef
dennisja <dennisjjagwe@gmail.com>
2020-05-17 17:29:13 +0200

    make styles non-null to fix the build

--- packages/vx-tooltip/test/TooltipWithBounds.test.tsx
+++ packages/vx-tooltip/test/TooltipWithBounds.test.tsx
@@ -9,7 +9,7 @@ describe('<TooltipWithBounds />', () => {
 
   it('should render the Tooltip with default styles by default', () => {
     const wrapper = mount(<TooltipWithBounds>Hello</TooltipWithBounds>);
-    const styles = wrapper.find(Tooltip).prop('style');
+    const styles = wrapper.find(Tooltip).props().style!;
     Object.entries(defaultStyles).forEach(([key, value]) => {
       expect(styles[key]).toBe(value);
     });
@@ -17,7 +17,7 @@ describe('<TooltipWithBounds />', () => {
 
   it('should render the tooltip without default styles if unstyled is set to true', () => {
     const wrapper = mount(<TooltipWithBounds unstyled>Hello</TooltipWithBounds>);
-    const styles = wrapper.find(Tooltip).prop('style');
+    const styles = wrapper.find(Tooltip).props().style!;
     Object.keys(defaultStyles).forEach(key => {
       expect(styles[key]).toBeUndefined();
     });
