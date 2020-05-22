bebc64e1635af9823303fb7c40abd451f41edb38
Chris Williams <chris.williams@airbnb.com>
2020-05-22 14:50:22 -0700

    internal(vx-demo): remove unused components/charts/*, components/util/*

--- packages/vx-demo/src/components/charts/flame/util/parseFoldedStack.js
+++
@@ -1,5 +0,0 @@
-export default ({ source }) => {
-  return source.split('\n').reduce((lines, line) => {
-    return lines.concat(line.split(' '));
-  }, []);
-};
--- packages/vx-demo/src/components/util/sillyColorScale.ts
+++
@@ -1,6 +0,0 @@
-const colors = ['#458aea', '#f7a055', '#9b3655', 'red', 'yellow', '#3f56d1', '#4fd5ef', '#2fd371'];
-const numColors = colors.length;
-
-export default (i: number) => {
-  return colors[i % numColors] || colors[0];
-};
