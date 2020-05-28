e4bac21a544199d01a61652ec215258f3c9f5422
Chris Williams <chris.williams@airbnb.com>
2020-05-28 14:55:53 -0700

    new(vx-demo): Add more example tiles to docs/Pattern

--- packages/vx-demo/src/pages/docs/Pattern.tsx
+++ packages/vx-demo/src/pages/docs/Pattern.tsx
@@ -8,10 +8,12 @@ import Pattern from '../../../../vx-pattern/src/patterns/Pattern';
 import Waves from '../../../../vx-pattern/src/patterns/Waves';
 import DocPage from '../../components/DocPage';
 import PatternsTile from '../../components/Gallery/PatternsTile';
+import StreamGraphTile from '../../components/Gallery/StreamGraphTile';
+import StatsPlotTile from '../../components/Gallery/StatsPlotTile';
 
 const components = [Pattern, Circles, Hexagons, Lines, Path, Waves];
 
-const examples = [PatternsTile];
+const examples = [PatternsTile, StreamGraphTile, StatsPlotTile];
 
 export default () => (
   <DocPage components={components} examples={examples} readme={PatternReadme} vxPackage="pattern" />
