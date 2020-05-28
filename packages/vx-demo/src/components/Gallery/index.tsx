c7af6853721a5e7807bcb44eb01342ff9990002a
Chris Williams <chris.williams@airbnb.com>
2020-05-28 14:38:36 -0700

    new(vx-demo): add package filters to Gallery

--- packages/vx-demo/src/components/Gallery/index.tsx
+++ packages/vx-demo/src/components/Gallery/index.tsx
@@ -1,44 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Tilt from 'react-tilt';
 
-import AreaTile from './AreaTile';
-import AxisTile from './AxisTile';
-import BarGroupHorizontalTile from './BarGroupHorizontalTile';
-import BarGroupTile from './BarGroupTile';
-import BarStackHorizontalTile from './BarStackHorizontalTile';
-import BarStackTile from './BarStackTile';
-import BarsTile from './BarsTile';
-import BrushTile from './BrushTile';
-import ChordTile from './ChordTile';
-import CurvesTile from './CurvesTile';
-import DendrogramsTile from './DendrogramsTile';
-import DotsTile from './DotsTile';
-import DragIITile from './DragIITile';
-import DragITile from './DragITile';
-import GeoCustomTile from './GeoCustomTile';
-import GeoMercatorTile from './GeoMercatorTile';
-import GlyphsTile from './GlyphsTile';
-import GradientsTile from './GradientsTile';
-import HeatmapsTile from './HeatmapsTile';
-import LegendsTile from './LegendsTile';
-import LineRadialTile from './LineRadialTile';
-import LinkTypesTile from './LinkTypesTile';
-import NetworkTile from './NetworkTile';
-import PackTile from './PackTile';
-import PatternsTile from './PatternsTile';
-import PiesTile from './PiesTile';
-import PolygonsTile from './PolygonsTile';
-import RadarTile from './RadarTile';
-import ResponsiveTile from './ResponsiveTile';
-import StackedAreasTile from './StackedAreasTile';
-import StatsPlotTile from './StatsPlotTile';
-import StreamGraphTile from './StreamGraphTile';
-import TextTile from './TextTile';
-import ThresholdTile from './ThresholdTile';
-import TreemapTile from './TreemapTile';
-import TreesTile from './TreesTile';
-import VoronoiTile from './VoronoiTile';
-import ZoomITile from './ZoomITile';
+import * as AreaTile from './AreaTile';
+import * as AxisTile from './AxisTile';
+import * as BarGroupHorizontalTile from './BarGroupHorizontalTile';
+import * as BarGroupTile from './BarGroupTile';
+import * as BarStackHorizontalTile from './BarStackHorizontalTile';
+import * as BarStackTile from './BarStackTile';
+import * as BarsTile from './BarsTile';
+import * as BrushTile from './BrushTile';
+import * as ChordTile from './ChordTile';
+import * as CurvesTile from './CurvesTile';
+import * as DendrogramsTile from './DendrogramsTile';
+import * as DotsTile from './DotsTile';
+import * as DragIITile from './DragIITile';
+import * as DragITile from './DragITile';
+import * as GeoCustomTile from './GeoCustomTile';
+import * as GeoMercatorTile from './GeoMercatorTile';
+import * as GlyphsTile from './GlyphsTile';
+import * as GradientsTile from './GradientsTile';
+import * as HeatmapsTile from './HeatmapsTile';
+import * as LegendsTile from './LegendsTile';
+import * as LineRadialTile from './LineRadialTile';
+import * as LinkTypesTile from './LinkTypesTile';
+import * as NetworkTile from './NetworkTile';
+import * as PackTile from './PackTile';
+import * as PatternsTile from './PatternsTile';
+import * as PiesTile from './PiesTile';
+import * as PolygonsTile from './PolygonsTile';
+import * as RadarTile from './RadarTile';
+import * as ResponsiveTile from './ResponsiveTile';
+import * as StackedAreasTile from './StackedAreasTile';
+import * as StatsPlotTile from './StatsPlotTile';
+import * as StreamGraphTile from './StreamGraphTile';
+import * as TextTile from './TextTile';
+import * as ThresholdTile from './ThresholdTile';
+import * as TreemapTile from './TreemapTile';
+import * as TreesTile from './TreesTile';
+import * as VoronoiTile from './VoronoiTile';
+import * as ZoomITile from './ZoomITile';
+import { VxPackage } from '../../types';
+import exampleToVxDependencyLookup, {
+  vxPackages,
+} from '../../sandboxes/exampleToVxDependencyLookup';
+
+const tiltOptions = { max: 8, scale: 1 };
+const columnCount = 3;
 
 const tiles = [
   AreaTile,
@@ -80,24 +87,93 @@ const tiles = [
   VoronoiTile,
   ZoomITile,
 ];
-const tiltOptions = { max: 8, scale: 1 };
 
 export default function Gallery() {
+  const [activePackageFilter, setActivePackageFilter] = useState<VxPackage | null>(null);
+  const filteredTiles = activePackageFilter
+    ? tiles.filter(Tile =>
+        exampleToVxDependencyLookup[Tile.packageJson.name]?.has(activePackageFilter),
+      )
+    : tiles;
+
   return (
     <>
       <div className="gallery">
-        {tiles.map((Tile, i) => (
-          <Tilt key={`tile-${i}`} className="tilt" options={tiltOptions}>
-            <Tile />
-          </Tilt>
-        ))}
+        <div className="filters">
+          <h6>Examples by package</h6>
+          {vxPackages.map(vxPackage => (
+            <button
+              key={vxPackage}
+              className={activePackageFilter === vxPackage ? 'emphasize' : undefined}
+              onClick={() =>
+                setActivePackageFilter(activePackageFilter === vxPackage ? null : vxPackage)
+              }
+            >
+              @vx/{vxPackage}
+            </button>
+          ))}
+        </div>
+        <div className="grid">
+          {filteredTiles.map((Tile, i) => (
+            <Tilt key={`tile-${i}`} className="tilt" options={tiltOptions}>
+              <Tile.default />
+            </Tilt>
+          ))}
+        </div>
       </div>
       <style jsx>{`
         .gallery {
+          display: flex;
+          flex-direction: row;
+        }
+        .grid {
+          flex-grow: 1;
           display: grid;
           grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
           overflow-x: hidden;
-          padding-bottom: 20px;
+          padding-bottom: 40px;
+        }
+        .filters {
+          margin-right: 24px;
+          width: 150px;
+          flex-shrink: 0;
+        }
+        h6 {
+          margin: 0 4px 0 0;
+        }
+        button {
+          display: block;
+          cursor: pointer;
+          border: none;
+          outline: none;
+          background: transparent;
+          color: #fc2e1c;
+          padding: 0;
+          margin: 4px 4px 12px 0;
+        }
+        .emphasize {
+          font-weight: bold;
+        }
+        @media (min-width: 800px) {
+          .emphasize::before {
+            content: '';
+            padding-left: 4px;
+            border-left: 2px solid #fc2e1c;
+          }
+        }
+        @media (max-width: 800px) {
+          .gallery {
+            flex-direction: column;
+            min-width: 90vw;
+            max-width: 90vw;
+            margin: 0;
+          }
+          .filters {
+            display: flex;
+            flex-wrap: wrap;
+            width: 100%;
+            justify-content: center;
+          }
         }
       `}</style>
     </>
