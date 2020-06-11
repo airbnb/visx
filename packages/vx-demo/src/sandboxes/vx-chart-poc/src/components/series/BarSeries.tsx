08d31bb427a29ebdd476f5a3e094b74c877505f3
Chris Williams <chris.williams@airbnb.com>
2020-06-11 16:27:02 -0700

    new(vx-chart-poc): add animated BarSeries + LineSeries

---
+++ packages/vx-demo/src/sandboxes/vx-chart-poc/src/components/series/BarSeries.tsx
@@ -0,0 +1,101 @@
+import React, { useContext, useCallback } from 'react';
+import { animated, useSprings } from 'react-spring';
+import ChartContext from '../../context/ChartContext';
+import { ChartContext as ChartContextType, SeriesProps } from '../../types';
+import withRegisteredData from '../../enhancers/withRegisteredData';
+import isValidNumber from '../../typeguards/isValidNumber';
+import useRegisteredData from '../../hooks/useRegisteredData';
+
+type BarSeriesProps<Datum, XScaleInput, YScaleInput> = SeriesProps<
+  Datum,
+  XScaleInput,
+  YScaleInput
+> & {
+  /** Whether bars should be rendered horizontally instead of vertically. */
+  horizontal?: boolean;
+  /** Specify bar thickness, useful when not using a 'band' scale. Defaults to `scale.bandwidth()` if available, else `available size / data.length` */
+  barThickness?: number;
+} & Omit<React.SVGProps<SVGRectElement>, 'x' | 'y' | 'width' | 'height' | 'ref'>;
+
+function BarSeries<Datum = unknown, XScaleInput = unknown, YScaleInput = unknown>({
+  dataKey,
+  data: _,
+  xAccessor: __,
+  yAccessor: ___,
+  mouseEvents,
+  horizontal,
+  barThickness: barThicknessProp,
+  ...barProps
+}: BarSeriesProps<Datum, XScaleInput, YScaleInput>) {
+  const { theme, xScale, yScale } = useContext(ChartContext) as ChartContextType<
+    Datum,
+    XScaleInput,
+    YScaleInput
+  >;
+  const { data, xAccessor, yAccessor } = useRegisteredData<Datum, XScaleInput, YScaleInput>(
+    dataKey,
+  );
+  const getScaledX = useCallback((d: Datum) => xScale(xAccessor(d)), [xScale, xAccessor]);
+  const getScaledY = useCallback((d: Datum) => yScale(yAccessor(d)), [yScale, yAccessor]);
+
+  const [xMin, xMax] = xScale.range();
+  const [yMax, yMin] = yScale.range();
+  const innerWidth = Math.abs(xMax - xMin);
+  const innerHeight = Math.abs(yMax - yMin);
+  const barThickness: number =
+    barThicknessProp ||
+    (horizontal
+      ? // non-bandwidth estimate assumes no missing data values
+        yScale.bandwidth?.() ?? innerHeight / data.length
+      : xScale.bandwidth?.() ?? innerWidth / data.length);
+
+  // try to figure out the 0 baseline for correct rendering of negative values
+  // we aren't sure if these are numeric scales or not a priori
+  // @ts-ignore
+  const maybeXZero = xScale(0);
+  // @ts-ignore
+  const maybeYZero = yScale(0);
+
+  const xZeroPosition = isValidNumber(maybeXZero)
+    ? // if maybeXZero _is_ a number, but the scale is not clamped and it's outside the domain
+      // fallback to the scale's minimum
+      (Math.max(maybeXZero, Math.min(xMin, xMax)) as number)
+    : Math.min(xMin, xMax);
+  const yZeroPosition = isValidNumber(maybeYZero)
+    ? (Math.min(maybeYZero, Math.max(yMin, yMax)) as number)
+    : Math.max(yMin, yMax);
+
+  const animatedBars = useSprings(
+    data.length,
+    data.map(datum => {
+      const x = getScaledX(datum);
+      const y = getScaledY(datum);
+      const barLength = horizontal ? x - xZeroPosition : y - yZeroPosition;
+
+      return {
+        x: horizontal ? xZeroPosition + Math.min(0, barLength) : x,
+        y: horizontal ? y : yZeroPosition + Math.min(0, barLength),
+        width: horizontal ? Math.abs(barLength) : barThickness,
+        height: horizontal ? barThickness : Math.abs(barLength),
+      };
+    }),
+  ) as { x: number; y: number; width: number; height: number }[];
+
+  return (
+    <g className="vx-chart bar-series">
+      {animatedBars.map(({ x, y, width, height }, i) => (
+        <animated.rect
+          key={i}
+          x={x}
+          y={y}
+          width={width}
+          height={height}
+          fill={theme?.colors?.[1]}
+          {...barProps}
+        />
+      ))}
+    </g>
+  );
+}
+
+export default withRegisteredData(BarSeries);
---
+++ packages/vx-demo/src/sandboxes/vx-chart-poc/src/components/series/LineSeries.tsx
@@ -0,0 +1,71 @@
+import React, { useContext, useCallback } from 'react';
+import { animated, useSpring } from 'react-spring';
+import LinePath, { LinePathProps } from '@vx/shape/lib/shapes/LinePath';
+import ChartContext from '../../context/ChartContext';
+import { SeriesProps } from '../../types';
+import withRegisteredData from '../../enhancers/withRegisteredData';
+import isValidNumber from '../../typeguards/isValidNumber';
+import useRegisteredData from '../../hooks/useRegisteredData';
+
+type LineSeriesProps<Datum, XScaleInput, YScaleInput> = SeriesProps<
+  Datum,
+  XScaleInput,
+  YScaleInput
+> &
+  Omit<LinePathProps<Datum>, 'x' | 'y' | 'data' | 'innerRef'> &
+  Omit<React.SVGProps<SVGPathElement>, keyof LinePathProps<Datum>>;
+
+function LineSeries<Datum = unknown, XScaleInput = unknown, YScaleInput = unknown>({
+  data: _,
+  xAccessor: __,
+  yAccessor: ___,
+  dataKey,
+  mouseEvents,
+  ...lineProps
+}: LineSeriesProps<Datum, XScaleInput, YScaleInput>) {
+  const { theme, xScale, yScale } = useContext(ChartContext);
+  const { data, xAccessor, yAccessor } = useRegisteredData<Datum, XScaleInput, YScaleInput>(
+    dataKey,
+  );
+
+  const getScaledX = useCallback(
+    (d: Datum) => {
+      const x = xScale(xAccessor(d));
+      return isValidNumber(x) ? x + (xScale.bandwidth?.() ?? 0) / 2 : null;
+    },
+    [xScale, xAccessor],
+  );
+
+  const getScaledY = useCallback(
+    (d: Datum) => {
+      const y = yScale(yAccessor(d));
+      return isValidNumber(y) ? y + (yScale.bandwidth?.() ?? 0) / 2 : null;
+    },
+    [yScale, yAccessor],
+  );
+
+  return (
+    <g>
+      <LinePath<Datum>
+        data={data}
+        x={getScaledX}
+        y={getScaledY}
+        stroke={theme?.colors?.[0]}
+        {...lineProps}
+      >
+        {({ path }) => <AnimatedPath {...lineProps} d={path(data) || ''} />}
+      </LinePath>
+    </g>
+  );
+}
+
+/** Separate component so that we don't use the `useSpring` hook in a render function callback. */
+function AnimatedPath({
+  d,
+  ...lineProps
+}: { d: string } & Partial<Omit<React.SVGProps<SVGPathElement>, 'ref'>>) {
+  const tweenedPath = useSpring({ d, config: { precision: 0.01 } });
+  return <animated.path d={tweenedPath.d} fill="transparent" {...lineProps} />;
+}
+
+export default React.memo(withRegisteredData(LineSeries));
