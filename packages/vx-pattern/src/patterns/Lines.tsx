5a0089b522a8da1a935bde97a82bb313b0f67935
dennisja <dennisjjagwe@gmail.com>
2020-06-11 08:10:09 +0200

    add diagonal right to left pattern

--- packages/vx-pattern/src/patterns/Lines.tsx
+++ packages/vx-pattern/src/patterns/Lines.tsx
@@ -3,7 +3,7 @@ import cx from 'classnames';
 import Pattern from './Pattern';
 import { PatternOrientation, PatternOrientationType } from '../constants';
 
-function pathForOrientation({
+export function pathForOrientation({
   height,
   orientation,
 }: {
@@ -19,6 +19,11 @@ function pathForOrientation({
       return `M 0,${height} l ${height},${-height} M ${-height / 4},${height / 4} l ${height /
         2},${-height / 2}
              M ${(3 / 4) * height},${(5 / 4) * height} l ${height / 2},${-height / 2}`;
+    case PatternOrientation.diagonalRightToLeft:
+      const half = height / 2;
+      return `M 0,0 l ${height},${height}
+        M ${-height / 4},${(3 / 4) * height} l ${half},${half}
+        M ${(3 / 4) * height},${-height / 4} l ${half},${half}`;
     default:
       return `M ${height / 2}, 0 l 0, ${height}`;
   }
