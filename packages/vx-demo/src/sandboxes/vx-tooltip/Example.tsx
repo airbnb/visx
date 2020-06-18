5e97744d89d9603cfc11815b7707bc17e7a516c2
Chris Williams <chris.williams@airbnb.com>
2020-06-17 17:38:32 -0700

    new(vx-demo): add Tooltip demo

---
+++ packages/vx-demo/src/sandboxes/vx-tooltip/Example.tsx
@@ -0,0 +1,241 @@
+import React, { useState, useCallback } from 'react';
+import useMeasure from 'react-use-measure';
+import { Tooltip, TooltipWithBounds, useTooltip, defaultStyles, Portal } from '@vx/tooltip/src';
+
+type Props = {
+  width: number;
+  height: number;
+  showControls?: boolean;
+};
+
+interface TooltipData {
+  text: string;
+  containerX: number;
+  containerY: number;
+  pageX: number;
+  pageY: number;
+}
+
+let tooltipTimeout: number;
+
+const positionIndicatorSize = 8;
+
+const tooltipStyles = {
+  ...defaultStyles,
+  backgroundColor: 'rgba(53,71,125,0.8)',
+  color: 'white',
+  width: 152,
+  height: 72,
+  padding: 12,
+};
+
+export default function Example({ width, height, showControls = true }: Props) {
+  const [ref, ownBounds] = useMeasure();
+  const [detectBounds, setDetectBounds] = useState(true);
+  const [renderInPortal, setRenderInPortal] = useState(false);
+
+  const {
+    showTooltip,
+    hideTooltip,
+    tooltipOpen,
+    tooltipData,
+    tooltipLeft,
+    tooltipTop,
+  } = useTooltip<TooltipData>({
+    tooltipOpen: true,
+    tooltipLeft: width / 3,
+    tooltipTop: height / 3,
+    tooltipData: {
+      text: 'Move me with your mouse or finger',
+      containerX: width / 3,
+      containerY: height / 3,
+      pageX: 0,
+      pageY: 0,
+    },
+  });
+
+  // event handlers
+  const handleMouseMove = useCallback(
+    (event: React.MouseEvent | React.TouchEvent) => {
+      if (tooltipTimeout) clearTimeout(tooltipTimeout);
+
+      const pageX = 'pageX' in event ? event.pageX : 0;
+      const pageY = 'pageY' in event ? event.pageY : 0;
+      const containerX = pageX - ownBounds.left;
+      const containerY = pageY - ownBounds.top;
+
+      showTooltip({
+        tooltipLeft: renderInPortal ? pageX : containerX,
+        tooltipTop: renderInPortal ? pageY : containerY,
+        tooltipData: {
+          containerX,
+          containerY,
+          pageX,
+          pageY,
+          text: detectBounds
+            ? 'I detect my container boundary'
+            : 'I will get clipped by my container',
+        },
+      });
+    },
+    [showTooltip, ownBounds, detectBounds, renderInPortal],
+  );
+
+  const TooltipComponent = detectBounds ? TooltipWithBounds : Tooltip;
+  const PortalWrapper = renderInPortal ? Portal : React.Fragment;
+
+  return (
+    <>
+      <div
+        ref={ref}
+        className="tooltip-example"
+        style={{ width, height }}
+        onMouseMove={handleMouseMove}
+        onTouchMove={handleMouseMove}
+      >
+        {tooltipOpen ? (
+          <>
+            <div
+              className="position-indicator"
+              style={{
+                width: positionIndicatorSize,
+                height: positionIndicatorSize,
+                transform: `translate(${tooltipData.containerX -
+                  positionIndicatorSize / 2}px, ${tooltipData.containerY -
+                  positionIndicatorSize / 2}px)`,
+              }}
+            />
+            <div
+              className="crosshair horizontal"
+              style={{ transform: `translateY(${tooltipData.containerY}px)` }}
+            />
+            <div
+              className="crosshair vertical"
+              style={{ transform: `translateX(${tooltipData.containerX}px)` }}
+            />
+            <PortalWrapper>
+              <TooltipComponent
+                key={Math.random()} // needed for bounds to update correctly
+                left={tooltipLeft + (detectBounds ? 0 : 10)}
+                top={tooltipTop + (detectBounds ? 0 : 10)}
+                style={tooltipStyles}
+              >
+                {tooltipData.text}
+                <br />
+                <br />
+                <strong>left</strong> {tooltipLeft?.toFixed(0)}px
+                <br />
+                <strong>top</strong> {tooltipTop?.toFixed(0)}px
+              </TooltipComponent>
+            </PortalWrapper>
+          </>
+        ) : (
+          <div className="no-tooltip">Move or touch the canvas to see the tooltip</div>
+        )}
+        <div className="z-index-bummer">
+          I have an annoying z-index. Try the portal{' '}
+          <span role="img" aria-label="yay">
+            🥳
+          </span>
+        </div>
+      </div>
+      {showControls && (
+        <div className="tooltip-controls">
+          <label>
+            <input
+              type="checkbox"
+              checked={detectBounds}
+              onChange={() => setDetectBounds(!detectBounds)}
+            />
+            &nbsp;Tooltip with boundary detection
+          </label>
+          <label>
+            <input
+              type="checkbox"
+              checked={renderInPortal}
+              onChange={() => {
+                const nextRenderInPortal = !renderInPortal;
+                setRenderInPortal(nextRenderInPortal);
+                if (tooltipOpen && tooltipData) {
+                  showTooltip({
+                    tooltipData,
+                    tooltipLeft:
+                      tooltipData[nextRenderInPortal ? 'pageX' : 'containerX'] || tooltipLeft,
+                    tooltipTop:
+                      tooltipData[nextRenderInPortal ? 'pageY' : 'containerY'] || tooltipTop,
+                  });
+                }
+              }}
+            />
+            &nbsp;Render in Portal
+          </label>
+          <button onClick={() => hideTooltip()}>Hide tooltip</button>
+        </div>
+      )}
+      <style jsx>{`
+        .tooltip-example {
+          z-index: 0;
+          position: relative;
+          overflow: hidden;
+          border-radius: 16px;
+          background: linear-gradient(45deg, #6c5b7b, #c06c84, #f67280);
+          font-size: 14px;
+          color: white;
+          width: 100%;
+          height: 100%;
+        }
+        .vx-tooltip {
+          background-color: #f67280;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: space-around;
+          width: 200px;
+          height: 100px;
+        }
+        .tooltip-controls label {
+          font-size: 14px;
+          margin-right: 8px;
+        }
+        .position-indicator {
+          border-radius: 50%;
+          background: #35477d;
+          position: absolute;
+        }
+        .crosshair {
+          position: absolute;
+          top: 0;
+          left: 0;
+        }
+        .crosshair.horizontal {
+          width: 100%;
+          height: 1px;
+          border-top: 1px dashed #35477d;
+        }
+        .crosshair.vertical {
+          height: 100%;
+          width: 1px;
+          border-left: 1px dashed #35477d;
+        }
+        .no-tooltip {
+          position: absolute;
+          left: 50%;
+          top: 50%;
+          transform: translate(-50%, -50%);
+        }
+        .z-index-bummer {
+          position: absolute;
+          right: 10%;
+          bottom: 20%;
+          max-width: 180px;
+          z-index: 2000;
+          background: rgba(255, 255, 255, 0.8);
+          color: #35477d;
+          border-radius: 8px;
+          padding: 16px;
+          line-height: 1.2em;
+        }
+      `}</style>
+    </>
+  );
+}
