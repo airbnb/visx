6cd16cd9cc8be75cb1cfc9baa36df3005e2f1e07
Chris Williams <chris.williams@airbnb.com>
2020-06-18 12:46:09 -0700

    new(vx-demo): add TooltipTile to /gallery and docs/tooltip

--- packages/vx-demo/src/pages/docs/tooltip.tsx
+++ packages/vx-demo/src/pages/docs/tooltip.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import TooltipReadme from '!!raw-loader!../../../../vx-tooltip/Readme.md';
 import DocPage from '../../components/DocPage';
+import TooltipTile from '../../components/Gallery/TooltipTile';
 import DotsTile from '../../components/Gallery/DotsTile';
 import BarStackHorizontalTile from '../../components/Gallery/BarStackHorizontalTile';
 import StatsPlotTile from '../../components/Gallery/StatsPlotTile';
 import AreaTile from '../../components/Gallery/AreaTile';
 
-const examples = [DotsTile, BarStackHorizontalTile, StatsPlotTile, AreaTile];
+const examples = [TooltipTile, DotsTile, BarStackHorizontalTile, StatsPlotTile, AreaTile];
 
 export default () => <DocPage examples={examples} readme={TooltipReadme} vxPackage="tooltip" />;
