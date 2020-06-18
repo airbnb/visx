b26e11be6d48e4172b00df51523178992f91f392
Chris Williams <chris.williams@airbnb.com>
2020-06-17 17:36:32 -0700

    new(vx-tooltip): add Portal

---
+++ packages/vx-tooltip/src/Portal.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export type PortalProps = {
+  /** Optional z-index to set on the Portal. */
+  zIndex?: number;
+  /** Content to render in the Portal. */
+  children: NonNullable<React.ReactNode>;
+};
+
+/** Render within a portal using a declarative component API. */
+export default class Portal extends React.PureComponent<PortalProps> {
+  private node?: HTMLDivElement;
+
+  componentWillUnmount() {
+    if (this.node && document.body) {
+      document.body.removeChild(this.node);
+      delete this.node;
+    }
+  }
+
+  render() {
+    // SSR check
+    if (!this.node && typeof document !== 'undefined') {
+      this.node = document.createElement('div');
+      if (this.props.zIndex != null) this.node.style.zIndex = `${this.props.zIndex}`;
+      document.body.append(this.node);
+    }
+
+    if (!this.node) {
+      return null;
+    }
+
+    return ReactDOM.createPortal(this.props.children, this.node);
+  }
+}
--- packages/vx-tooltip/src/index.ts
+++ packages/vx-tooltip/src/index.ts
@@ -2,3 +2,4 @@ export { default as withTooltip } from './enhancers/withTooltip';
 export { default as useTooltip } from './hooks/useTooltip';
 export { default as Tooltip, defaultStyles } from './tooltips/Tooltip';
 export { default as TooltipWithBounds } from './tooltips/TooltipWithBounds';
+export { default as Portal } from './Portal';
