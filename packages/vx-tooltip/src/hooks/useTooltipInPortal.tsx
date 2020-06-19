a66eda4909155b4f46c7987dba0d74d8ce1a0f09
Chris Williams <chris.williams@airbnb.com>
2020-06-19 16:40:08 -0700

    new(vx-tooltip): add useTooltipInPortal

---
+++ packages/vx-tooltip/src/hooks/useTooltipInPortal.tsx
@@ -0,0 +1,51 @@
+import React, { useMemo } from 'react';
+import useMeasure from 'react-use-measure';
+
+import Tooltip, { TooltipProps } from '../tooltips/Tooltip';
+import TooltipWithBounds from '../tooltips/TooltipWithBounds';
+import Portal from '../Portal';
+
+export type UseTooltipPortalParams = {
+  /** Whether TooltipWithBounds should be used to auto-detect its container boundaries and update its position accordingly. */
+  detectBounds?: boolean;
+} & UseMeasureOptions;
+
+export type UseMeasureOptions = {
+  /** whether TooltipWithBounds should be used to auto-detect (page) boundaries and reposition itself. */
+  detectBounds?: boolean;
+  /** Debounce resize or scroll events in milliseconds (needed for positioning) */
+  debounce?: number | { scroll: number; resize: number };
+  /** React to nested scroll changes, don't use this if you know your view is static */
+  scroll?: boolean;
+  /** You can optionally inject a ResizeObserver polyfill. */
+  polyfill?: any;
+};
+
+/**
+ * Hook that handles rendering of a Tooltip or TooltipWithBounds in a Portal.
+ * Handles conversion of container coordinates to page coordinates using the container bounds.
+ */
+export default function useTooltipInPortal({
+  scroll,
+  detectBounds = true,
+}: UseTooltipPortalParams | undefined = {}) {
+  const [containerRef, containerBounds] = useMeasure({ scroll });
+
+  const TooltipInPortal = useMemo(
+    () => ({ left: containerLeft = 0, top: containerTop = 0, ...tooltipProps }: TooltipProps) => {
+      const TooltipComponent = detectBounds ? TooltipWithBounds : Tooltip;
+      // convert container coordinates to page coordinates
+      const portalLeft = containerLeft + (containerBounds.left || 0) + window.scrollX;
+      const portalTop = containerTop + (containerBounds.top || 0) + window.scrollY;
+
+      return (
+        <Portal>
+          <TooltipComponent left={portalLeft} top={portalTop} {...tooltipProps} />
+        </Portal>
+      );
+    },
+    [detectBounds, containerBounds.left, containerBounds.top],
+  );
+
+  return { containerRef, containerBounds, TooltipInPortal };
+}
