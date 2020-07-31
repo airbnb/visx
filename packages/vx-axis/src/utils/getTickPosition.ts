e62f03d17ef8f20946f0dab520c72c9f1c462608
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-07-31 11:54:57 -0700

    feat: add align to getTickPosition

--- packages/vx-axis/src/utils/getTickPosition.ts
+++ packages/vx-axis/src/utils/getTickPosition.ts
@@ -4,14 +4,18 @@ import { AxisScale, AxisScaleOutput } from '../types';
 /**
  * Create a function that returns a tick position for the given tick value
  */
-export default function getTickPosition<Scale extends AxisScale>(scale: Scale) {
+export default function getTickPosition<Scale extends AxisScale>(
+  scale: Scale,
+  align: 'start' | 'center' | 'end' = 'center',
+) {
   // Broaden type before using 'xxx' in s as typeguard.
   const s = scale as AxisScale;
 
   // For point or band scales,
-  // have to add offset to make the tick centered.
-  if ('bandwidth' in s) {
-    let offset = s.bandwidth() / 2;
+  // have to add offset to make the tick at center or end.
+  if (align !== 'start' && 'bandwidth' in s) {
+    let offset = s.bandwidth();
+    if (align === 'center') offset /= 2;
     if (s.round()) offset = Math.round(offset);
     return (d: ScaleInput<Scale>) => {
       const scaledValue = s(d);
