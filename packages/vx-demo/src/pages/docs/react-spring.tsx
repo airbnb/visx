46d1979574b377da99ea0b8b279d616e3af4a13c
Chris Williams <chris.williams@airbnb.com>
2020-08-19 16:29:51 -0700

    docs(react-spring): add package docs page

---
+++ packages/vx-demo/src/pages/docs/react-spring.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactSpringReadme from '!!raw-loader!../../../../vx-react-spring/Readme.md';
+import AnimatedAxis from '../../../../vx-react-spring/src/axis/AnimatedAxis';
+import DocPage from '../../components/DocPage';
+import AxisTile from '../../components/Gallery/AxisTile';
+
+const components = [AnimatedAxis];
+
+const examples = [AxisTile];
+
+export default () => (
+  <DocPage
+    components={components}
+    examples={examples}
+    readme={ReactSpringReadme}
+    vxPackage="react-spring"
+  />
+);
