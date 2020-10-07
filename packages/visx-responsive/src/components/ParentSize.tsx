715c6e05b2ec7e0a68518ff6b959ec32dd85707c
Neil Kistner <neil.kistner@gmail.com>
2020-10-07 09:17:29 -0500

    Rename prop from `disableFor` to `ignoreDimensions`

--- packages/visx-responsive/src/components/ParentSize.tsx
+++ packages/visx-responsive/src/components/ParentSize.tsx
@@ -7,10 +7,10 @@ export type ParentSizeProps = {
   className?: string;
   /** Child render updates upon resize are delayed until `debounceTime` milliseconds _after_ the last resize event is observed. */
   debounceTime?: number;
-  /** Optional dimensions provided won't trigger a state change when changed. */
-  disableFor?: keyof ParentSizeState | (keyof ParentSizeState)[];
   /** Optional flag to toggle leading debounce calls. When set to true this will ensure that the component always renders immediately. (defaults to true) */
   enableDebounceLeadingCall?: boolean;
+  /** Optional dimensions provided won't trigger a state change when changed. */
+  ignoreDimensions?: keyof ParentSizeState | (keyof ParentSizeState)[];
   /** Optional `style` object to apply to the parent `div` wrapper used for size measurement. */
   parentSizeStyles?: React.CSSProperties;
   /** Child render function `({ width, height, top, left, ref, resize }) => ReactNode`. */
@@ -35,7 +35,7 @@ export default function ParentSize({
   className,
   children,
   debounceTime = 300,
-  disableFor,
+  ignoreDimensions = [],
   parentSizeStyles = { width: '100%', height: '100%' },
   enableDebounceLeadingCall = true,
   ...restProps
@@ -46,13 +46,13 @@ export default function ParentSize({
   const [state, setState] = useState<ParentSizeState>({ width: 0, height: 0, top: 0, left: 0 });
 
   const resize = useMemo(() => {
-    const normalized = disableFor ? (Array.isArray(disableFor) ? disableFor : [disableFor]) : [];
+    const normalized = Array.isArray(ignoreDimensions) ? ignoreDimensions : [ignoreDimensions];
 
     return debounce(
       (incoming: ParentSizeState) => {
         setState(existing => {
           const stateKeys = Object.keys(existing) as (keyof ParentSizeState)[];
-          const keysWithChanges = stateKeys.filter(key => existing[key] !== incoming[key])
+          const keysWithChanges = stateKeys.filter(key => existing[key] !== incoming[key]);
           const shouldBail = keysWithChanges.every(key => normalized.includes(key));
 
           return shouldBail ? existing : incoming;
@@ -61,7 +61,7 @@ export default function ParentSize({
       debounceTime,
       { leading: enableDebounceLeadingCall },
     );
-  }, [debounceTime, disableFor, enableDebounceLeadingCall]);
+  }, [debounceTime, enableDebounceLeadingCall, ignoreDimensions]);
 
   useEffect(() => {
     const observer = new ResizeObserver((entries = [] /** , observer */) => {
