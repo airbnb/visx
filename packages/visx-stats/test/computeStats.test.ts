def221f65ab144f9930d7bb8895de47d52f11d10
Saurabh Kanhegaonkar <saurabh.kanhegaonkar@gmail.com>
2020-10-07 12:48:12 -0700

    fixed prettier issue and have only single quotes

--- packages/visx-stats/test/computeStats.test.ts
+++ packages/visx-stats/test/computeStats.test.ts
@@ -1,26 +1,26 @@
-import { computeStats } from "../src";
+import { computeStats } from '../src';
 
 const data = [1, 2, 3, 4, 5, 6, 6, 7, 8, 9, 1];
 const edgeCaseData = [10000, 2400, 10000, 10000];
 
-describe("computeStats", () => {
-  test("it should be defined", () => {
+describe('computeStats', () => {
+  test('it should be defined', () => {
     expect(computeStats).toBeDefined();
   });
 
-  test("it should have boxPlot and binData", () => {
+  test('it should have boxPlot and binData', () => {
     const stats = computeStats(data);
     expect(stats.boxPlot).toBeDefined();
     expect(stats.binData).toBeDefined();
   });
 
-  test("it should have boxPlot and binData when first and third quartile are equal", () => {
+  test('it should have boxPlot and binData when first and third quartile are equal', () => {
     const stats = computeStats(edgeCaseData);
     expect(stats.boxPlot).toBeDefined();
     expect(stats.binData).toBeDefined();
   });
 
-  test("min/max should match the dataset when there are no outliers", () => {
+  test('min/max should match the dataset when there are no outliers', () => {
     const stats = computeStats(edgeCaseData);
     expect(stats.boxPlot.min).toBe(Math.min(...edgeCaseData));
     expect(stats.boxPlot.max).toBe(Math.max(...edgeCaseData));
