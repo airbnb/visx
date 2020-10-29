19504847509d784772b38d9bd933cbecd9ea6328
Chris Williams <chris.williams@airbnb.com>
2020-10-29 12:15:13 -0700

    new(drag): add useStateWithCallback util

---
+++ packages/visx-drag/src/util/useStateWithCallback.ts
@@ -0,0 +1,31 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+
+type SetStateWithCallback<State> = (
+  nextState: State | ((currState: State) => State),
+  callback?: (currState: State) => void,
+) => void;
+
+export default function useStateWithCallback<State>(
+  initialState: State,
+): [State, SetStateWithCallback<State>] {
+  const [state, setState] = useState(initialState);
+  const callbackRef = useRef<null | ((state: State) => void)>(null);
+
+  const setStateCallback = useCallback<SetStateWithCallback<State>>(
+    (nextState, callback) => {
+      callbackRef.current = callback || null;
+      setState(nextState);
+    },
+    [setState],
+  );
+
+  useEffect(() => {
+    // `null` on initial render, so we only execute callback on state *updates*
+    if (callbackRef.current) {
+      callbackRef.current(state);
+      callbackRef.current = null; // reset callback after execution
+    }
+  }, [state]);
+
+  return [state, setStateCallback];
+}
