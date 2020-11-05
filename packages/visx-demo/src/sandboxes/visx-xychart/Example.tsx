2f54108675bb717206c46b2eee328c6fc721519b
Chris Williams <williaster@users.noreply.github.com>
2020-11-05 11:19:06 -0800

    new(xychart): make DataProvider optional (#913)
    * new(xychart/XYChart): wrap self in DataProvider to make DataProvider rendering optional
    
    * new(demo/xychart): remove DataProvider from demo
    
    * fix(xychart): prevent edge case infinite recursion

--- packages/visx-demo/src/sandboxes/visx-xychart/Example.tsx
+++ packages/visx-demo/src/sandboxes/visx-xychart/Example.tsx
@@ -9,7 +9,6 @@ import {
   AnimatedGlyphSeries,
   AnimatedGrid,
   AnimatedLineSeries,
-  DataProvider,
   Tooltip,
   XYChart,
 } from '@visx/xychart';
@@ -53,173 +52,171 @@ export default function Example({ height }: Props) {
         xAxisOrientation,
         yAxisOrientation,
       }) => (
-        <DataProvider theme={theme} xScale={config.x} yScale={config.y}>
-          <XYChart height={Math.min(400, height)}>
-            <CustomChartBackground />
-            <AnimatedGrid
-              key={`grid-${animationTrajectory}`} // force animate on update
-              rows={showGridRows}
-              columns={showGridColumns}
-              animationTrajectory={animationTrajectory}
-              numTicks={numTicks}
-            />
-            {renderBarStack && (
-              <AnimatedBarStack>
-                <AnimatedBarSeries
-                  dataKey="New York"
-                  data={data}
-                  xAccessor={accessors.x['New York']}
-                  yAccessor={accessors.y['New York']}
-                />
-                <AnimatedBarSeries
-                  dataKey="San Francisco"
-                  data={data}
-                  xAccessor={accessors.x['San Francisco']}
-                  yAccessor={accessors.y['San Francisco']}
-                />
-                <AnimatedBarSeries
-                  dataKey="Austin"
-                  data={data}
-                  xAccessor={accessors.x.Austin}
-                  yAccessor={accessors.y.Austin}
-                />
-              </AnimatedBarStack>
-            )}
-            {renderBarGroup && (
-              <AnimatedBarGroup>
-                <AnimatedBarSeries
-                  dataKey="New York"
-                  data={data}
-                  xAccessor={accessors.x['New York']}
-                  yAccessor={accessors.y['New York']}
-                />
-                <AnimatedBarSeries
-                  dataKey="San Francisco"
-                  data={data}
-                  xAccessor={accessors.x['San Francisco']}
-                  yAccessor={accessors.y['San Francisco']}
-                />
-                <AnimatedBarSeries
-                  dataKey="Austin"
-                  data={data}
-                  xAccessor={accessors.x.Austin}
-                  yAccessor={accessors.y.Austin}
-                />
-              </AnimatedBarGroup>
-            )}
-            {renderBarSeries && (
+        <XYChart theme={theme} xScale={config.x} yScale={config.y} height={Math.min(400, height)}>
+          <CustomChartBackground />
+          <AnimatedGrid
+            key={`grid-${animationTrajectory}`} // force animate on update
+            rows={showGridRows}
+            columns={showGridColumns}
+            animationTrajectory={animationTrajectory}
+            numTicks={numTicks}
+          />
+          {renderBarStack && (
+            <AnimatedBarStack>
               <AnimatedBarSeries
                 dataKey="New York"
                 data={data}
                 xAccessor={accessors.x['New York']}
                 yAccessor={accessors.y['New York']}
               />
-            )}
-            {renderAreaSeries && (
-              <>
-                <AnimatedAreaSeries
-                  dataKey="Austin"
-                  data={data}
-                  xAccessor={accessors.x.Austin}
-                  yAccessor={accessors.y.Austin}
-                  fillOpacity={0.5}
-                  curve={curve}
-                />
-                <AnimatedAreaSeries
-                  dataKey="San Francisco"
-                  data={data}
-                  xAccessor={accessors.x['San Francisco']}
-                  yAccessor={accessors.y['San Francisco']}
-                  fillOpacity={0.5}
-                  curve={curve}
-                />
-              </>
-            )}
-            {renderLineSeries && (
-              <>
-                <AnimatedLineSeries
-                  dataKey="Austin"
-                  data={data}
-                  xAccessor={accessors.x.Austin}
-                  yAccessor={accessors.y.Austin}
-                  curve={curve}
-                />
-                <AnimatedLineSeries
-                  dataKey="San Francisco"
-                  data={data}
-                  xAccessor={accessors.x['San Francisco']}
-                  yAccessor={accessors.y['San Francisco']}
-                  curve={curve}
-                />
-              </>
-            )}
-            {renderGlyphSeries && (
-              <AnimatedGlyphSeries
+              <AnimatedBarSeries
                 dataKey="San Francisco"
                 data={data}
                 xAccessor={accessors.x['San Francisco']}
                 yAccessor={accessors.y['San Francisco']}
-                renderGlyph={renderGlyph}
               />
-            )}
-            <AnimatedAxis
-              key={`time-axis-${animationTrajectory}-${renderHorizontally}`}
-              orientation={renderHorizontally ? yAxisOrientation : xAxisOrientation}
-              numTicks={numTicks}
-              animationTrajectory={animationTrajectory}
-            />
-            <AnimatedAxis
-              key={`temp-axis-${animationTrajectory}-${renderHorizontally}`}
-              label="Temperature (°F)"
-              orientation={renderHorizontally ? xAxisOrientation : yAxisOrientation}
-              numTicks={numTicks}
-              animationTrajectory={animationTrajectory}
-            />
-            {showTooltip && (
-              <Tooltip<CityTemperature>
-                showHorizontalCrosshair={showHorizontalCrosshair}
-                showVerticalCrosshair={showVerticalCrosshair}
-                snapTooltipToDatumX={snapTooltipToDatumX}
-                snapTooltipToDatumY={snapTooltipToDatumY}
-                showDatumGlyph={(snapTooltipToDatumX || snapTooltipToDatumY) && !renderBarGroup}
-                showSeriesGlyphs={sharedTooltip && !renderBarGroup}
-                renderTooltip={({ tooltipData, colorScale }) => (
-                  <>
-                    {/** date */}
-                    {(tooltipData?.nearestDatum?.datum &&
-                      accessors.date(tooltipData?.nearestDatum?.datum)) ||
-                      'No date'}
-                    <br />
-                    <br />
-                    {/** temperatures */}
-                    {((sharedTooltip
-                      ? Object.keys(tooltipData?.datumByKey ?? {})
-                      : [tooltipData?.nearestDatum?.key]
-                    ).filter(city => city) as City[]).map(city => (
-                      <div key={city}>
-                        <em
-                          style={{
-                            color: colorScale?.(city),
-                            textDecoration:
-                              tooltipData?.nearestDatum?.key === city ? 'underline' : undefined,
-                          }}
-                        >
-                          {city}
-                        </em>{' '}
-                        {tooltipData?.nearestDatum?.datum
-                          ? accessors[renderHorizontally ? 'x' : 'y'][city](
-                              tooltipData?.nearestDatum?.datum,
-                            )
-                          : '–'}
-                        ° F
-                      </div>
-                    ))}
-                  </>
-                )}
+              <AnimatedBarSeries
+                dataKey="Austin"
+                data={data}
+                xAccessor={accessors.x.Austin}
+                yAccessor={accessors.y.Austin}
               />
-            )}
-          </XYChart>
-        </DataProvider>
+            </AnimatedBarStack>
+          )}
+          {renderBarGroup && (
+            <AnimatedBarGroup>
+              <AnimatedBarSeries
+                dataKey="New York"
+                data={data}
+                xAccessor={accessors.x['New York']}
+                yAccessor={accessors.y['New York']}
+              />
+              <AnimatedBarSeries
+                dataKey="San Francisco"
+                data={data}
+                xAccessor={accessors.x['San Francisco']}
+                yAccessor={accessors.y['San Francisco']}
+              />
+              <AnimatedBarSeries
+                dataKey="Austin"
+                data={data}
+                xAccessor={accessors.x.Austin}
+                yAccessor={accessors.y.Austin}
+              />
+            </AnimatedBarGroup>
+          )}
+          {renderBarSeries && (
+            <AnimatedBarSeries
+              dataKey="New York"
+              data={data}
+              xAccessor={accessors.x['New York']}
+              yAccessor={accessors.y['New York']}
+            />
+          )}
+          {renderAreaSeries && (
+            <>
+              <AnimatedAreaSeries
+                dataKey="Austin"
+                data={data}
+                xAccessor={accessors.x.Austin}
+                yAccessor={accessors.y.Austin}
+                fillOpacity={0.5}
+                curve={curve}
+              />
+              <AnimatedAreaSeries
+                dataKey="San Francisco"
+                data={data}
+                xAccessor={accessors.x['San Francisco']}
+                yAccessor={accessors.y['San Francisco']}
+                fillOpacity={0.5}
+                curve={curve}
+              />
+            </>
+          )}
+          {renderLineSeries && (
+            <>
+              <AnimatedLineSeries
+                dataKey="Austin"
+                data={data}
+                xAccessor={accessors.x.Austin}
+                yAccessor={accessors.y.Austin}
+                curve={curve}
+              />
+              <AnimatedLineSeries
+                dataKey="San Francisco"
+                data={data}
+                xAccessor={accessors.x['San Francisco']}
+                yAccessor={accessors.y['San Francisco']}
+                curve={curve}
+              />
+            </>
+          )}
+          {renderGlyphSeries && (
+            <AnimatedGlyphSeries
+              dataKey="San Francisco"
+              data={data}
+              xAccessor={accessors.x['San Francisco']}
+              yAccessor={accessors.y['San Francisco']}
+              renderGlyph={renderGlyph}
+            />
+          )}
+          <AnimatedAxis
+            key={`time-axis-${animationTrajectory}-${renderHorizontally}`}
+            orientation={renderHorizontally ? yAxisOrientation : xAxisOrientation}
+            numTicks={numTicks}
+            animationTrajectory={animationTrajectory}
+          />
+          <AnimatedAxis
+            key={`temp-axis-${animationTrajectory}-${renderHorizontally}`}
+            label="Temperature (°F)"
+            orientation={renderHorizontally ? xAxisOrientation : yAxisOrientation}
+            numTicks={numTicks}
+            animationTrajectory={animationTrajectory}
+          />
+          {showTooltip && (
+            <Tooltip<CityTemperature>
+              showHorizontalCrosshair={showHorizontalCrosshair}
+              showVerticalCrosshair={showVerticalCrosshair}
+              snapTooltipToDatumX={snapTooltipToDatumX}
+              snapTooltipToDatumY={snapTooltipToDatumY}
+              showDatumGlyph={(snapTooltipToDatumX || snapTooltipToDatumY) && !renderBarGroup}
+              showSeriesGlyphs={sharedTooltip && !renderBarGroup}
+              renderTooltip={({ tooltipData, colorScale }) => (
+                <>
+                  {/** date */}
+                  {(tooltipData?.nearestDatum?.datum &&
+                    accessors.date(tooltipData?.nearestDatum?.datum)) ||
+                    'No date'}
+                  <br />
+                  <br />
+                  {/** temperatures */}
+                  {((sharedTooltip
+                    ? Object.keys(tooltipData?.datumByKey ?? {})
+                    : [tooltipData?.nearestDatum?.key]
+                  ).filter(city => city) as City[]).map(city => (
+                    <div key={city}>
+                      <em
+                        style={{
+                          color: colorScale?.(city),
+                          textDecoration:
+                            tooltipData?.nearestDatum?.key === city ? 'underline' : undefined,
+                        }}
+                      >
+                        {city}
+                      </em>{' '}
+                      {tooltipData?.nearestDatum?.datum
+                        ? accessors[renderHorizontally ? 'x' : 'y'][city](
+                            tooltipData?.nearestDatum?.datum,
+                          )
+                        : '–'}
+                      ° F
+                    </div>
+                  ))}
+                </>
+              )}
+            />
+          )}
+        </XYChart>
       )}
     </ExampleControls>
   );
--- packages/visx-xychart/src/components/XYChart.tsx
+++ packages/visx-xychart/src/components/XYChart.tsx
@@ -1,6 +1,8 @@
-/* eslint-disable jsx-a11y/mouse-events-have-key-events */
+/* eslint jsx-a11y/mouse-events-have-key-events: 'off', @typescript-eslint/no-explicit-any: 'off' */
 import React, { useCallback, useContext, useEffect } from 'react';
 import ParentSize from '@visx/responsive/lib/components/ParentSize';
+import { AxisScaleOutput } from '@visx/axis';
+import { ScaleConfig } from '@visx/scale';
 
 import DataContext from '../context/DataContext';
 import { Margin } from '../types';
@@ -8,25 +10,45 @@ import useEventEmitter from '../hooks/useEventEmitter';
 import EventEmitterProvider from '../providers/EventEmitterProvider';
 import TooltipContext from '../context/TooltipContext';
 import TooltipProvider from '../providers/TooltipProvider';
+import DataProvider, { DataProviderProps } from '../providers/DataProvider';
 
 const DEFAULT_MARGIN = { top: 50, right: 50, bottom: 50, left: 50 };
 
-type Props = {
+export type XYChartProps<
+  XScaleConfig extends ScaleConfig<AxisScaleOutput, any, any>,
+  YScaleConfig extends ScaleConfig<AxisScaleOutput, any, any>
+> = {
+  /** aria-label for the chart svg element. */
   accessibilityLabel?: string;
-  events?: boolean;
+  /** Total width of the desired chart svg, including margin. */
   width?: number;
+  /** Total height of the desired chart svg, including margin. */
   height?: number;
+  /** Margin to apply around the outside the. */
   margin?: Margin;
+  /** XYChart children (Series, Tooltip, etc.). */
   children: React.ReactNode;
+  /** If DataContext is not available, XYChart will wrap itself in a DataProvider and set this as the theme. */
+  theme?: DataProviderProps<XScaleConfig, YScaleConfig>['theme'];
+  /** If DataContext is not available, XYChart will wrap itself in a DataProvider and set this as the xScale config. */
+  xScale?: DataProviderProps<XScaleConfig, YScaleConfig>['xScale'];
+  /** If DataContext is not available, XYChart will wrap itself in a DataProvider and set this as the yScale config. */
+  yScale?: DataProviderProps<XScaleConfig, YScaleConfig>['yScale'];
 };
 
-export default function XYChart(props: Props) {
+export default function XYChart<
+  XScaleConfig extends ScaleConfig<AxisScaleOutput, any, any>,
+  YScaleConfig extends ScaleConfig<AxisScaleOutput, any, any>
+>(props: XYChartProps<XScaleConfig, YScaleConfig>) {
   const {
     accessibilityLabel = 'XYChart',
     children,
-    width,
     height,
     margin = DEFAULT_MARGIN,
+    theme,
+    width,
+    xScale,
+    yScale,
   } = props;
   const { setDimensions } = useContext(DataContext);
   const tooltipContext = useContext(TooltipContext);
@@ -49,8 +71,36 @@ export default function XYChart(props: Props) {
   );
 
   // if Context or dimensions are not available, wrap self in the needed providers
+  if (!setDimensions) {
+    if (!xScale || !yScale) {
+      console.warn(
+        '[@visx/xychart] XYChart: When no DataProvider is available in context, you must pass xScale & yScale config to XYChart.',
+      );
+      return null;
+    }
+    return (
+      <DataProvider
+        xScale={xScale}
+        yScale={yScale}
+        theme={theme}
+        initialDimensions={{ width, height, margin }}
+      >
+        <XYChart {...props} />
+      </DataProvider>
+    );
+  }
   if (width == null || height == null) {
-    return <ParentSize>{dims => <XYChart {...dims} {...props} />}</ParentSize>;
+    return (
+      <ParentSize>
+        {dims => (
+          <XYChart
+            {...props}
+            width={props.width == null ? dims.width : props.width}
+            height={props.height == null ? dims.height : props.height}
+          />
+        )}
+      </ParentSize>
+    );
   }
   if (emit == null) {
     return (
