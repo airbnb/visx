be548890cbb3381841cf695cbada4c84832fc5dc
Chris Williams <chris.williams@airbnb.com>
2020-11-24 20:07:27 -0800

    new(xychart/useEventEmitter): update to pointer events, add prop annotations, add source filtering

--- packages/visx-xychart/src/hooks/useEventEmitter.ts
+++ packages/visx-xychart/src/hooks/useEventEmitter.ts
@@ -2,10 +2,15 @@ import { useCallback, useContext, useEffect } from 'react';
 import { localPoint } from '@visx/event';
 import EventEmitterContext from '../context/EventEmitterContext';
 
-export type EventType = 'mousemove' | 'mouseout' | 'touchmove' | 'touchend' | 'click';
+export type EventType = 'pointermove' | 'pointerout' | 'pointerup';
+
 export type HandlerParams = {
-  event: React.MouseEvent | React.TouchEvent;
+  /** The react PointerEvent. */
+  event: React.PointerEvent;
+  /** Position of the PointerEvent in svg coordinates. */
   svgPoint: ReturnType<typeof localPoint>;
+  /** The source of the event. This can be anything, but for this package is the name of the component which emitted the event. */
+  source?: string;
 };
 export type Handler = (params?: HandlerParams) => void;
 
@@ -13,23 +18,36 @@ export type Handler = (params?: HandlerParams) => void;
  * Hook for optionally subscribing to a specified EventType,
  * and returns emitter for emitting events.
  */
-export default function useEventEmitter(eventType?: EventType, handler?: Handler) {
+export default function useEventEmitter(
+  /** Type of event to subscribe to. */
+  eventType?: EventType,
+  /** Handler invoked on emission of EventType event.  */
+  handler?: Handler,
+  /** Optional valid sources for EventType subscription. */
+  sources?: string[],
+) {
   const emitter = useContext(EventEmitterContext);
 
-  /** wrap emitter.emit so we can enforce stricter type signature */
+  // wrap emitter.emit so we can enforce stricter type signature and filter events by source
   const emit = useCallback(
-    (type: EventType, event: HandlerParams['event']) =>
-      emitter?.emit<HandlerParams>(type, { event, svgPoint: localPoint(event) }),
+    (type: EventType, event: HandlerParams['event'], source?: string) => {
+      emitter?.emit<HandlerParams>(type, { event, svgPoint: localPoint(event), source });
+    },
     [emitter],
   );
 
   useEffect(() => {
     if (emitter && eventType && handler) {
-      emitter.on<HandlerParams>(eventType, handler);
-      return () => emitter?.off<HandlerParams>(eventType, handler);
+      const handlerWithSourceFilter: Handler = sources
+        ? (params?: HandlerParams) => {
+            if (!params?.source || sources.includes(params.source)) handler(params);
+          }
+        : handler;
+      emitter.on<HandlerParams>(eventType, handlerWithSourceFilter);
+      return () => emitter?.off<HandlerParams>(eventType, handlerWithSourceFilter);
     }
     return undefined;
-  }, [emitter, eventType, handler]);
+  }, [emitter, eventType, handler, sources]);
 
   return emitter ? emit : null;
 }
