904ee003d42fef4eb951800f7dc4c1a234f2f5a1
Kristján Oddsson <koddsson@gmail.com>
2020-11-27 10:16:26 +0000

    Ignore ESLint error in types file

--- packages/visx-responsive/src/types/index.ts
+++ packages/visx-responsive/src/types/index.ts
@@ -9,6 +9,7 @@ interface ResizeObserverEntry {
 }
 type ResizeObserverCallback = (entries: ResizeObserverEntry[]) => void;
 export interface ResizeObserver {
+  // eslint-disable-next-line @typescript-eslint/no-misused-new
   new (callback: ResizeObserverCallback): ResizeObserver;
   observe(el: Element): void;
   disconnect(): void;
