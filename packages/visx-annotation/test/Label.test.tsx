83b6c01ef736fe8714222c3850a62dbd264ade65
Kyle Lanmon <kyle.lanmon@gmail.com>
2020-12-10 04:33:24 +0000

    :white_check_mark: change Label to use React Testing Library

--- packages/visx-annotation/test/Label.test.tsx
+++ packages/visx-annotation/test/Label.test.tsx
@@ -1,48 +1,49 @@
-import React from 'react';
-import ResizeObserver from 'resize-observer-polyfill';
-import Text from '@visx/text/lib/Text';
-import { shallow } from 'enzyme';
-import { Label } from '../src';
+import React from "react";
+import ResizeObserver from "resize-observer-polyfill";
+import { Label } from "../src";
+import { render, screen } from "@testing-library/react";
 
-describe('<Label />', () => {
-  it('should be defined', () => {
-    expect(Label).toBeDefined();
+describe("<Label />", () => {
+  const wrapper: React.FunctionComponent = ({ children }) => (
+    <svg>{children}</svg>
+  );
+  it("should render title Text", () => {
+    render(
+      <Label title="title test" resizeObserverPolyfill={ResizeObserver} />,
+      { wrapper }
+    );
+    expect(screen.getByText("title test")).toBeVisible();
   });
-  it('should render title Text', () => {
-    expect(
-      shallow(<Label title="title test" resizeObserverPolyfill={ResizeObserver} />)
-        .children()
-        .find(Text)
-        .prop('children'),
-    ).toBe('title test');
+  it("should render subtitle Text", () => {
+    render(
+      <Label
+        subtitle="subtitle test"
+        resizeObserverPolyfill={ResizeObserver}
+      />,
+      { wrapper }
+    );
+    expect(screen.getByText("subtitle test")).toBeVisible();
   });
-  it('should render subtitle Text', () => {
-    expect(
-      shallow(
-        <Label
-          title="title test"
-          subtitle="subtitle test"
-          resizeObserverPolyfill={ResizeObserver}
-        />,
-      )
-        .children()
-        .find(Text)
-        .at(1)
-        .prop('children'),
-    ).toBe('subtitle test');
+  it("should render a background", () => {
+    render(
+      <Label
+        title="test"
+        showBackground
+        resizeObserverPolyfill={ResizeObserver}
+      />,
+      { wrapper }
+    );
+    expect(document.querySelector("rect")).toBeVisible();
   });
-  it('should render a background', () => {
-    expect(
-      shallow(
-        <Label title="title test" showBackground resizeObserverPolyfill={ResizeObserver} />,
-      ).find('rect'),
-    ).toHaveLength(1);
-  });
-  it('should render an anchor line', () => {
-    expect(
-      shallow(
-        <Label title="title test" showAnchorLine resizeObserverPolyfill={ResizeObserver} />,
-      ).find('line'),
-    ).toHaveLength(1);
+  it("should render an anchor line", () => {
+    render(
+      <Label
+        title="test"
+        showAnchorLine
+        resizeObserverPolyfill={ResizeObserver}
+      />,
+      { wrapper }
+    );
+    expect(document.querySelector("line")).toBeVisible();
   });
 });
