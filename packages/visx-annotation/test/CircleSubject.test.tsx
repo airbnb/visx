d6ea4341145460373ba2a00cade0ef0d4f797308
Kyle Lanmon <kyle.lanmon@gmail.com>
2020-12-10 03:37:40 +0000

    :white_check_mark: change CircleSubject to use React Testing Library

--- packages/visx-annotation/test/CircleSubject.test.tsx
+++ packages/visx-annotation/test/CircleSubject.test.tsx
@@ -1,12 +1,45 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import { CircleSubject } from '../src';
+import React from "react";
+import { AnnotationContext, CircleSubject } from "../src";
+import { render } from "@testing-library/react";
 
-describe('<CircleSubject />', () => {
-  it('should be defined', () => {
-    expect(CircleSubject).toBeDefined();
+describe("<CircleSubject />", () => {
+  const wrapper: React.FunctionComponent = ({ children }) => (
+    <svg>{children}</svg>
+  );
+  it("should render a cirlce", () => {
+    render(<CircleSubject />, { wrapper });
+
+    expect(document.querySelector("circle")).toBeVisible();
   });
-  it('should render a cirlce', () => {
-    expect(shallow(<CircleSubject />).find('circle')).toHaveLength(1);
+  it("should have default attributes", () => {
+    const context = { x: -50, y: 100, dx: 1000, dy: 7 };
+    render(
+      <AnnotationContext.Provider value={context}>
+        <CircleSubject />
+      </AnnotationContext.Provider>,
+      { wrapper }
+    );
+
+    const circle = document.querySelector("circle");
+    expect(circle).toHaveAttribute("cx", `${context.x}`);
+    expect(circle).toHaveAttribute("cy", `${context.y}`);
+    expect(circle).toHaveAttribute("r", "16");
+    expect(circle).toHaveAttribute("stroke", "#222");
+  });
+  it("should override default attributes with props", () => {
+    const context = { x: -50, y: 100, dx: 1000, dy: 7 };
+    const props = { x: 1, y: 2, stroke: "#123", radius: 3 };
+    render(
+      <AnnotationContext.Provider value={context}>
+        <CircleSubject {...props} />
+      </AnnotationContext.Provider>,
+      { wrapper }
+    );
+
+    const circle = document.querySelector("circle");
+    expect(circle).toHaveAttribute("cx", `${props.x}`);
+    expect(circle).toHaveAttribute("cy", `${props.y}`);
+    expect(circle).toHaveAttribute("r", `${props.radius}`);
+    expect(circle).toHaveAttribute("stroke", `${props.stroke}`);
   });
 });
