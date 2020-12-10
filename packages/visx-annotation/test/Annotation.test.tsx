3ead4d27fc1b1e38f301546132bed5803ca5c0cc
Kyle Lanmon <kyle.lanmon@gmail.com>
2020-12-10 02:59:52 +0000

    :white_check_mark: change Annotation tests to use React Testing Library

--- packages/visx-annotation/test/Annotation.test.tsx
+++ packages/visx-annotation/test/Annotation.test.tsx
@@ -1,25 +1,33 @@
-import { mount } from 'enzyme';
-import React, { useContext } from 'react';
-import { Annotation } from '../src';
-import AnnotationContext from '../src/context/AnnotationContext';
+import React, { useContext } from "react";
+import { Annotation } from "../src";
+import AnnotationContext from "../src/context/AnnotationContext";
+import { render, screen } from "@testing-library/react";
 
-describe('<Annotation />', () => {
-  it('should be defined', () => {
-    expect(Annotation).toBeDefined();
-  });
-  it('should provide AnnotationContext', () => {
-    expect.assertions(1);
-    const annotation = { x: -50, y: 100, dx: 1000, dy: 7 };
+describe("<Annotation /> RTL", () => {
+  it("should provide AnnotationContext", () => {
     function AnnotationChild() {
-      const annotationContext = useContext(AnnotationContext);
-      expect(annotationContext).toEqual(annotation);
-      return null;
+      const { x, y, dx, dy } = useContext(AnnotationContext);
+      return (
+        <>
+          <p data-testid="x">{x}</p>
+          <p data-testid="y">{y}</p>
+          <p data-testid="dx">{dx}</p>
+          <p data-testid="dy">{dy}</p>
+        </>
+      );
     }
 
-    mount(
+    const annotation = { x: -50, y: 100, dx: 1000, dy: 7 };
+
+    render(
       <Annotation {...annotation}>
         <AnnotationChild />
-      </Annotation>,
+      </Annotation>
     );
+
+    expect(screen.getByTestId("x")).toHaveTextContent(`${annotation.x}`);
+    expect(screen.getByTestId("y")).toHaveTextContent(`${annotation.y}`);
+    expect(screen.getByTestId("dx")).toHaveTextContent(`${annotation.dx}`);
+    expect(screen.getByTestId("dy")).toHaveTextContent(`${annotation.dy}`);
   });
 });
