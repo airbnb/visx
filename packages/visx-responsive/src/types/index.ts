178e7130b0b6ee0de3f13cfdc78d1f6b9a99c17a
susiwen8 <susiwen8@gmail.com>
2020-12-28 18:50:53 +0800

    chore: move types into right file

--- packages/visx-responsive/src/types/index.ts
+++ packages/visx-responsive/src/types/index.ts
@@ -14,3 +14,16 @@ export interface ResizeObserver {
   observe(el: Element): void;
   disconnect(): void;
 }
+
+export type WithSizeProps = {
+  debounceTime?: number;
+  enableDebounceLeadingCall?: boolean;
+};
+
+export type WithSizeProvidedProps = {
+  width?: number;
+  screenWidth?: number;
+  height?: number;
+  initWidth?: number;
+  initHeight?: number;
+};
