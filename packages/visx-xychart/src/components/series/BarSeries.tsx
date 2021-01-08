afc0b0ec30631c6a335f0093e448adcc26dfcb1b
Chris Williams <chris.williams@airbnb.com>
2021-01-08 11:55:58 -0800

    type(xychart): fix colorAccessor generics in BarSeries

--- packages/visx-xychart/src/components/series/BarSeries.tsx
+++ packages/visx-xychart/src/components/series/BarSeries.tsx
@@ -3,10 +3,19 @@ import React from 'react';
 import BaseBarSeries, { BaseBarSeriesProps } from './private/BaseBarSeries';
 import Bars from './private/Bars';
 
-function BarSeries<XScale extends AxisScale, YScale extends AxisScale, Datum extends object>(
-  props: Omit<BaseBarSeriesProps<XScale, YScale, Datum>, 'BarsComponent'>,
-) {
-  return <BaseBarSeries<XScale, YScale, Datum> {...props} BarsComponent={Bars} />;
+function BarSeries<XScale extends AxisScale, YScale extends AxisScale, Datum extends object>({
+  colorAccessor,
+  ...props
+}: Omit<BaseBarSeriesProps<XScale, YScale, Datum>, 'BarsComponent'>) {
+  return (
+    <BaseBarSeries<XScale, YScale, Datum>
+      {...props}
+      // @TODO currently generics for non-SeriesProps are not passed correctly in
+      // withRegisteredData HOC
+      colorAccessor={colorAccessor as BaseBarSeriesProps<XScale, YScale, object>['colorAccessor']}
+      BarsComponent={Bars}
+    />
+  );
 }
 
 export default BarSeries;
