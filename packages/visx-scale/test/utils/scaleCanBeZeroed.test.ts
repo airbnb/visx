18f2d6fc1d72ea566d39657c1e86c2dcd3778bfa
Chris Williams <chris.williams@airbnb.com>
2021-01-08 13:14:37 -0800

    test(scale/scaleCanBeZeroed): DRY tests

--- packages/visx-scale/test/utils/scaleCanBeZeroed.test.ts
+++ packages/visx-scale/test/utils/scaleCanBeZeroed.test.ts
@@ -2,21 +2,23 @@ import scaleCanBeZeroed from '../../src/utils/scaleCanBeZeroed';
 
 describe('scaleCanBeZeroed(scaleConfig)', () => {
   it('returns true for zero-able scales', () => {
-    expect(scaleCanBeZeroed({ type: 'linear' })).toBe(true);
-    expect(scaleCanBeZeroed({ type: 'pow' })).toBe(true);
-    expect(scaleCanBeZeroed({ type: 'quantize' })).toBe(true);
-    expect(scaleCanBeZeroed({ type: 'sqrt' })).toBe(true);
-    expect(scaleCanBeZeroed({ type: 'symlog' })).toBe(true);
+    const zeroAble = ['linear', 'pow', 'quantize', 'sqrt', 'symlog'] as const;
+    expect.assertions(zeroAble.length);
+    zeroAble.forEach(type => expect(scaleCanBeZeroed({ type })).toBe(true));
   });
   it('returns false for non-zero-able scales', () => {
-    expect(scaleCanBeZeroed({ type: 'log' })).toBe(false);
-    expect(scaleCanBeZeroed({ type: 'radial' })).toBe(false);
-    expect(scaleCanBeZeroed({ type: 'time' })).toBe(false);
-    expect(scaleCanBeZeroed({ type: 'utc' })).toBe(false);
-    expect(scaleCanBeZeroed({ type: 'quantile' })).toBe(false);
-    expect(scaleCanBeZeroed({ type: 'threshold' })).toBe(false);
-    expect(scaleCanBeZeroed({ type: 'ordinal' })).toBe(false);
-    expect(scaleCanBeZeroed({ type: 'point' })).toBe(false);
-    expect(scaleCanBeZeroed({ type: 'band' })).toBe(false);
+    const notZeroAble = [
+      'log',
+      'radial',
+      'time',
+      'utc',
+      'quantile',
+      'threshold',
+      'ordinal',
+      'point',
+      'band',
+    ] as const;
+    expect.assertions(notZeroAble.length);
+    notZeroAble.forEach(type => expect(scaleCanBeZeroed({ type })).toBe(false));
   });
 });
