d9ffc251dd67861e61adca0b32808ec33d26823e
Chris Williams <chris.williams@airbnb.com>
2021-01-12 18:45:36 -0800

    new(xychart): add working polished AreaStack, AnimatedAreaStack

---
+++ packages/visx-xychart/src/components/series/AnimatedAreaStack.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { AxisScale } from '@visx/axis/lib/types';
+import BaseAreaStack, { BaseAreaStackProps } from './private/BaseAreaStack';
+import AnimatedPath from './private/AnimatedPath';
+
+export default function AnimatedAreaStack<
+  XScale extends AxisScale,
+  YScale extends AxisScale,
+  Datum extends object
+>(props: Omit<BaseAreaStackProps<XScale, YScale, Datum>, 'PathComponent'>) {
+  return <BaseAreaStack<XScale, YScale, Datum> {...props} PathComponent={AnimatedPath} />;
+}
