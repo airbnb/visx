d8ae983b5dd913754b4f99108ca4f5f31f15731d
Chris Williams <chris.williams@airbnb.com>
2021-01-25 12:50:00 -0800

    internal(demo): use stable randomness for all demos

--- packages/visx-demo/src/sandboxes/visx-drag-i/generateCircles.ts
+++ packages/visx-demo/src/sandboxes/visx-drag-i/generateCircles.ts
@@ -1,3 +1,5 @@
+import { getSeededRandom } from '@visx/mock-data';
+
 export interface Circle {
   id: string;
   radius: number;
@@ -5,15 +7,20 @@ export interface Circle {
   y: number;
 }
 
-const generateCircles = ({ width, height }: { width: number; height: number }) =>
-  new Array(width < 360 ? 40 : 185).fill(1).map((d, i) => {
-    const radius = 25 - Math.random() * 20;
+const generateCircles = ({ width, height }: { width: number; height: number }) => {
+  const radiusRandom = getSeededRandom(0.2);
+  const xRandom = getSeededRandom(0.3);
+  const yRandom = getSeededRandom(0.4);
+
+  return new Array(width < 360 ? 40 : 185).fill(1).map((d, i) => {
+    const radius = 25 - radiusRandom() * 20;
     return {
       id: `${i}`,
       radius,
-      x: Math.round(Math.random() * (width - radius * 2) + radius),
-      y: Math.round(Math.random() * (height - radius * 2) + radius),
+      x: Math.round(xRandom() * (width - radius * 2) + radius),
+      y: Math.round(yRandom() * (height - radius * 2) + radius),
     };
   });
+};
 
 export default generateCircles;
