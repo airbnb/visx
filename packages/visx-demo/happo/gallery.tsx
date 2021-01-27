67bc021aaa12a1ca5f1d2c5efb99b0f7be24cdae
Chris Williams <chris.williams@airbnb.com>
2021-01-27 14:09:56 -0800

    internal(happo): remove special case references

--- packages/visx-demo/happo/gallery.tsx
+++ packages/visx-demo/happo/gallery.tsx
@@ -10,17 +10,13 @@ type HappoSnapshot = {
   };
 };
 
-const specialCases = new Set(['@visx/demo-xychart', '@visx/demo-axis']);
-
 const getComponentName = (Example: typeof examples[0]) =>
   Example.packageJson.name || 'missing-name';
 
-const snapshots: HappoSnapshot[] = examples
-  .filter(Example => specialCases.has(getComponentName(Example)))
-  .map(Example => ({
-    // note: this (reasonably) asserts Examples have unique names
-    component: getComponentName(Example),
-    variants: { default: () => <Example.default /> },
-  }));
+const snapshots: HappoSnapshot[] = examples.map(Example => ({
+  // note: this (reasonably) asserts Examples have unique names
+  component: getComponentName(Example),
+  variants: { default: () => <Example.default /> },
+}));
 
 export default snapshots;
