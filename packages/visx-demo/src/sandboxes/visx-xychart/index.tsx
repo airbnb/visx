ad5ec3cac9c445901365edbdad000cd99c18b0ef
Chris Williams <chris.williams@airbnb.com>
2021-01-26 19:57:08 -0800

    fix(demo/xychart): remove unused props

--- packages/visx-demo/src/sandboxes/visx-xychart/index.tsx
+++ packages/visx-demo/src/sandboxes/visx-xychart/index.tsx
@@ -5,14 +5,7 @@ import ParentSize from '@visx/responsive/lib/components/ParentSize';
 import Example from './Example';
 import './sandbox-styles.css';
 
-const prefersReducedMotionQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
-const prefersReducedMotion = !prefersReducedMotionQuery || !!prefersReducedMotionQuery.matches;
-
 render(
-  <ParentSize>
-    {({ width, height }) => (
-      <Example width={width} height={height} prefersReducedMotion={prefersReducedMotion} />
-    )}
-  </ParentSize>,
+  <ParentSize>{({ width, height }) => <Example width={width} height={height} />}</ParentSize>,
   document.getElementById('root'),
 );
