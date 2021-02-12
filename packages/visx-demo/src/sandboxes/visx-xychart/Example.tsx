d5ecf4bb032063bdd1726b75d4a8c829d3a31a36
Chris Williams <chris.williams@airbnb.com>
2021-02-11 20:03:56 -0800

    fix(demo/xychart): handle missing values in tooltip

--- packages/visx-demo/src/sandboxes/visx-xychart/Example.tsx
+++ packages/visx-demo/src/sandboxes/visx-xychart/Example.tsx
@@ -304,25 +304,30 @@ export default function Example({ height }: XYChartProps) {
                   {((sharedTooltip
                     ? Object.keys(tooltipData?.datumByKey ?? {})
                     : [tooltipData?.nearestDatum?.key]
-                  ).filter(city => city) as City[]).map(city => (
-                    <div key={city}>
-                      <em
-                        style={{
-                          color: colorScale?.(city),
-                          textDecoration:
-                            tooltipData?.nearestDatum?.key === city ? 'underline' : undefined,
-                        }}
-                      >
-                        {city}
-                      </em>{' '}
-                      {tooltipData?.nearestDatum?.datum
-                        ? accessors[renderHorizontally ? 'x' : 'y'][city](
-                            tooltipData?.nearestDatum?.datum,
-                          )
-                        : '–'}
-                      ° F
-                    </div>
-                  ))}
+                  ).filter(city => city) as City[]).map(city => {
+                    const temperature =
+                      tooltipData?.nearestDatum?.datum &&
+                      accessors[renderHorizontally ? 'x' : 'y'][city](
+                        tooltipData?.nearestDatum?.datum,
+                      );
+
+                    return (
+                      <div key={city}>
+                        <em
+                          style={{
+                            color: colorScale?.(city),
+                            textDecoration:
+                              tooltipData?.nearestDatum?.key === city ? 'underline' : undefined,
+                          }}
+                        >
+                          {city}
+                        </em>{' '}
+                        {temperature == null || Number.isNaN(temperature)
+                          ? '–'
+                          : `${temperature}° F`}
+                      </div>
+                    );
+                  })}
                 </>
               )}
             />
