22478a15162d22840b5bcb03a0bdcb3cf12f0879
Chris Williams <chris.williams@airbnb.com>
2021-02-17 16:39:28 -0800

    type(xychart/AreaSeries, AnimatedAreaSeries): fix generic

--- packages/visx-xychart/src/components/series/AnimatedAreaSeries.tsx
+++ packages/visx-xychart/src/components/series/AnimatedAreaSeries.tsx
@@ -8,5 +8,7 @@ export default function AnimatedAreaSeries<
   YScale extends AxisScale,
   Datum extends object
 >(props: Omit<BaseAreaSeriesProps<XScale, YScale, Datum>, 'PathComponent'>) {
+  // @TODO currently generics for non-SeriesProps are not passed correctly in withRegisteredData HOC
+  // @ts-expect-error
   return <BaseAreaSeries<XScale, YScale, Datum> {...props} PathComponent={AnimatedPath} />;
 }
