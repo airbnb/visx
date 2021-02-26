3c1ad08b1878111767e656e3ecafcbbc54680f74
Chris Williams <chris.williams@airbnb.com>
2021-02-25 20:39:08 -0800

    fix(workflows/release): set registry config in script

--- scripts/performRelease/performLernaRelease.ts
+++ scripts/performRelease/performLernaRelease.ts
@@ -35,6 +35,7 @@ export default async function performLernaRelease(prsSinceLastTag: PR[]) {
     }`;
     console.log(`Attempting to publish a '${version}' release.`);
 
+    await exec(`npm config set '//registry.npmjs.org/:_authToken=${process.env.NODE_AUTH_TOKEN}'`);
     const whoami = await exec('npm whoami');
     console.log(whoami.stdout);
 
