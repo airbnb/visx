2644c69d29f136e0fb5a933709402579aa12c51f
Chris Williams <chris.williams@airbnb.com>
2021-03-04 12:07:01 -0800

    fix(xychart/Axis): set default scale generic, export AxisScale type

--- packages/visx-xychart/src/components/axis/Axis.tsx
+++ packages/visx-xychart/src/components/axis/Axis.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import { Axis as VisxAxis, AxisScale } from '@visx/axis';
+import { Axis as VisxAxis, AxisScale as AxisAxisScale } from '@visx/axis';
 import BaseAxis, { BaseAxisProps } from './BaseAxis';
 
-export type AxisProps<Scale extends AxisScale> = Omit<BaseAxisProps<Scale>, 'AxisComponent'>;
+export type AxisScale = AxisAxisScale;
 
-export default function Axis<Scale extends AxisScale>(props: AxisProps<Scale>) {
+export type AxisProps<Scale extends AxisScale = AxisScale> = Omit<
+  BaseAxisProps<Scale>,
+  'AxisComponent'
+>;
+
+export default function Axis<Scale extends AxisScale = AxisScale>(props: AxisProps<Scale>) {
   return <BaseAxis<Scale> AxisComponent={VisxAxis} {...props} />;
 }
