7f0bb8abb23e282c2e1adb76178cd8fd18446009
Krist Wongsuphasawat <krist.wongz@gmail.com>
2021-06-30 14:23:12 -0700

    docs: update example

--- packages/visx-demo/src/sandboxes/visx-shape-splitlinepath/Example.tsx
+++ packages/visx-demo/src/sandboxes/visx-shape-splitlinepath/Example.tsx
@@ -21,7 +21,20 @@ export type SplitLinePathExampleProps = {
   numberOfSegments?: number;
 };
 
-const CustomSegment: SplitLinePathChildren = ({ segment, styles }) => (
+const NumberSegment: SplitLinePathChildren = ({ segment, styles }) => (
+  <g>
+    {segment.map(({ x, y }, i) =>
+      i % 10 === 0 ? (
+        <text key={i} x={x} y={y} dy={3} fontSize={8} textAnchor="middle">
+          {i}
+        </text>
+      ) : null,
+    )}
+  </g>
+);
+
+/** Overlay growing circles instead of drawing a line */
+const CircleSegment: SplitLinePathChildren = ({ segment, styles }) => (
   <g>
     {segment.map(({ x, y }, i) =>
       i % 8 === 0 ? (
@@ -39,6 +52,8 @@ const CustomSegment: SplitLinePathChildren = ({ segment, styles }) => (
   </g>
 );
 
+const PADDING = 20;
+
 export default function SplitLinePathExample({
   width,
   height,
@@ -48,17 +63,31 @@ export default function SplitLinePathExample({
   const data = useMemo(
     () => ({
       leftToRight: generateSinSegments({
-        width: width / 2,
-        height: height / 2,
+        width: width / 2 - PADDING * 2,
+        height: height / 2 - PADDING * 2,
         numberOfWaves,
         pointsPerWave,
       }),
       rightToLeft: generateSinSegments({
-        width: width / 2,
-        height: height / 2,
+        width: width / 2 - PADDING * 2,
+        height: height / 2 - PADDING * 2,
         numberOfWaves,
         pointsPerWave,
         direction: 'right-to-left',
+      }),
+      topToBottom: generateSinSegments({
+        width: width / 2 - PADDING * 2,
+        height: height / 2 - PADDING * 2,
+        numberOfWaves,
+        pointsPerWave,
+        direction: 'top-to-bottom',
+      }),
+      bottomToTop: generateSinSegments({
+        width: width / 2 - PADDING * 2,
+        height: height / 2 - PADDING * 2,
+        numberOfWaves,
+        pointsPerWave,
+        direction: 'bottom-to-top',
       }),
     }),
     [width, height, numberOfWaves, pointsPerWave],
@@ -85,7 +114,8 @@ export default function SplitLinePathExample({
         />
 
         {/* left to right */}
-        <g transform={`translate(${width / 2}, ${height / 4})`}>
+        <g transform={`translate(${PADDING}, ${height / 4})`}>
+          {/* Render all segments as a single line for comparison */}
           <LinePath
             data={data.leftToRight.flat()}
             x={getX}
@@ -109,9 +139,8 @@ export default function SplitLinePathExample({
             ]}
           >
             {({ segment, styles, index }) =>
-              /** overlay circles to a couple of the segments */
               index === numberOfWaves - 1 || index === 2 ? (
-                <CustomSegment segment={segment} styles={styles} index={index} />
+                <CircleSegment segment={segment} styles={styles} index={index} />
               ) : (
                 <LinePath data={segment} x={getX} y={getY} {...styles} />
               )
@@ -120,7 +149,8 @@ export default function SplitLinePathExample({
         </g>
 
         {/* right to left */}
-        <g transform={`translate(${width}, ${(height * 3) / 4})`}>
+        <g transform={`translate(${width / 2 - PADDING}, ${(height * 3) / 4})`}>
+          {/* Render all segments as a single line for comparison */}
           <LinePath
             data={data.rightToLeft.flat()}
             x={getX}
@@ -144,9 +174,80 @@ export default function SplitLinePathExample({
             ]}
           >
             {({ segment, styles, index }) =>
-              /** overlay circles to a couple of the segments */
               index === numberOfWaves - 1 || index === 2 ? (
-                <CustomSegment segment={segment} styles={styles} index={index} />
+                <NumberSegment segment={segment} styles={styles} index={index} />
+              ) : (
+                <LinePath data={segment} x={getX} y={getY} {...styles} />
+              )
+            }
+          </SplitLinePath>
+        </g>
+
+        {/* top to bottom */}
+        <g transform={`translate(${(width * 3) / 4}, ${PADDING})`}>
+          {/* Render all segments as a single line for comparison */}
+          <LinePath
+            data={data.topToBottom.flat()}
+            x={getX}
+            y={getY}
+            strokeWidth={8}
+            stroke="#fff"
+            strokeOpacity={0.15}
+            curve={curveCardinal}
+          />
+
+          <SplitLinePath
+            sampleRate={1}
+            segments={data.topToBottom}
+            segmentation="y"
+            x={getX}
+            y={getY}
+            curve={curveCardinal}
+            styles={[
+              { stroke: foreground, strokeWidth: 3 },
+              { stroke: '#fff', strokeWidth: 2, strokeDasharray: '9,5' },
+              { stroke: background, strokeWidth: 2 },
+            ]}
+          >
+            {({ segment, styles, index }) =>
+              index === numberOfWaves - 1 || index === 2 ? (
+                <CircleSegment segment={segment} styles={styles} index={index} />
+              ) : (
+                <LinePath data={segment} x={getX} y={getY} {...styles} />
+              )
+            }
+          </SplitLinePath>
+        </g>
+
+        {/* bottom to top */}
+        <g transform={`translate(${(width * 3) / 4}, ${height - PADDING})`}>
+          {/* Render all segments as a single line for comparison */}
+          <LinePath
+            data={data.bottomToTop.flat()}
+            x={getX}
+            y={getY}
+            strokeWidth={8}
+            stroke="#fff"
+            strokeOpacity={0.15}
+            curve={curveCardinal}
+          />
+
+          <SplitLinePath
+            sampleRate={1}
+            segments={data.bottomToTop}
+            segmentation="y"
+            x={getX}
+            y={getY}
+            curve={curveCardinal}
+            styles={[
+              { stroke: foreground, strokeWidth: 3 },
+              { stroke: '#fff', strokeWidth: 2, strokeDasharray: '9,5' },
+              { stroke: background, strokeWidth: 2 },
+            ]}
+          >
+            {({ segment, styles, index }) =>
+              index === numberOfWaves - 1 || index === 2 ? (
+                <CircleSegment segment={segment} styles={styles} index={index} />
               ) : (
                 <LinePath data={segment} x={getX} y={getY} {...styles} />
               )
