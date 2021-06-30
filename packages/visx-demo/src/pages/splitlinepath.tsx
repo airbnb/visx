6038cdab466a90686cac9b518f9bad6eb25a07c3
Krist Wongsuphasawat <krist.wongz@gmail.com>
2021-06-30 15:33:39 -0700

    docs: rename example

--- packages/visx-demo/src/pages/splitlinepath.tsx
+++ packages/visx-demo/src/pages/splitlinepath.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Show from '../components/Show';
-import SplitLinePath from '../sandboxes/visx-shape-splitlinepath/Example';
+import SplitLinePathExample from '../sandboxes/visx-shape-splitlinepath/Example';
 import StatsPlotSource from '!!raw-loader!../sandboxes/visx-shape-splitlinepath/Example';
 import packageJson from '../sandboxes/visx-shape-splitlinepath/package.json';
 
 const SplitLinePathPage = () => (
   <Show
     events
-    component={SplitLinePath}
+    component={SplitLinePathExample}
     title="SplitLinePath"
     codeSandboxDirectoryName="visx-shape-splitlinepath"
     packageJson={packageJson}
