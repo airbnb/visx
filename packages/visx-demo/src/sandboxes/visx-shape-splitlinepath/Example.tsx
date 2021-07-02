15e3f8cd33c4df1982a2f70487e367c6fac87d7c
Krist Wongsuphasawat <krist.wongz@gmail.com>
2021-07-02 12:38:16 -0700

    feat(shape): add more segmentation for SplitLinePath (#1261)
    * feat(shape): add more segmentation for SplitLinePath
    
    * docs: update SplitLinePath example
    
    * docs: update example
    
    * docs: rename example
    
    * refactor: getOrCreateMeasurementElement
    
    * docs: update example
    
    * fix: typings
    
    * fix: split by length
    
    * docs: update comment
    
    * fix: remove console.log
    
    * refactor: rename
    
    * fix: Update packages/visx-demo/src/sandboxes/visx-shape-splitlinepath/generateSnakePath.ts
    
    Co-authored-by: Chris Williams <williaster@users.noreply.github.com>
    
    * Update packages/visx-shape/src/util/getSplitLineSegments.ts
    
    Co-authored-by: Chris Williams <williaster@users.noreply.github.com>
    
    * refactor: rename variables
    
    * chore: add lodash dependency
    
    Co-authored-by: Chris Williams <williaster@users.noreply.github.com>

--- packages/visx-demo/src/sandboxes/visx-shape-splitlinepath/Example.tsx
+++ packages/visx-demo/src/sandboxes/visx-shape-splitlinepath/Example.tsx
@@ -1,10 +1,11 @@
 import React, { useMemo } from 'react';
-import { scaleLinear } from '@visx/scale';
+import { chunk } from 'lodash';
 import { curveCardinal } from '@visx/curve';
 import { LinePath, SplitLinePath } from '@visx/shape';
 import { LinearGradient } from '@visx/gradient';
-
-import generateSinPoints from './generateSinPoints';
+import { SplitLinePathRenderer } from '@visx/shape/lib/shapes/SplitLinePath';
+import generateSinSegments from './generateSinSegments';
+import generateSnakePath from './generateSnakePath';
 
 type Point = { x: number; y: number };
 const getX = (d: Point) => d.x;
@@ -13,7 +14,7 @@ export const background = '#045275';
 export const backgroundLight = '#089099';
 export const foreground = '#b7e6a5';
 
-export type SplitLinePathProps = {
+export type SplitLinePathExampleProps = {
   width: number;
   height: number;
   margin?: { top: number; right: number; bottom: number; left: number };
@@ -22,40 +23,86 @@ export type SplitLinePathProps = {
   numberOfSegments?: number;
 };
 
-export default function SplitPath({
+const renderNumberSegment: SplitLinePathRenderer = ({ segment, styles }) => (
+  <g>
+    {segment.map(({ x, y }, i) =>
+      i % 25 === 0 ? (
+        <g transform={`translate(${x},${y})`}>
+          <circle r={2} fill="#222" />
+          <text key={i} dx={3} dy={3} fontSize={8}>
+            {i}
+          </text>
+        </g>
+      ) : null,
+    )}
+  </g>
+);
+
+/** Overlay growing circles instead of drawing a line */
+const renderCircleSegment: SplitLinePathRenderer = ({ segment, styles }) => (
+  <g>
+    {segment.map(({ x, y }, i) =>
+      i % 8 === 0 ? (
+        <circle
+          key={i}
+          cx={x}
+          cy={y}
+          r={10 * (i / segment.length)}
+          stroke={styles?.stroke}
+          fill="transparent"
+          strokeWidth={1}
+        />
+      ) : null,
+    )}
+  </g>
+);
+
+const PADDING = 30;
+
+export default function SplitLinePathExample({
   width,
   height,
   numberOfWaves = 10,
   pointsPerWave = 100,
-  numberOfSegments = 8,
-}: SplitLinePathProps) {
-  const data = useMemo(() => generateSinPoints({ width, height, numberOfWaves, pointsPerWave }), [
-    width,
-    height,
-    numberOfWaves,
-    pointsPerWave,
-  ]);
-
-  const dividedData = useMemo(() => {
-    const segmentLength = Math.floor(data.length / numberOfSegments);
-    return new Array(numberOfSegments)
-      .fill(null)
-      .map((_, i) => data.slice(i * segmentLength, (i + 1) * segmentLength));
-  }, [numberOfSegments, data]);
-
-  const getScaledX = useMemo(() => {
-    const xScale = scaleLinear({ range: [0, width], domain: [0, width] });
-    return (d: Point) => xScale(getX(d)) ?? 0;
-  }, [width]);
-
-  const getScaledY = useMemo(() => {
-    const yScale = scaleLinear({ range: [0, height], domain: [height, 0] });
-    return (d: Point) => yScale(getY(d)) ?? 0;
-  }, [height]);
+}: SplitLinePathExampleProps) {
+  const data = useMemo(
+    () => ({
+      leftToRight: generateSinSegments({
+        width: width / 2 - PADDING * 2,
+        height: height / 2 - PADDING * 2,
+        numberOfWaves,
+        pointsPerWave,
+      }),
+      rightToLeft: generateSinSegments({
+        width: width / 2 - PADDING * 2,
+        height: height / 2 - PADDING * 2,
+        numberOfWaves,
+        pointsPerWave,
+        direction: 'right-to-left',
+      }),
+      topToBottom: generateSinSegments({
+        width: width / 2 - PADDING * 2,
+        height: height / 2 - PADDING * 2,
+        numberOfWaves: 5,
+        pointsPerWave,
+        direction: 'top-to-bottom',
+      }),
+      bottomToTop: generateSinSegments({
+        width: width / 2 - PADDING * 2,
+        height: height / 2 - PADDING * 2,
+        numberOfWaves: 5,
+        pointsPerWave,
+        direction: 'bottom-to-top',
+      }),
+      snake: chunk(generateSnakePath({ width: width / 4, height: height / 4, step: 20 }), 8),
+    }),
+    [width, height, numberOfWaves, pointsPerWave],
+  );
 
   return width < 10 ? null : (
     <div>
       <svg width={width} height={height}>
+        {/* Background */}
         <LinearGradient
           id="visx-shape-splitlinepath-gradient"
           from={background}
@@ -72,11 +119,13 @@ export default function SplitPath({
           rx={14}
         />
 
-        <g transform={`rotate(${0})translate(${-0}, ${-height * 0.5})`}>
+        {/* left to right */}
+        <g transform={`translate(${PADDING}, ${height / 4})`}>
+          {/* Render all segments as a single line for comparison */}
           <LinePath
-            data={data}
-            x={getScaledX}
-            y={getScaledY}
+            data={data.leftToRight.flat()}
+            x={getX}
+            y={getY}
             strokeWidth={8}
             stroke="#fff"
             strokeOpacity={0.15}
@@ -84,9 +133,11 @@ export default function SplitPath({
           />
 
           <SplitLinePath
-            segments={dividedData}
-            x={getScaledX}
-            y={getScaledY}
+            sampleRate={2}
+            segments={data.leftToRight}
+            segmentation="x"
+            x={getX}
+            y={getY}
             curve={curveCardinal}
             styles={[
               { stroke: foreground, strokeWidth: 3 },
@@ -95,31 +146,168 @@ export default function SplitPath({
             ]}
           >
             {({ segment, styles, index }) =>
-              /** overlay circles to a couple of the segments */
-              index === numberOfSegments - 1 || index === 2 ? (
-                segment.map(({ x, y }, i) =>
-                  i % 8 === 0 ? (
-                    <circle
-                      key={i}
-                      cx={x}
-                      cy={y}
-                      r={10 * (i / segment.length)}
-                      stroke={styles?.stroke}
-                      fill="transparent"
-                      strokeWidth={1}
-                    />
-                  ) : null,
-                )
+              index === numberOfWaves - 1 || index === 2 ? (
+                renderCircleSegment({ segment, styles, index })
               ) : (
-                <LinePath
-                  data={segment}
-                  x={(d: Point) => d.x || 0}
-                  y={(d: Point) => d.y || 0}
-                  {...styles}
-                />
+                <LinePath data={segment} x={getX} y={getY} {...styles} />
               )
             }
           </SplitLinePath>
+          <text dy="0.3em" fontSize={10} fontWeight="bold" textAnchor="middle">
+            Start
+          </text>
+        </g>
+
+        {/* right to left */}
+        <g transform={`translate(${width / 2 - PADDING}, ${(height * 3) / 4})`}>
+          {/* Render all segments as a single line for comparison */}
+          <LinePath
+            data={data.rightToLeft.flat()}
+            x={getX}
+            y={getY}
+            strokeWidth={8}
+            stroke="#fff"
+            strokeOpacity={0.15}
+            curve={curveCardinal}
+          />
+
+          <SplitLinePath
+            sampleRate={1}
+            segments={data.rightToLeft}
+            segmentation="x"
+            x={getX}
+            y={getY}
+            curve={curveCardinal}
+            styles={[
+              { stroke: foreground, strokeWidth: 3 },
+              { stroke: '#fff', strokeWidth: 2, strokeDasharray: '9,5' },
+              { stroke: background, strokeWidth: 2 },
+            ]}
+          >
+            {({ segment, styles, index }) =>
+              index === numberOfWaves - 1 || index === 2 ? (
+                renderNumberSegment({ segment, styles, index })
+              ) : (
+                <LinePath data={segment} x={getX} y={getY} {...styles} />
+              )
+            }
+          </SplitLinePath>
+          <text dy="0.3em" fontSize={10} fontWeight="bold" textAnchor="middle">
+            Start
+          </text>
+        </g>
+
+        {/* top to bottom */}
+        <g transform={`translate(${(width * 3) / 4}, ${PADDING})`}>
+          {/* Render all segments as a single line for comparison */}
+          <LinePath
+            data={data.topToBottom.flat()}
+            x={getX}
+            y={getY}
+            strokeWidth={8}
+            stroke="#fff"
+            strokeOpacity={0.15}
+            curve={curveCardinal}
+          />
+
+          <SplitLinePath
+            sampleRate={1}
+            segments={data.topToBottom}
+            segmentation="y"
+            x={getX}
+            y={getY}
+            curve={curveCardinal}
+            styles={[
+              { stroke: foreground, strokeWidth: 3 },
+              { stroke: '#fff', strokeWidth: 2, strokeDasharray: '9,5' },
+              { stroke: background, strokeWidth: 2 },
+            ]}
+          >
+            {({ segment, styles, index }) =>
+              index === numberOfWaves - 1 || index === 2 ? (
+                renderNumberSegment({ segment, styles, index })
+              ) : (
+                <LinePath data={segment} x={getX} y={getY} {...styles} />
+              )
+            }
+          </SplitLinePath>
+          <text dy="0.3em" fontSize={10} fontWeight="bold" textAnchor="middle">
+            Start
+          </text>
+        </g>
+
+        {/* bottom to top */}
+        <g transform={`translate(${(width * 3) / 4}, ${height - PADDING})`}>
+          {/* Render all segments as a single line for comparison */}
+          <LinePath
+            data={data.bottomToTop.flat()}
+            x={getX}
+            y={getY}
+            strokeWidth={8}
+            stroke="#fff"
+            strokeOpacity={0.15}
+            curve={curveCardinal}
+          />
+
+          <SplitLinePath
+            sampleRate={1}
+            segments={data.bottomToTop}
+            segmentation="y"
+            x={getX}
+            y={getY}
+            curve={curveCardinal}
+            styles={[
+              { stroke: foreground, strokeWidth: 3 },
+              { stroke: '#fff', strokeWidth: 2, strokeDasharray: '9,5' },
+              { stroke: background, strokeWidth: 2 },
+            ]}
+          >
+            {({ segment, styles, index }) =>
+              index === numberOfWaves - 1 || index === 2 ? (
+                renderCircleSegment({ segment, styles, index })
+              ) : (
+                <LinePath data={segment} x={getX} y={getY} {...styles} />
+              )
+            }
+          </SplitLinePath>
+          <text dy="0.3em" fontSize={10} fontWeight="bold" textAnchor="middle">
+            Start
+          </text>
+        </g>
+        {/* snake */}
+        <g transform={`translate(${width / 2 - width / 8}, ${height / 2 - height / 8})`}>
+          {/* Render all segments as a single line for comparison */}
+          <LinePath
+            data={data.snake.flat()}
+            x={getX}
+            y={getY}
+            strokeWidth={8}
+            stroke="#fff"
+            strokeOpacity={0.15}
+          />
+
+          <SplitLinePath
+            sampleRate={1}
+            segments={data.snake}
+            segmentation="length"
+            x={getX}
+            y={getY}
+            styles={[
+              { stroke: foreground, strokeWidth: 3 },
+              { stroke: '#fff', strokeWidth: 2, strokeDasharray: '9,5' },
+              { stroke: background, strokeWidth: 2 },
+            ]}
+          />
+          <text
+            x={width / 8}
+            y={height / 8}
+            dy="0.3em"
+            fontSize={10}
+            fontWeight="bold"
+            textAnchor="middle"
+          >
+            Start
+          </text>
         </g>
       </svg>
     </div>
--- packages/visx-shape/src/shapes/SplitLinePath.tsx
+++ packages/visx-shape/src/shapes/SplitLinePath.tsx
@@ -1,73 +1,86 @@
 import React, { useMemo } from 'react';
-import LinePath from './LinePath';
-import getSplitLineSegments from '../util/getSplitLineSegments';
+import getSplitLineSegments, { GetLineSegmentsConfig } from '../util/getSplitLineSegments';
+import { line } from '../util/D3ShapeFactories';
 import { LinePathConfig } from '../types';
+import LinePath from './LinePath';
 
 interface Point {
   x: number;
   y: number;
 }
 
-type SplitLinePathProps<Datum> = {
+const getX = (d: Point) => d.x || 0;
+const getY = (d: Point) => d.y || 0;
+
+export type SplitLinePathRenderer = (renderProps: {
+  index: number;
+  segment: { x: number; y: number }[];
+  styles?: Omit<React.SVGProps<SVGPathElement>, 'x' | 'y' | 'children'>;
+}) => React.ReactNode;
+
+export type SplitLinePathProps<Datum> = {
   /** Array of data segments, where each segment will be a separate path in the rendered line. */
   segments: Datum[][];
   /** Styles to apply to each segment. If fewer styles are specified than the number of segments, they will be re-used. */
   styles: Omit<React.SVGProps<SVGPathElement>, 'x' | 'y' | 'children'>[];
   /** Override render function which is passed the configured path generator as input. */
-  children?: (renderProps: {
-    index: number;
-    segment: { x: number; y: number }[];
-    styles?: Omit<React.SVGProps<SVGPathElement>, 'x' | 'y' | 'children'>;
-  }) => React.ReactNode;
+  children?: SplitLinePathRenderer;
   /** className applied to path element. */
   className?: string;
-  /** Optionally specify the sample rate for interpolating line segments. */
-  sampleRate?: number;
-} & LinePathConfig<Datum>;
+} & LinePathConfig<Datum> &
+  Pick<GetLineSegmentsConfig, 'segmentation' | 'sampleRate'>;
 
 export default function SplitLinePath<Datum>({
   children,
   className,
   curve,
   defined,
+  segmentation,
   sampleRate,
   segments,
   x,
   y,
   styles,
 }: SplitLinePathProps<Datum>) {
-  // combine data to first draw entire path
-  const combinedSegments: Datum[] = useMemo(
-    () => segments.reduce((flat, segmentData) => flat.concat([...segmentData]), []),
-    [segments],
+  // Convert data in all segments to points.
+  const pointsInSegments = useMemo(() => {
+    const xFn = typeof x === 'number' || typeof x === 'undefined' ? () => x : x;
+    const yFn = typeof y === 'number' || typeof y === 'undefined' ? () => y : y;
+    return segments.map(s => s.map((value, i) => ({ x: xFn(value, i, s), y: yFn(value, i, s) })));
+  }, [x, y, segments]);
+
+  const pathString = useMemo(() => {
+    const path = line<Datum>({ x, y, defined, curve });
+    return path(segments.flat()) || '';
+  }, [x, y, defined, curve, segments]);
+
+  const splitLineSegments = useMemo(
+    () =>
+      getSplitLineSegments({
+        path: pathString,
+        segmentation,
+        pointsInSegments,
+        sampleRate,
+      }),
+    [pathString, segmentation, pointsInSegments, sampleRate],
   );
 
   return (
-    <LinePath data={combinedSegments} defined={defined} curve={curve} x={x} y={y}>
-      {({ path }) => {
-        // use entire path to interpolate individual segments
-        const entirePath = path(combinedSegments);
-        const computedLineSegments = getSplitLineSegments({
-          path: entirePath || '',
-          segments,
-          sampleRate,
-        });
-
-        return computedLineSegments.map((segment, index) =>
-          children ? (
-            children({ index, segment, styles: styles[index] || styles[index % styles.length] })
-          ) : (
-            <LinePath
-              key={index}
-              className={className}
-              data={segment}
-              x={(d: Point) => d.x || 0}
-              y={(d: Point) => d.y || 0}
-              {...(styles[index] || styles[index % styles.length])}
-            />
-          ),
-        );
-      }}
-    </LinePath>
+    <g>
+      {splitLineSegments.map((segment, index) =>
+        children ? (
+          children({ index, segment, styles: styles[index] || styles[index % styles.length] })
+        ) : (
+          <LinePath
+            key={index}
+            className={className}
+            data={segment}
+            x={getX}
+            y={getY}
+            {...(styles[index] || styles[index % styles.length])}
+          />
+        ),
+      )}
+    </g>
   );
 }
