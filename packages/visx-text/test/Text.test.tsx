59ee866d5324e17af298221a333bc682dfce382f
gazcn007 <shikang.liu@mail.mcgill.ca>
2021-07-19 15:22:45 +0800

    update visx-text

--- packages/visx-text/test/Text.test.tsx
+++ packages/visx-text/test/Text.test.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
+import { render } from '@testing-library/react';
 import { renderHook } from '@testing-library/react-hooks';
+import '@testing-library/jest-dom';
 import { Text, getStringWidth, useText } from '../src';
 import { addMock, removeMock } from './svgMock';
 
@@ -87,54 +89,56 @@ describe('<Text />', () => {
   });
 
   it('Render 0 success when specify the width', () => {
-    const wrapper = mount(
+    const { container } = render(
       <Text x={0} y={0} width={30}>
         0
       </Text>,
     );
 
-    console.log('wrapper', wrapper.text());
-    expect(wrapper.text()).toContain('0');
+    const text = container.querySelector('tspan');
+    expect(text?.textContent).toEqual('0');
   });
 
   it('Render 0 success when not specify the width', () => {
-    const wrapper = mount(
+    const { container } = render(
       <Text x={0} y={0}>
         0
       </Text>,
     );
-
-    expect(wrapper.text()).toContain('0');
+    const text = container.querySelector('tspan');
+    expect(text?.textContent).toEqual('0');
   });
 
   it('Render text when x or y is a percentage', () => {
-    const wrapper = mount(
+    const { container } = render(
       <Text x="50%" y="50%">
         anything
       </Text>,
     );
-
-    expect(wrapper.text()).toContain('anything');
+    const text = container.querySelector('tspan');
+    expect(text?.textContent).toEqual('anything');
   });
 
   it("Don't Render text when x or y is NaN", () => {
-    const wrapperNan = mount(
+    const { container } = render(
       <Text x={NaN} y={10}>
         anything
       </Text>,
     );
-
-    expect(wrapperNan.text()).not.toContain('anything');
+    const text = container.querySelector('tspan');
+    expect(text).toBeNull();
   });
 
   it('Render text when children 0 is a number', () => {
-    const wrapper = mount(
+    const num = 0;
+    const { container } = render(
       <Text x={0} y={0}>
-        {0}
+        {num}
       </Text>,
     );
 
-    expect(wrapper.text()).toContain('0');
+    const text = container.querySelector('tspan');
+    expect(text?.textContent).toEqual('0');
   });
 
   it('Applies transform if scaleToFit is set', () => {
@@ -165,31 +169,27 @@ describe('<Text />', () => {
   });
 
   it('Offsets vertically if verticalAnchor is given', () => {
-    let wrapper = mount<Text>(
+    let { container } = render(
       <Text width={200} style={{ fontFamily: 'Courier' }}>
         This is really long text
       </Text>,
     );
-    const getVerticalOffset = (w: typeof wrapper) =>
-      w
-        .find('tspan')
-        .first()
-        .prop('dy');
+    const getVerticalOffset = (c: HTMLElement) => c?.querySelector('tspan')?.getAttribute('dy');
 
-    expect(getVerticalOffset(wrapper)).toBe('-1em');
+    expect(getVerticalOffset(container)).toBe('-1em');
 
-    wrapper = mount<Text>(
+    ({ container } = render(
       <Text width={200} verticalAnchor="middle" style={{ fontFamily: 'Courier' }}>
         This is really long text
       </Text>,
-    );
-    expect(getVerticalOffset(wrapper)).toBe('-0.145em');
+    ));
+    expect(getVerticalOffset(container)).toBe('-0.145em');
 
-    wrapper = mount<Text>(
+    ({ container } = render(
       <Text width={200} verticalAnchor="start" style={{ fontFamily: 'Courier' }}>
         This is really long text
       </Text>,
-    );
-    expect(getVerticalOffset(wrapper)).toBe('0.71em');
+    ));
+    expect(getVerticalOffset(container)).toBe('0.71em');
   });
 });
