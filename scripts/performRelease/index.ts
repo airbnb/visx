950fed25cb967ba52e8f02a2d009368cd3938bc0
Chris Williams <williaster@users.noreply.github.com>
2021-08-11 16:09:29 -0700

    fix(scripts/performRelease): fix alpha release logic (#1310)
    * 3.0.0-alpha.0 => 2.0.0-alpha.0
    
    * lerna version 2.0.0-alpha.1
    
    * fix(scripts/performRelease): fix alpha release logic
    
    * fix(deps): remove ^ in @visx inter-dependencies

--- scripts/performRelease/index.ts
+++ scripts/performRelease/index.ts
@@ -19,50 +19,74 @@ async function performRelease() {
 
   console.log('Running release script');
 
-  // fetch most recent non-alpha tag
-  // this allows posting on PRs for the alpha + final release
+  // fetch most recent alpha + non-alpha tags
+  // this allows posting on PRs for the alpha / non-alpha release
+  // while only using PRs since the last (alpha or non-alpha) tag to dictate the next release version
   const tagsRequest = await fetchTags(client); // sorted new => old
-  const mostRecentTag = tagsRequest.data.find(tag => !tag.name.includes('alpha'));
+  const mostRecentTag = tagsRequest.data[0];
+  const mostRecentNonAlphaTag = tagsRequest.data.find(tag => !tag.name.includes('alpha'));
 
-  if (!mostRecentTag) {
-    console.log('Could not find recent non-alpha tag. Exiting.');
+  if (!mostRecentTag || !mostRecentNonAlphaTag) {
+    console.log('Could not find recent tag. Exiting.');
     process.exit(1);
   }
 
-  console.log('Most recent non-alpha tag found is', mostRecentTag.name);
+  console.log('Most recent tag found is', mostRecentTag.name);
+  console.log('Most recent non-alpha tag found is', mostRecentNonAlphaTag.name);
 
-  // get commits since most recent tag
-  const commitsSinceTagRequest = await fetchCommitsSinceTag(client, mostRecentTag.commit.sha);
-  const commitsSinceTag = commitsSinceTagRequest.data.commits;
+  // get commits since most recent alpha / non-alpha tag
+  const commitsSinceLastTagRequest = await fetchCommitsSinceTag(client, mostRecentTag.commit.sha);
+  const commitsSinceNonAlphaTagRequest = await fetchCommitsSinceTag(
+    client,
+    mostRecentNonAlphaTag.commit.sha,
+  );
 
-  if (commitsSinceTag.length === 0) {
-    console.log(`No commits found since last release. Exiting.`);
+  const commitsSinceNonAlphaTag = commitsSinceNonAlphaTagRequest.data.commits;
+  const commitsSinceLastTag = commitsSinceLastTagRequest.data.commits;
+
+  if (commitsSinceLastTag.length === 0) {
+    console.log(`No commits found since last release tag. Exiting.`);
     process.exit(0);
   }
 
-  // find PRs since last tag, to determine if we need to release and for changelog generation
+  // find PRs since last alpha / non-alpha tags, to determine if we need to release
+  // and for changelog generation
   const prsSinceLastTag = await fetchPRsForCommits(
     client,
-    commitsSinceTag.map(commit => commit.sha),
+    commitsSinceLastTag.map(commit => commit.sha),
+  );
+  const prsSinceLastNonAlphaTag = await fetchPRsForCommits(
+    client,
+    commitsSinceNonAlphaTag.map(commit => commit.sha),
   );
 
+  // release is based on tags since last tag regardless of alpha / non-alpha
+  // this better matches the workflow where you
+  //   1) publish alpha major release (e.g., 2.0.0-alpha.0)
+  //   2) publish alpha patch changes (e.g., 2.0.1-alpha.0)
+  //   3) publish non-alpha patch     (e.g., 2.0.2)
+  // if we base the release on PRs since the last non-alpha tag, we would get
+  // get multiple major version bumps
   const isPreRelease = await performLernaRelease(prsSinceLastTag);
 
   const newTagsRequest = await fetchTags(client);
   const newTag = newTagsRequest.data[0];
 
+  // no new tag was created, bail
   if (newTag.name === mostRecentTag.name) {
     console.log('Newest tag equals previous tag, will not update changelog or post on PRs.');
     process.exit(0);
   }
 
-  // update changelog only for non-pre-releases
+  // update changelog only for non-alpha releases
+  // include all PRs since last non-alpha release
   if (!isPreRelease) {
-    await updateChangelog(prsSinceLastTag, newTag.name);
+    await updateChangelog(prsSinceLastNonAlphaTag, newTag.name);
   }
 
-  // post release version on all PRs to inform authors
-  await postReleaseOnPrs(client, prsSinceLastTag, newTag.name);
+  // post release version on all PRs since last non-alpha release to inform authors
+  // this will double post on alpha PRs (which is desired)
+  await postReleaseOnPrs(client, prsSinceLastNonAlphaTag, newTag.name);
 }
 
 performRelease().catch(error => {
