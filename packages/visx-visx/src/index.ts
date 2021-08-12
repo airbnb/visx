c5b1cb360400ec6518f1a6ba880afab7f749d4ba
robsutcliffe <rob@firefields.com>
2021-08-12 01:03:23 +0100

    breaking(visx/visx): export by package namespace (#1121)
    * add namespaces for each package.
    While still leaving the existing export statements to not break legacy code
    
    * add namespaces for each package.
    While still leaving the existing export statements to not break legacy code
    
    * updated unit test variables
    
    * avoiding typescript error by importing and exporting separately
    
    * ran prettier on edited files
    
    * added trailing comma
    
    * added semi colon
    
    * agggh. sorry. added a line break at the end. didn't have prettier installed properly. obvs
    
    * Update packages/visx-visx/src/index.ts
    
    Co-authored-by: Chris Williams <williaster@users.noreply.github.com>
    
    * missed this one from the original
    
    * fix(visx-visx): fix test import syntax
    
    Co-authored-by: Chris Williams <williaster@users.noreply.github.com>

--- packages/visx-visx/src/index.ts
+++ packages/visx-visx/src/index.ts
@@ -1,33 +1,61 @@
-// has name conflicts with other packages
-import * as XYChartPackage from '@visx/xychart';
+import * as XYChart from '@visx/xychart';
+import * as Annotation from '@visx/annotation';
+import * as Axis from '@visx/axis';
+import * as Bounds from '@visx/bounds';
+import * as Brush from '@visx/brush';
+import * as ClipPath from '@visx/clip-path';
+import * as Curve from '@visx/curve';
+import * as Drag from '@visx/drag';
+import * as Event from '@visx/event';
+import * as Geo from '@visx/geo';
+import * as Glyph from '@visx/glyph';
+import * as Gradient from '@visx/gradient';
+import * as Grid from '@visx/grid';
+import * as Group from '@visx/group';
+import * as Heatmap from '@visx/heatmap';
+import * as Hierarchy from '@visx/hierarchy';
+import * as Legend from '@visx/legend';
+import * as Marker from '@visx/marker';
+import * as MockData from '@visx/mock-data';
+import * as Network from '@visx/network';
+import * as Pattern from '@visx/pattern';
+import * as Point from '@visx/point';
+import * as Responsive from '@visx/responsive';
+import * as Scale from '@visx/scale';
+import * as Shape from '@visx/shape';
+import * as Text from '@visx/text';
+import * as Tooltip from '@visx/tooltip';
+import * as Voronoi from '@visx/voronoi';
+import * as Zoom from '@visx/zoom';
 
-export * from '@visx/annotation';
-export * from '@visx/axis';
-export * from '@visx/bounds';
-export * from '@visx/brush';
-export * from '@visx/clip-path';
-export * from '@visx/curve';
-export * from '@visx/drag';
-export * from '@visx/event';
-export * from '@visx/geo';
-export * from '@visx/glyph';
-export * from '@visx/gradient';
-export * from '@visx/grid';
-export * from '@visx/group';
-export * from '@visx/heatmap';
-export * from '@visx/hierarchy';
-export * from '@visx/legend';
-export * from '@visx/marker';
-export * from '@visx/mock-data';
-export * from '@visx/network';
-export * from '@visx/pattern';
-export * from '@visx/point';
-export * from '@visx/responsive';
-export * from '@visx/scale';
-export * from '@visx/shape';
-export * from '@visx/text';
-export * from '@visx/tooltip';
-export * from '@visx/voronoi';
-export * from '@visx/zoom';
-
-export const XYChart = XYChartPackage;
+export {
+  XYChart,
+  Annotation,
+  Axis,
+  Bounds,
+  Brush,
+  ClipPath,
+  Curve,
+  Drag,
+  Event,
+  Geo,
+  Glyph,
+  Gradient,
+  Grid,
+  Group,
+  Heatmap,
+  Hierarchy,
+  Legend,
+  Marker,
+  MockData,
+  Network,
+  Pattern,
+  Point,
+  Responsive,
+  Scale,
+  Shape,
+  Text,
+  Tooltip,
+  Voronoi,
+  Zoom,
+};
