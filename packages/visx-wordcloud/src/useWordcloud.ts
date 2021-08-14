7866eb179234c9a9ac0a13f6ae636f592b32edcb
Cezar <craciuncezar1996@gmail.com>
2021-08-14 22:02:13 +0300

    Use safer check for undefined

--- packages/visx-wordcloud/src/useWordcloud.ts
+++ packages/visx-wordcloud/src/useWordcloud.ts
@@ -24,16 +24,16 @@ export default function useWordcloud<Datum extends BaseDatum>({
 
     const layout = d3Cloud<Datum>();
 
-    if (width !== undefined && height !== undefined) layout.size([width, height]);
-    if (words !== undefined) layout.words(words);
-    if (random !== undefined) layout.random(random);
-    if (font !== undefined) layout.font(font);
-    if (padding !== undefined) layout.padding(padding);
-    if (fontSize !== undefined) layout.fontSize(fontSize);
-    if (fontStyle !== undefined) layout.fontStyle(fontStyle);
-    if (fontWeight !== undefined) layout.fontWeight(fontWeight);
-    if (rotate !== undefined) layout.rotate(rotate);
-    if (spiral !== undefined) layout.spiral(spiral);
+    layout.size([width, height]);
+    layout.words(words);
+    if (typeof random !== 'undefined') layout.random(random);
+    if (typeof font !== 'undefined') layout.font(font);
+    if (typeof padding !== 'undefined') layout.padding(padding);
+    if (typeof fontSize !== 'undefined') layout.fontSize(fontSize);
+    if (typeof fontStyle !== 'undefined') layout.fontStyle(fontStyle);
+    if (typeof fontWeight !== 'undefined') layout.fontWeight(fontWeight);
+    if (typeof rotate !== 'undefined') layout.rotate(rotate);
+    if (typeof spiral !== 'undefined') layout.spiral(spiral);
 
     layout.on('end', setCloudWords);
     layout.start();
