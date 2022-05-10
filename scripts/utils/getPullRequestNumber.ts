ac336cc3169818329e463420f680e1b74d3dc940
Harphuna <86975697+Hakunasc11@users.noreply.github.com>
2022-05-10 18:17:55 +0200

    Delete getPullRequestNumber.ts

--- scripts/utils/getPullRequestNumber.ts
+++
@@ -1,6 +0,0 @@
-export default function getPullRequestNumber() {
-  const prNumberString = process.env.PR_NUMBER;
-  if (!prNumberString) throw new Error('No PR number available. Aborting.');
-
-  return Number(prNumberString);
-}
