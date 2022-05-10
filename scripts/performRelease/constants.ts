254129f62953968ac6995ac0f072c5a44b6186de
Harphuna <86975697+Hakunasc11@users.noreply.github.com>
2022-05-10 18:06:31 +0200

    Delete constants.ts

--- scripts/performRelease/constants.ts
+++
@@ -1,5 +0,0 @@
-export const ALPHA_RELEASE = 'alpha-release';
-export const PATCH_RELEASE = 'patch-release';
-export const MINOR_RELEASE = 'minor-release';
-export const MAJOR_RELEASE = 'major-release';
-export const RELEASE_LABELS = [PATCH_RELEASE, MINOR_RELEASE, MAJOR_RELEASE, ALPHA_RELEASE];
