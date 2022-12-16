8eab0324117c346a8ddba3b3dba631a228dde8c1
Chris Williams <chris.williams@airbnb.com>
2022-12-16 15:12:11 -0800

    internal(dev/prettier): migrate off nimbus

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  printWidth: 100,
+  proseWrap: 'always',
+  requirePragma: false,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+};
