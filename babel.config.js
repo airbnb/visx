bcb25625dbcb2cd191d3e37d3752f4ae92075c13
Chris Williams <chris.williams@airbnb.com>
2022-12-16 14:19:09 -0800

    internal(dev/babel): migrate off nimbus

---
+++ babel.config.js
@@ -0,0 +1,53 @@
+const esm = process.env.ESM;
+
+const envOptions = {
+  loose: true,
+  modules: esm ? false : 'commonjs',
+  shippedProposals: true,
+  targets: {
+    ie: 11,
+  },
+  bugfixes: false,
+};
+
+const presets = [
+  ['@babel/preset-env', envOptions],
+  '@babel/preset-react',
+  '@babel/preset-typescript',
+];
+
+const plugins = ['babel-plugin-typescript-to-proptypes'];
+
+const ignore = [
+  'coverage/',
+  'node_modules/',
+  'public/',
+  'esm/',
+  'lib/',
+  'tmp/',
+  'dist/',
+  '*.d.ts',
+  '__tests__',
+  '__mocks__',
+];
+
+switch (process.env.NODE_ENV) {
+  case 'test': {
+    envOptions.modules = 'commonjs';
+    envOptions.targets = { node: 'current' };
+    plugins.push('babel-plugin-dynamic-import-node');
+    break;
+  }
+
+  case 'development':
+  case 'production':
+  default: {
+    break;
+  }
+}
+
+module.exports = {
+  ignore,
+  plugins,
+  presets,
+};
