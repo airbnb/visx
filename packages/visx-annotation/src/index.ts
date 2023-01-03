9f8f46005e50a92134dbe37bd29ec15c75448439
Chris Williams <chris.williams@airbnb.com>
2023-01-03 13:20:05 -0800

    breaking(annotation): remove deprecated/LinePathAnnotation

--- packages/visx-annotation/src/index.ts
+++ packages/visx-annotation/src/index.ts
@@ -6,6 +6,3 @@ export { default as LineSubject } from './components/LineSubject';
 export { default as Annotation } from './components/Annotation';
 export { default as EditableAnnotation } from './components/EditableAnnotation';
 export { default as AnnotationContext } from './context/AnnotationContext';
-
-// @TODO deprecated, remove in 2.0
-export { default as LinePathAnnotation } from './deprecated/LinePathAnnotation';
