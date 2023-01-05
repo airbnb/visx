ce33b87c20b111c3f0f43ebcd5a7e2ad9c7c1e1a
Chris Williams <chris.williams@airbnb.com>
2023-01-04 16:05:39 -0800

    fix(demo/XYChartTile): disable SSR

--- packages/visx-demo/src/components/Gallery/XYChartTile.tsx
+++ packages/visx-demo/src/components/Gallery/XYChartTile.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import XYChart, { XYChartProps } from '../../sandboxes/visx-xychart/Example';
+import dynamic from 'next/dynamic';
+import { XYChartProps } from '../../sandboxes/visx-xychart/Example';
 import GalleryTile from '../GalleryTile';
+
+const XYChart = dynamic(() => import('../../sandboxes/visx-xychart/Example'), { ssr: false });
 
 export { default as packageJson } from '../../sandboxes/visx-xychart/package.json';
 
