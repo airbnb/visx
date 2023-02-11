ef94c10e3baac2463ac1efff17f15fab96a39407
jonambas <jon@jonambas.com>
2023-02-11 09:03:01 -0500

    fix(demo): fix missing additional missing docs

--- packages/visx-demo/src/pages/docs/xychart.tsx
+++ packages/visx-demo/src/pages/docs/xychart.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import XYChartReadme from '!!raw-loader!../../../../visx-xychart/README.md';
-import * as XYChartPackage from '../../../../visx-xychart';
+import * as XYChartPackage from '../../../../visx-xychart/src';
 import DocPage from '../../components/DocPage';
 import XYChartTile from '../../components/Gallery/XYChartTile';
 
