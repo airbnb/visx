af536db7b0f28d47a354d3f4f6951f5c97e0687e
Jon Ambas <jon@jonambas.com>
2023-02-13 15:02:58 -0500

    fix(demo): fix missing API documentation (#1648)
    * fix(demo): fix missing visx/shape documentation
    
    * fix(demo): fix missing additional missing docs
    
    * fix(demo): fix inlined code block

--- packages/visx-demo/src/pages/docs/gradient.tsx
+++ packages/visx-demo/src/pages/docs/gradient.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import GradientReadme from '!!raw-loader!../../../../visx-gradient/Readme.md';
-import * as Gradients from '../../../../visx-gradient';
+import * as Gradients from '../../../../visx-gradient/src';
 import DocPage from '../../components/DocPage';
 import GradientsTile from '../../components/Gallery/GradientsTile';
 import AreaTile from '../../components/Gallery/AreaTile';
