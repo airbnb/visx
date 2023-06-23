8b3a82dd2451ec5a2fd72e0e69d0b226c6ceb5cf
Chris Williams <chris.williams@airbnb.com>
2023-06-23 15:49:42 -0700

    fix things to fully build

--- packages/visx-vendor/test/internmap.test.ts
+++ packages/visx-vendor/test/internmap.test.ts
@@ -1,9 +1,8 @@
 /* This test verifies that these modules and types are exported correctly */
 import {
-  // @ts-expect-error Make sure invalid imports fail:
-  INVALID_TYPE,
   InternSet,
   InternMap,
+  // @ts-expect-error no types
 } from '@visx/vendor/internmap';
 
 describe('internmap', () => {
