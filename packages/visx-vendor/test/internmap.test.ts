df32bb6ab6a5749bb9a517efba41b164c6171881
Chris Williams <chris.williams@airbnb.com>
2023-06-27 10:15:43 -0700

    vendor: internmap fix again

--- packages/visx-vendor/test/internmap.test.ts
+++ packages/visx-vendor/test/internmap.test.ts
@@ -1,9 +1,5 @@
 /* This test verifies that these modules and types are exported correctly */
-import {
-  InternSet,
-  InternMap,
-  // @ts-expect-error no types
-} from '@visx/vendor/internmap';
+import { InternSet, InternMap } from '@visx/vendor/internmap';
 
 describe('internmap', () => {
   it('exports valid classes', () => {
