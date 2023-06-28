e71d2819beb9680a7bc94ceaf3006f7c0f6847b5
Chris Williams <williaster@users.noreply.github.com>
2023-06-28 16:10:30 -0700

    github: fix release types and logic (#1723)

--- scripts/performRelease/fetchPRsForCommits.ts
+++ scripts/performRelease/fetchPRsForCommits.ts
@@ -7,36 +7,36 @@ export default async function fetchPRsForCommits(
   shas: string[],
 ): Promise<PR[]> {
   const { owner, repo } = getRepoContext();
-  const prs = [];
+  const prs: PR[] = [];
 
-  // for-loop because of async calls
-  for (let index = 0; index < shas.length; index += 1) {
-    const sha = shas[index];
-    console.log('Fetching PRs associated with commit', sha);
+  await Promise.all(
+    shas.map(async (sha) => {
+      console.log('Fetching PRs associated with commit', sha);
 
-    const prsForCommit = await client.request(
-      'GET /repos/{owner}/{repo}/commits/{commit_sha}/pulls',
-      {
-        owner,
-        repo,
-        commit_sha: sha,
-        // special header needed https://docs.github.com/en/rest/reference/repos#list-pull-requests-associated-with-a-commit
-        mediaType: {
-          previews: ['groot'],
+      const prsForCommit = await client.request(
+        'GET /repos/{owner}/{repo}/commits/{commit_sha}/pulls',
+        {
+          owner,
+          repo,
+          commit_sha: sha,
+          // special header needed https://docs.github.com/en/rest/reference/repos#list-pull-requests-associated-with-a-commit
+          mediaType: {
+            previews: ['groot'],
+          },
         },
-      },
-    );
-    if (prsForCommit.data.length === 0) {
-      console.log('Ignoring commit with no PRs', sha);
-      continue;
-    }
-    if (prsForCommit.data.length > 1) {
-      console.warn('Multiple PRs associated with commit, only considering the first', sha);
-    }
+      );
+      if (prsForCommit.data.length === 0) {
+        console.log('Ignoring commit with no PRs', sha);
+        return;
+      }
+      if (prsForCommit.data.length > 1) {
+        console.warn('Multiple PRs associated with commit, only considering the first', sha);
+      }
 
-    const prForCommit = prsForCommit.data[0];
-    prs.push(prForCommit);
-  }
+      const prForCommit = prsForCommit.data[0] as PR;
+      prs.push(prForCommit);
+    }),
+  );
 
   return prs;
 }
