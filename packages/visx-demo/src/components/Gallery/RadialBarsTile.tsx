7b3f28aca3ec9a096020352b8ae8b028422560a2
Adam Zuchowicz <67815421+adamzuch@users.noreply.github.com>
2024-01-11 14:58:29 +1100

    feat(demo): add radial bars demo using Arc (#1785)

---
+++ packages/visx-demo/src/components/Gallery/RadialBarsTile.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import RadialBars, { RadialBarsProps } from '../../sandboxes/visx-radial-bars/Example';
+import GalleryTile from '../GalleryTile';
+
+export { default as packageJson } from '../../sandboxes/visx-radial-bars/package.json';
+
+const tileStyles = { background: '#3dbdb1' };
+const detailsStyles = { color: '#93F9B9' };
+const exampleProps = { showControls: false };
+
+export default function BarsTile() {
+  return (
+    <GalleryTile<RadialBarsProps>
+      title="Radial Bars"
+      description="<Shape.Arc />"
+      exampleProps={exampleProps}
+      exampleRenderer={RadialBars}
+      exampleUrl="/radial-bars"
+      tileStyles={tileStyles}
+      detailsStyles={detailsStyles}
+    />
+  );
+}
