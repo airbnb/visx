73d031e75a475b094dd2d87b3115a511a6d49d69
Zach Barbre <zach.barbre@gmail.com>
2024-05-13 23:05:37 -0400

    Add tests, fix LegendThreshold default scale for negitive domain

--- packages/visx-legend/src/legends/Threshold.tsx
+++ packages/visx-legend/src/legends/Threshold.tsx
@@ -42,7 +42,7 @@ function defaultTransform<Scale extends AnyThresholdScale>({
       if (d0 == null && typeof d1 === 'number') {
         // lower threshold e.g., [undefined, number]
         delimiter = labelLower || delimiter;
-        value = d1 - (2 * d1 - 1); // guarantees a value smaller than the lower threshold
+        value = d1 - 2 * Math.abs(d1); // guarantees a value smaller than the lower threshold
         text = `${delimiter}${formatZero(labelFormat(d1, i))}`;
       } else if (d0 != null && d1 != null) {
         // threshold step
@@ -51,7 +51,7 @@ function defaultTransform<Scale extends AnyThresholdScale>({
       } else if (typeof d0 === 'number' && d1 == null) {
         // upper threshold e.g., [number, undefined]
         delimiter = labelUpper || delimiter;
-        value = d0 + (2 * d0 + 1); // // guarantees a value larger than the upper threshold
+        value = d0 + 2 * Math.abs(d0); // // guarantees a value larger than the upper threshold
         text = `${delimiter}${formatZero(labelFormat(d0, i))}`;
       }
 
@@ -80,7 +80,9 @@ export default function Threshold<Scale extends AnyThresholdScale>({
   // https://github.com/d3/d3-scale#threshold_domain
   // therefore if a domain is not specified we transform the range into input values
   // because it should contain more elements
-  const domain = inputDomain || scale.range().map((output) => scale.invertExtent(output)[0]);
+
+  const domain =
+    inputDomain || scale.range().map((output) => scale.invertExtent(output)[0] as Range);
 
   const labelTransform =
     inputLabelTransform ||
