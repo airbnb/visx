f6b1495a3f117d09bc985ee5446d4b5f37f62ddb
Jackson Hardaker <jackson@bitdrift.io>
2024-09-25 10:30:58 -0400

    add exports

---
+++ packages/visx-sankey/src/index.ts
@@ -0,0 +1,9 @@
+export { default as Sankey } from './Sankey';
+export {
+  sankey,
+  sankeyLinkHorizontal,
+  sankeyLeft,
+  sankeyRight,
+  sankeyCenter,
+  sankeyJustify,
+} from 'd3-sankey';
---
+++ packages/visx-sankey/src/types.ts
@@ -0,0 +1,10 @@
+export type { SankeyProps } from './Sankey';
+export type {
+  SankeyExtraProperties,
+  SankeyGraph,
+  SankeyLink,
+  SankeyNode,
+  SankeyNodeMinimal,
+  SankeyLinkMinimal,
+  SankeyLayout,
+} from 'd3-sankey';
