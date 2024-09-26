71208f7120c62fd7d0a4ccbbe5117ae7b159d8a7
Jackson Hardaker <jackson@bitdrift.io>
2024-09-26 10:26:20 -0400

    add example tile to docs page

--- packages/visx-demo/src/pages/docs/sankey.tsx
+++ packages/visx-demo/src/pages/docs/sankey.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import SankeyReadme from '!!raw-loader!../../../../visx-sankey/Readme.md';
 import Sankey from '../../../../visx-sankey/src/Sankey';
 import DocPage from '../../components/DocPage';
+import SankeyTile from '../../components/Gallery/SankeyTile';
 
 const components = [Sankey];
 
-const examples = [];
+const examples = [SankeyTile];
 
 function SankeyDocs() {
   return (
