320467e35322037106d441a54c9a6ca977deccf8
Tobias <t@tobiasjordans.de>
2025-10-08 01:33:13 +0200

    Docs: Fix link (#1898)

--- packages/visx-demo/src/pages/docs.tsx
+++ packages/visx-demo/src/pages/docs.tsx
@@ -11,7 +11,7 @@ export default function Docs() {
         <p>
           <code>visx</code> is a suite of several low-level standalone packages for building visual
           interfaces with <code>react</code>. Packages can be mixed and used together depending on
-          your use case, or you can simply add the umbrella <a href="/docs/visx">@visx/visx</a>{' '}
+          your use case, or you can simply add the umbrella <a href="/visx/docs/visx">@visx/visx</a>{' '}
           package to use them all.
           <br /> <br />
           Individual packages can be roughly categorized as follows:
