b5ab53219daaafba8f91b0a947d965bfc3105d20
Harrison Shoff <hi@hshoff.com>
2025-10-27 14:42:33 -0400

    internal(react-spring): migrate tests to use vitest and add mock implementations

---
+++ packages/visx-react-spring/vitest.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vitest/config';
+import path from 'path';
+
+export default defineConfig({
+  test: {
+    name: '@visx/react-spring',
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: [],
+    coverage: {
+      provider: 'v8',
+      reporter: ['lcov', 'json-summary', 'html', 'json', 'text'],
+      include: ['packages/visx-react-spring/src/**/*.{ts,tsx}'],
+      exclude: ['**/node_modules/**', '**/esm/**', '**/lib/**', '**/test/**', '**/dist/**'],
+      reportsDirectory: './coverage',
+    },
+  },
+  resolve: {
+    alias: {
+      '@visx/react-spring': path.resolve(__dirname, './src'),
+    },
+  },
+});
