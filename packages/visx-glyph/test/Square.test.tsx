b5134b57760bd01cbdbc38b14ea2b9290a1d9c84
Harrison Shoff <hi@hshoff.com>
2025-10-27 15:58:19 -0400

    tests(glyph): add missing import

--- packages/visx-glyph/test/Square.test.tsx
+++ packages/visx-glyph/test/Square.test.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { vi } from 'vitest';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { GlyphSquare } from '../src';
