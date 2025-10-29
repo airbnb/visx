0cacfdaea842d6e3420dc9d551eb13a188397c59
Harrison Shoff <hi@hshoff.com>
2025-10-29 16:42:20 -0400

    refactor: correct type assertion in isChildWithProps function to use React.ReactElement

--- packages/visx-xychart/src/utils/getChildrenAndGrandchildrenWithProps.ts
+++ packages/visx-xychart/src/utils/getChildrenAndGrandchildrenWithProps.ts
@@ -3,7 +3,7 @@ import React from 'react';
 /** Returns whether the React.ReactNode has props (and therefore is an `Element` versus primitive type) */
 function isChildWithProps<P extends object>(
   child: React.ReactNode,
-): child is React.ComponentType<P> {
+): child is React.ReactElement<P> {
   return !!child && typeof child === 'object' && 'props' in child && child.props != null;
 }
 
