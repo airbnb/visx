2a9fc62ba049be830c0267c866c4fce9a78b3884
Harrison Shoff <hi@hshoff.com>
2025-10-29 16:41:58 -0400

    refactor: update NodeProps type to clarify usage of node prop in DefaultNode component

--- packages/visx-network/src/DefaultNode.tsx
+++ packages/visx-network/src/DefaultNode.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 export type NodeProps = {
   cx?: number;
   cy?: number;
+  node?: unknown; // Allow node prop for internal usage when used as nodeComponent
 };
 
 export default function DefaultNode({
   r = 15,
   fill = '#21D4FD',
+  node, // Accept but don't use the node prop
   ...rest
 }: NodeProps & Omit<React.SVGProps<SVGCircleElement>, keyof NodeProps>) {
   return <circle r={r} fill={fill} {...rest} />;
